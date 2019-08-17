dcb78231bf27d43a1bf8fbf9bb332c09d46fb701
Martin Kielhorn <kielhorn.martin@gmail.com>
2019-08-17 12:40:51 +0200

    type definition of loop variable in dotimes

--- c.lisp
+++ c.lisp
@@ -412,7 +412,7 @@ entry return-values contains a list of return values"
 			       (emit iter)
 			       (emit `(progn ,@body)))))
 		(dotimes (destructuring-bind ((i n &optional (step 1)) &rest body) (cdr code)
-			   (emit `(for ((= ,(emit i) 0)
+			   (emit `(for ((,(format nil "int ~a = 0" (emit i)))
 					(< ,(emit i) ,(emit n))
 					(incf ,(emit i) ,(emit step)))
 				       ,@body))))
--- example/03_cuda/source/shader.cu
+++ example/03_cuda/source/shader.cu
@@ -1,29 +1,29 @@
 #include <wmma.h>
 // independent dot products
 // inefficient due to large working sets to hold parts of A and B
-for (i = 0; i < M; (i) += (1)) {
-  for (j = 0; j < N; (j) += (1)) {
-    for (k = 0; k < K; (k) += (1)) {
+for (int i = 0(); i < M; (i) += (1)) {
+  for (int j = 0(); j < N; (j) += (1)) {
+    for (int k = 0(); k < K; (k) += (1)) {
       (C[i][j]) += (((A[i][k]) * (B[k][j])));
     }
   }
 }
 // accumulate outer products
 // load elements of A and B exactly once
-for (k = 0; k < K; (k) += (1)) {
-  for (i = 0; i < M; (i) += (1)) {
-    for (j = 0; j < N; (j) += (1)) {
+for (int k = 0(); k < K; (k) += (1)) {
+  for (int i = 0(); i < M; (i) += (1)) {
+    for (int j = 0(); j < N; (j) += (1)) {
       (C[i][j]) += (((A[i][k]) * (B[k][j])));
     }
   }
 }
 // partition into Mtile-by-Ntile independent matrix products
-for (mb = 0; mb < M; (mb) += (Mtile)) {
-  for (nb = 0; nb < N; (nb) += (Ntile)) {
-    for (kb = 0; kb < K; (kb) += (Ktile)) {
-      for (k = 0; k < Ktile; (k) += (1)) {
-        for (i = 0; i < Mtile; (i) += (1)) {
-          for (j = 0; j < Ntile; (j) += (1)) {
+for (int mb = 0(); mb < M; (mb) += (Mtile)) {
+  for (int nb = 0(); nb < N; (nb) += (Ntile)) {
+    for (int kb = 0(); kb < K; (kb) += (Ktile)) {
+      for (int k = 0(); k < Ktile; (k) += (1)) {
+        for (int i = 0(); i < Mtile; (i) += (1)) {
+          for (int j = 0(); j < Ntile; (j) += (1)) {
             auto = ((mb) + (i));
             auto = ((nb) + (j));
             (C[row][col]) +=
@@ -35,13 +35,13 @@ for (mb = 0; mb < M; (mb) += (Mtile)) {
   }
 }
 // each warp computes independent matrix product
-for (mb = 0; mb < M; (mb) += (Mtile)) {
-  for (nb = 0; nb < N; (nb) += (Ntile)) {
-    for (kb = 0; kb < K; (kb) += (Ktile)) {
+for (int mb = 0(); mb < M; (mb) += (Mtile)) {
+  for (int nb = 0(); nb < N; (nb) += (Ntile)) {
+    for (int kb = 0(); kb < K; (kb) += (Ktile)) {
       // load A and B tiles into shared memory
-      for (m = 0; m < Mtile; (m) += (warp_m)) {
-        for (n = 0; n < Ntile; (n) += (warp_n)) {
-          for (k = 0; k < Ktile; (k) += (warp_k)) {
+      for (int m = 0(); m < Mtile; (m) += (warp_m)) {
+        for (int n = 0(); n < Ntile; (n) += (warp_n)) {
+          for (int k = 0(); k < Ktile; (k) += (warp_k)) {
             // compute warp_m by warp_n by warp_k GEMM
           }
         }
@@ -50,17 +50,17 @@ for (mb = 0; mb < M; (mb) += (Mtile)) {
   }
 }
 // accumulated matrix product in warps
-for (mb = 0; mb < M; (mb) += (Mtile)) {
-  for (nb = 0; nb < N; (nb) += (Ntile)) {
-    for (kb = 0; kb < K; (kb) += (Ktile)) {
+for (int mb = 0(); mb < M; (mb) += (Mtile)) {
+  for (int nb = 0(); nb < N; (nb) += (Ntile)) {
+    for (int kb = 0(); kb < K; (kb) += (Ktile)) {
       // load A and B tiles into shared memory
-      for (m = 0; m < Mtile; (m) += (warp_m)) {
-        for (n = 0; n < Ntile; (n) += (warp_n)) {
-          for (k = 0; k < Ktile; (k) += (warp_k)) {
+      for (int m = 0(); m < Mtile; (m) += (warp_m)) {
+        for (int n = 0(); n < Ntile; (n) += (warp_n)) {
+          for (int k = 0(); k < Ktile; (k) += (warp_k)) {
             // load A and B tile from SMEM into registers
-            for (tm = 0; tm < warp_m; (tm) += (thread_m)) {
-              for (tn = 0; tn < warp_n; (tn) += (thread_n)) {
-                for (tk = 0; tk < warp_k; (tk) += (thread_k)) {
+            for (int tm = 0(); tm < warp_m; (tm) += (thread_m)) {
+              for (int tn = 0(); tn < warp_n; (tn) += (thread_n)) {
+                for (int tk = 0(); tk < warp_k; (tk) += (thread_k)) {
                   // compute thread_m by thread_n by thread_k GEMM
                 }
               }
@@ -75,20 +75,20 @@ for (mb = 0; mb < M; (mb) += (Mtile)) {
 // A,B and C held in registers
 // O(M*N) computations on O(M+N) elements
 // opportunity for data reuse
-for (mb = 0; mb < M; (mb) += (Mtile)) {
-  for (nb = 0; nb < N; (nb) += (Ntile)) {
-    for (kb = 0; kb < K; (kb) += (Ktile)) {
+for (int mb = 0(); mb < M; (mb) += (Mtile)) {
+  for (int nb = 0(); nb < N; (nb) += (Ntile)) {
+    for (int kb = 0(); kb < K; (kb) += (Ktile)) {
       // load A and B tiles into shared memory
-      for (m = 0; m < Mtile; (m) += (warp_m)) {
-        for (n = 0; n < Ntile; (n) += (warp_n)) {
-          for (k = 0; k < Ktile; (k) += (warp_k)) {
+      for (int m = 0(); m < Mtile; (m) += (warp_m)) {
+        for (int n = 0(); n < Ntile; (n) += (warp_n)) {
+          for (int k = 0(); k < Ktile; (k) += (warp_k)) {
             // load A and B tile from SMEM into registers
-            for (tm = 0; tm < warp_m; (tm) += (thread_m)) {
-              for (tn = 0; tn < warp_n; (tn) += (thread_n)) {
-                for (tk = 0; tk < warp_k; (tk) += (thread_k)) {
-                  for (m = 0; m < thread_m; (m) += (1)) {
-                    for (n = 0; n < thread_n; (n) += (1)) {
-                      for (k = 0; k < thread_k; (k) += (1)) {
+            for (int tm = 0(); tm < warp_m; (tm) += (thread_m)) {
+              for (int tn = 0(); tn < warp_n; (tn) += (thread_n)) {
+                for (int tk = 0(); tk < warp_k; (tk) += (thread_k)) {
+                  for (int m = 0(); m < thread_m; (m) += (1)) {
+                    for (int n = 0(); n < thread_n; (n) += (1)) {
+                      for (int k = 0(); k < thread_k; (k) += (1)) {
                         // FMA instructions
                         (C[m][n]) += (((A[m][k]) * (B[n][k])));
                       }
@@ -115,27 +115,29 @@ for (mb = 0; mb < M; (mb) += (Mtile)) {
 // the same data from B wmma api is an alternative to the thread tile structure
 // each tensor core can process a 4x4x4 operation with matrices D=A.B+C (A,B
 // fp16; D,C fp16 or fp32)
-for (mb = 0; mb < M; (mb) += (Mtile)) {
-  for (nb = 0; nb < N; (nb) += (Ntile)) {
-    for (kb = 0; kb < K; (kb) += (Ktile)) {
+for (int mb = 0(); mb < M; (mb) += (Mtile)) {
+  for (int nb = 0(); nb < N; (nb) += (Ntile)) {
+    for (int kb = 0(); kb < K; (kb) += (Ktile)) {
       // load A and B tiles into shared memory
-      for (m = 0; m < Mtile; (m) += (warp_m)) {
-        for (n = 0; n < Ntile; (n) += (warp_n)) {
+      for (int m = 0(); m < Mtile; (m) += (warp_m)) {
+        for (int n = 0(); n < Ntile; (n) += (warp_n)) {
           __device__ void block_matrix_product() {
             fp16 frag_a[THREAD_ITEMS_X];
             fp16 frag_b[THREAD_ITEMS_Y];
             fp32 accum[THREAD_ITEMS_X][THREAD_ITEMS_Y];
             // load A and B tile from SMEM into registers
-            for (kblock = 0; kblock < Kdim; (kblock) += (BlockItemsK)) {
+            for (int kblock = 0(); kblock < Kdim; (kblock) += (BlockItemsK)) {
               __syncthreads();
 #pragma unroll
-              for (warp_k = 0; warp_k < BlockItemsK; (warp_k) += (WarpItemsK)) {
+              for (int warp_k = 0(); warp_k < BlockItemsK;
+                   (warp_k) += (WarpItemsK)) {
 // fetch frag_a and frag_b from SMEM corresponding to k index
 // accumulate an outer product
 #pragma unroll
-                for (thread_x = 0; thread_x < ThreadItemsX; (thread_x) += (1)) {
+                for (int thread_x = 0(); thread_x < ThreadItemsX;
+                     (thread_x) += (1)) {
 #pragma unroll
-                  for (thread_y = 0; thread_y < ThreadItemsY;
+                  for (int thread_y = 0(); thread_y < ThreadItemsY;
                        (thread_y) += (1)) {
                     (accum[thread_x][thread_y]) +=
                         (((frag_a[y]) * (frag_b[x])));
