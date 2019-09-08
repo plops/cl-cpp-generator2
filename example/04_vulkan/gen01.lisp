fcfd8a7d3be1a5bb6bc2a8e648e9a91bd0b957d9
Martin Kielhorn <kielhorn.martin@gmail.com>
2019-09-08 16:31:00 +0200

    start with vulkan base code

---
+++ example/04_vulkan/gen01.lisp
@@ -0,0 +1,67 @@
+(eval-when (:compile-toplevel :execute :load-toplevel)
+  (ql:quickload "cl-cpp-generator2"))
+
+(in-package :cl-cpp-generator2)
+
+(progn
+  (defparameter *code-file* (asdf:system-relative-pathname 'cl-cpp-generator2 "example/04_vulkan/source/run_01_base.cpp"))
+  (let* ((code
+	  `(do0
+	    "// https://vulkan-tutorial.com/"
+	    "// g++ -std=c++17 run_00_test.cpp  `pkg-config --static --libs glfw3` -lvulkan -o run_00_test"
+	    " "
+	    (do0 "#define GLFW_INCLUDE_VULKAN"
+		 (include <GLFW/glfw3.h>)
+		 " ")
+	    
+	    (do0
+	     "#define GLM_FORCE_RADIANS"
+	     "#define GLM_FORCE_DEPTH_ZERO_TO_ONE"
+	     " "
+	     (include <glm/vec4.hpp>
+		      <glm/mat4x4.hpp>)
+	     " "
+	     )
+	    
+	    " "
+	    (include <iostream>
+		     <stdexcept>
+		     <functional>
+		     <cstdlib>
+		     <vulkan/vulkan.h>)
+	    (defclass HelloTriangleApplication ()
+	      "public:"
+	      (defun run ()
+		(initVulkan)
+		(mainLoop)
+		(cleanup))
+	      "private:"
+	      (defun initVulkan ())
+	      (defun mainLoop ())
+	      (defun cleanup ()))
+	    (defun main ()
+	      (declare (values int))
+	      (glfwInit)
+	      (glfwWindowHint GLFW_CLIENT_API GLFW_NO_API)
+	      (let ((window (glfwCreateWindow 800 600
+					      (string "vulkan window")
+					      nullptr
+					      nullptr)))
+		(let ((extensionCount 0))
+		  (declare (type uint32_t extensionCount))
+		  (vkEnumerateInstanceExtensionProperties
+		   nullptr
+		   &extensionCount
+		   nullptr))
+		(let ((matrix)
+		      (vec)
+		      (test (* matrix vec)))
+		  (declare (type "glm::mat4" matrix)
+			   (type "glm::vec4" vec))
+		  (while (not (glfwWindowShouldClose window))
+		    (glfwPollEvents))
+		  (glfwDestroyWindow window)
+		  (glfwTerminate)
+		  (return 0)))))))
+    (write-source *code-file* code)))
+ 
---
+++ example/04_vulkan/source/run_01_base.cpp
@@ -0,0 +1,48 @@
+// https://vulkan-tutorial.com/
+// g++ -std=c++17 run_00_test.cpp  `pkg-config --static --libs glfw3` -lvulkan
+// -o run_00_test
+
+#define GLFW_INCLUDE_VULKAN
+#include <GLFW/glfw3.h>
+;
+#define GLM_FORCE_RADIANS
+#define GLM_FORCE_DEPTH_ZERO_TO_ONE
+
+#include <glm/mat4x4.hpp>
+#include <glm/vec4.hpp>
+;
+
+#include <cstdlib>
+#include <functional>
+#include <iostream>
+#include <stdexcept>
+#include <vulkan/vulkan.h>
+class HelloTriangleApplication : parents {
+public:
+  nil run() {
+    initVulkan();
+    mainLoop();
+    cleanup();
+  }
+
+private:
+  nil initVulkan() {}
+  nil mainLoop() {}
+  nil cleanup() {}
+};
+int main() {
+  glfwInit();
+  glfwWindowHint(GLFW_CLIENT_API, GLFW_NO_API);
+  auto window = glfwCreateWindow(800, 600, "vulkan window", nullptr, nullptr);
+  uint32_t extensionCount = 0;
+  vkEnumerateInstanceExtensionProperties(nullptr, &extensionCount, nullptr);
+  glm::mat4 matrix;
+  glm::vec4 vec;
+  auto test = ((matrix) * (vec));
+  while (!(glfwWindowShouldClose(window))) {
+    glfwPollEvents();
+  }
+  glfwDestroyWindow(window);
+  glfwTerminate();
+  return 0;
+}
