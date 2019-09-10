47b96b8e8487de4da5e2f00afa15f70e809fafad
Martin Kielhorn <kielhorn.martin@gmail.com>
2019-09-10 17:00:14 +0200

    typo

--- example/04_vulkan/gen01.lisp
+++ example/04_vulkan/gen01.lisp
@@ -40,7 +40,7 @@ more structs. this function helps to initialize those structs."
 	 (frag-code
 	  `(do0
 	    "#version 450"
-	    "extension GL_ARB_separate_shader_objects : enable"
+	    "#extension GL_ARB_separate_shader_objects : enable"
 	    " "
 	    "layout(location = 0) out vec4 outColor;"
 	    
--- example/04_vulkan/source/run_01_base.frag
+++ example/04_vulkan/source/run_01_base.frag
@@ -1,6 +1,5 @@
 #version 450
-extension GL_ARB_separate_shader_objects
-    : enable
+#extension GL_ARB_separate_shader_objects : enable
 
-          layout(location = 0) out vec4 outColor;
+layout(location = 0) out vec4 outColor;
 void main() { outColor = vec4(1, (0.0e+0), (0.0e+0), 1); }
