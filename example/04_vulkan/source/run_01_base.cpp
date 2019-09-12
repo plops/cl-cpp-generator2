1c905cba425ef2f43c258bbcdcf3366a887bcaeb
Martin Kielhorn <kielhorn.martin@gmail.com>
2019-09-12 13:40:45 +0200

    enable more compiler warnings

--- example/04_vulkan/source/run_01_base.cpp
+++ example/04_vulkan/source/run_01_base.cpp
@@ -1,7 +1,12 @@
 // https://vulkan-tutorial.com/en/Drawing_a_triangle/Setup/Base_code
 // https://vulkan-tutorial.com/en/Drawing_a_triangle/Setup/Validation_layers
 // g++ -std=c++17 run_01_base.cpp  `pkg-config --static --libs glfw3` -lvulkan
-// -o run_01_base
+// -o run_01_base -pedantic -Wall -Wextra -Wcast-align -Wcast-qual
+// -Wctor-dtor-privacy -Wdisabled-optimization -Wformat=2 -Winit-self
+// -Wlogical-op -Wmissing-declarations -Wmissing-include-dirs -Wnoexcept
+// -Wold-style-cast -Woverloaded-virtual -Wredundant-decls -Wshadow
+// -Wsign-conversion -Wsign-promo -Wstrict-null-sentinel -Wstrict-overflow=5
+// -Wswitch-default -Wundef -march=native -O2 -g
 
 #define GLFW_INCLUDE_VULKAN
 #include <GLFW/glfw3.h>
