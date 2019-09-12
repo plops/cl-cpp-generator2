2568aa7c1d5a1f1bd26cd6e4bd62bdbf01d6283f
Martin Kielhorn <kielhorn.martin@gmail.com>
2019-09-12 21:24:00 +0200

    start adding ubo

--- example/04_vulkan/gen01.lisp
+++ example/04_vulkan/gen01.lisp
@@ -103,6 +103,7 @@ more structs. this function helps to initialize those structs."
 	    "#version 450"
 	    "#extension GL_ARB_separate_shader_objects : enable"
 	    " "
+	    "layout(binding = 0) uniform UniformBufferObject { mat4 model; mat4 view; mat4 proj; } ubo;"
 	    "layout(location = 0) in vec2 inPosition;" ;; if inPosition was dvec3 than next location would need to be 2
 	    "layout(location = 1) in vec3 inColor;"
 	    "layout(location = 0) out vec3 fragColor;"
@@ -110,9 +111,12 @@ more structs. this function helps to initialize those structs."
 	    (defun main ()
 	      (declare (values void))
 	      (setf gl_Position
-		    (vec4 inPosition
-			  .0
-			  1.)
+		    (* ubo.proj
+		       ubo.view
+		       ubo.model
+		       (vec4 inPosition
+			   .0
+			   1.))
 		    fragColor inColor))
 	    "// vertex shader end "))
 	 (frag-code
