0b2929d92587fe199aaded7779c46b54999849e1
Martin Kielhorn <kielhorn.martin@gmail.com>
2019-09-13 06:53:53 +0200

    still everything black

--- example/04_vulkan/source/run_01_base.cpp
+++ example/04_vulkan/source/run_01_base.cpp
@@ -792,7 +792,7 @@ private:
     rasterizer.polygonMode = VK_POLYGON_MODE_FILL;
     rasterizer.lineWidth = (1.e+0f);
     rasterizer.cullMode = VK_CULL_MODE_BACK_BIT;
-    rasterizer.frontFace = VK_FRONT_FACE_COUNTER_CLOCKWISE;
+    rasterizer.frontFace = VK_FRONT_FACE_CLOCKWISE;
     rasterizer.depthBiasEnable = VK_FALSE;
     rasterizer.depthBiasConstantFactor = (0.0e+0f);
     rasterizer.depthBiasClamp = (0.0e+0f);
