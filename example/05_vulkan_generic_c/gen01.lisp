8e1889637597f5d52445c484a6646592f4778505
Martin Kielhorn <kielhorn.martin@gmail.com>
2019-09-25 08:37:45 +0200

    comment into sh

--- example/05_vulkan_generic_c/gen01.lisp
+++ example/05_vulkan_generic_c/gen01.lisp
@@ -15,14 +15,6 @@
 ;; gcc -std=c18 -c vulkan_00_main.c -Wmissing-declarations
 ;; gcc -std=c18 -c vulkan_01_instance.c -Wmissing-declarations
 
-;; vulkan_01_instance.c:43:6: warning: no previous declaration for 'createInstance' [-Wmissing-declarations]
-;;    43 | void createInstance() {
-;;       |      ^~~~~~~~~~~~~~
-
-;; gcc -std=c18 -c vulkan_01_instance.c -Wmissing-declarations 2>&1|grep '{'|cut -d '|' -f 2|cut -d '{' -f 1
-;; void createInstance() 
-
-;;gcc -std=c18 -c source/vulkan_*.c -Wmissing-declarations 2>&1|grep '{'|cut -d '|' -f 2|cut -d '{' -f 1|awk '{print $N";"}' > source/proto.h
 
 (progn
   ;; make sure to run this code twice during the first time, so that
--- example/05_vulkan_generic_c/gen_proto.sh
+++ example/05_vulkan_generic_c/gen_proto.sh
@@ -1,1 +1,10 @@
+
+# vulkan_01_instance.c:43:6: warning: no previous declaration for 'createInstance' [-Wmissing-declarations]
+#    43 | void createInstance() {
+#       |      ^~~~~~~~~~~~~~
+
+# gcc -std=c18 -c vulkan_01_instance.c -Wmissing-declarations 2>&1|grep '{'|cut -d '|' -f 2|cut -d '{' -f 1
+# void createInstance() 
+
+
 gcc -std=c18 -c source/vulkan_*.c -Wmissing-declarations 2>&1|grep '{'|cut -d '|' -f 2|cut -d '{' -f 1|awk '{print $N";"}' > source/proto.h
