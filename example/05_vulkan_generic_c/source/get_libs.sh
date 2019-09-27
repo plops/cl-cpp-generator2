665fe03b923f386fdf93787b2ab788af620d6437
Martin Kielhorn <kielhorn.martin@gmail.com>
2019-09-27 09:15:07 +0200

    get_libs.sh

---
+++ example/05_vulkan_generic_c/source/get_libs.sh
@@ -0,0 +1,6 @@
+curl -O https://raw.githubusercontent.com/nothings/stb/master/stb_image.h
+curl -O https://vulkan-tutorial.com/images/texture.jpg
+curl -O https://raw.githubusercontent.com/syoyo/tinyobjloader/master/tiny_obj_loader.h
+curl -O https://vulkan-tutorial.com/resources/chalet.obj.zip
+unzip chalet.obj.zip
+curl -O https://vulkan-tutorial.com/resources/chalet.jpg
