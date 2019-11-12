ce384dbf87b6b41085bcc8acb3f3ced26a6af4ae
Martin Kielhorn <kielhorn.martin@gmail.com>
2019-11-12 20:21:06 +0100

    more glfw boiler plate

--- example/03_cuda/gen06.lisp
+++ example/03_cuda/gen06.lisp
@@ -7,34 +7,53 @@
 
 ;; http://www.findinglisp.com/blog/2004/06/basic-automaton-macro.html
 
-(defun define-automaton (state)
-  
-  ((init
-    (t init_glfw))
-   (init_glfw
-    (t (do0
-	(unless (glfwInit)
-	  (setf state error)))))
-   (idle
-       (t idle
-	  (case )))
-   (error
-       (t error))))
-
 (progn
   (defparameter *code-file* (asdf:system-relative-pathname 'cl-cpp-generator2 "example/03_cuda/source/06interop.cu"))
   (let* ((code
 	  `(do0
-	    "// nvcc -o 06interop 06interop.cu -lglfw"
+	    "// nvcc -o 06interop 06interop.cu -lglfw -lGL"
 	    "// note that nvcc requires gcc 8"
 	    "// nvprof 06interpo"
 	    (include <GLFW/glfw3.h>)
+	    (defun key_callback (window key scancode action mods)
+	      (declare (type GLFWwindow* window)
+                      (type int key scancode action mods))
+             (when (and (or (== key GLFW_KEY_ESCAPE)
+                            (== key GLFW_KEY_Q))
+                        (== action GLFW_PRESS))
+               (glfwSetWindowShouldClose window GLFW_TRUE))
+
+	      
+	      )
 	    (defun main ()
 	      (declare (values int))
 	      (unless (glfwInit)
+
+		(glfwWindowHint GLFW_CONTEXT_VERSION_MAJOR 2)
+		(glfwWindowHint GLFW_CONTEXT_VERSION_MINOR 0)
+
 		(let ((window (glfwCreateWindow 640 480 (string "cuda interop")
 						NULL NULL)))
-		  (unless window))
+		  (unless window
+		    (exit -1))
+		  (glfwSetKeyCallback window key_callback)
+		  (glfwMakeContextCurrent window)
+                      
+		  (do0
+                     (let ((width)
+                           (height))
+                       (declare (type int width height))
+                       (glfwGetFramebufferSize window &width &height)
+                       (glViewport 0 0 width height))
+                     (glfwSwapInterval 1)
+                     (glClearColor 0 0 0 0)
+                     (glBlendFunc GL_SRC_ALPHA GL_ONE_MINUS_SRC_ALPHA)
+                     (glEnable GL_BLEND)
+                     (glEnable GL_LINE_SMOOTH)
+                     (glHint GL_LINE_SMOOTH GL_NICEST)
+                     )
+
+		  )
 		(glfwTerminate))
 	      ))))
     (write-source *code-file* code)))
--- example/03_cuda/source/06interop.cu
+++ example/03_cuda/source/06interop.cu
@@ -2,8 +2,31 @@
 // note that nvcc requires gcc 8
 // nvprof 06interpo
 #include <GLFW/glfw3.h>
+void key_callback (GLFWwindow* window, int key, int scancode, int action, int mods){
+        if ( (((((key)==(GLFW_KEY_ESCAPE))||((key)==(GLFW_KEY_Q))))&&((action)==(GLFW_PRESS))) ) {
+                        glfwSetWindowShouldClose(window, GLFW_TRUE);
+};
+}
 int main (){
         if ( !(glfwInit()) ) {
-                        glfwTerminate();
+                        glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 2);
+        glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 0);
+                auto window  = glfwCreateWindow(640, 480, "cuda interop", NULL, NULL);
+        if ( !(window) ) {
+                                    exit(-1);
+};
+        glfwSetKeyCallback(window, key_callback);
+        glfwMakeContextCurrent(window);
+                        int width ;
+        int height ;
+        glfwGetFramebufferSize(window, &width, &height);
+        glViewport(0, 0, width, height);
+        glfwSwapInterval(1);
+        glClearColor(0, 0, 0, 0);
+        glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA);
+        glEnable(GL_BLEND);
+        glEnable(GL_LINE_SMOOTH);
+        glHint(GL_LINE_SMOOTH, GL_NICEST);
+        glfwTerminate();
 };
 }
