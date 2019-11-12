84d3687ce20055f459903c330ac5634d0bd21edc
Martin Kielhorn <kielhorn.martin@gmail.com>
2019-11-12 21:14:36 +0100

    start implementing some cuda kernels

--- example/03_cuda/gen06.lisp
+++ example/03_cuda/gen06.lisp
@@ -17,7 +17,9 @@
 	    (include <GLFW/glfw3.h>
 		     <cassert>
 		     <cstdio>
-		     <iostream>)
+		     <iostream>
+		     <cuda_runtime.h>
+		     <cuda_gl_interop.h>)
 
 	    (defun key_callback (window key scancode action mods)
 	      (declare (type GLFWwindow* window)
@@ -34,6 +36,53 @@
                       description))
 
 	    "using namespace std;"
+
+	    (defstruct0 BC
+		(x int)
+	      (y int)
+	      (rad float)
+	      (chamfer int)
+	      (t_s float)
+	      (t_a float)
+	      (t_g float))
+
+	    "enum {TX=32, TY=32};"
+	    (defun divUp (a b)
+	      (declare (type int a b)
+		       (values int))
+	      (return (/ (+ a b -1)
+			 b)))
+
+	    (defun resetKernel (d_temp w h bc)
+	      (declare (type float* d_temp)
+		       (type int w h)
+		       (type BC bc)
+		       (values "__global__ void"))
+	      (let ((col (+ (* blockIdx.x
+			       blockDim.x)
+			    threadIdx.x))
+		    (row (+ (* blockIdx.y
+			       blockDim.y)
+			    threadIdx.y)))
+		(when (or (<= w col)
+			  (<= h row))
+		  (return)
+		  )
+		(setf (aref d_temp (+ col (* row w)))
+		      bc.t_a)))
+	    (defun resetTemperature (d_temp w h bc)
+	      (declare (type float* d_temp)
+		       (type int w h)
+		       (type BC bc))
+	      (let (((blockSize TX TY) )
+		    ((gridSize (divUp w TX)
+			       (divUp h TY))))
+		(declare (type "const dim3"
+			       (blockSize TX TY)
+			       (gridSize (divUp w TX)
+					 (divUp h TY))))
+		("resetKernel<<<gridSize,blockSize>>>" d_temp w h bc)))
+	    
 	    (defun main ()
 	      (declare (values int))
 	      (<< cout (string "bla") endl)
@@ -41,8 +90,9 @@
 		
 		(glfwSetErrorCallback error_callback)
 
-		(glfwWindowHint GLFW_CONTEXT_VERSION_MAJOR 2)
-		(glfwWindowHint GLFW_CONTEXT_VERSION_MINOR 0)
+		(do0
+		 (glfwWindowHint GLFW_CONTEXT_VERSION_MAJOR 2)
+		 (glfwWindowHint GLFW_CONTEXT_VERSION_MINOR 0))
 
 		(let ((window (glfwCreateWindow 640 480 (string "cuda interop")
 						NULL NULL)))
@@ -63,9 +113,23 @@
 			  (glEnable GL_BLEND)
 			  (glEnable GL_LINE_SMOOTH)
 			  (glHint GL_LINE_SMOOTH GL_NICEST))
+		     (do0
+		      (let ((d_temp  (static_cast<float*> 0))
+			    (bc (cast BC (curly
+					  (/ width 2)
+					  (/ height 2)
+					  (/ width 10s0)
+					  150
+					  212s0
+					  70s0
+					  0s0))))
+			(cudaMalloc &d_temp (* width height (sizeof *d_temp)))
+			(resetTemperature d_temp width height bc)
+			
+			))
                      )
 
-		   (while (not (glfwWindowShouldClose window))
+		  (while (not (glfwWindowShouldClose window))
 		     (glfwPollEvents)
                       (let ((time (glfwGetTime)))
                         (glClear GL_COLOR_BUFFER_BIT)
--- example/03_cuda/source/06interop.cu
+++ example/03_cuda/source/06interop.cu
@@ -4,6 +4,8 @@
 #include <GLFW/glfw3.h>
 #include <cassert>
 #include <cstdio>
+#include <cuda_gl_interop.h>
+#include <cuda_runtime.h>
 #include <iostream>
 void key_callback(GLFWwindow *window, int key, int scancode, int action,
                   int mods) {
@@ -16,6 +18,31 @@ void error_callback(int err, const char *description) {
   fprintf(stderr, "Error: %s\n", description);
 }
 using namespace std;
+struct BC {
+  int x;
+  int y;
+  float rad;
+  int chamfer;
+  float t_s;
+  float t_a;
+  float t_g;
+};
+typedef struct BC BC;
+enum { TX = 32, TY = 32 };
+int divUp(int a, int b) { return ((((a) + (b) + (-1))) / (b)); }
+__global__ void resetKernel(float *d_temp, int w, int h, BC bc) {
+  auto col = ((((blockIdx.x) * (blockDim.x))) + (threadIdx.x));
+  auto row = ((((blockIdx.y) * (blockDim.y))) + (threadIdx.y));
+  if ((((w) <= (col)) || ((h) <= (row)))) {
+    return;
+  };
+  d_temp[((col) + (((row) * (w))))] = bc.t_a;
+}
+void resetTemperature(float *d_temp, int w, int h, BC bc) {
+  const dim3 blockSize(TX, TY);
+  const dim3 gridSize(divUp(w, TX), divUp(h, TY));
+  resetKernel<<<gridSize, blockSize>>>(d_temp, w, h, bc);
+}
 int main() {
   (cout) << ("bla") << (endl);
   if (glfwInit()) {
@@ -32,6 +59,16 @@ int main() {
     glViewport(0, 0, width, height);
     glfwSwapInterval(1);
     glClearColor(0, 0, 0, 0);
+    auto d_temp = static_cast<float *>(0);
+    auto bc = (BC){((width) / (2)),
+                   ((height) / (2)),
+                   ((width) / ((1.e+1f))),
+                   150,
+                   (2.12e+2f),
+                   (7.e+1f),
+                   (0.0e+0f)};
+    cudaMalloc(&d_temp, ((width) * (height) * (sizeof(*d_temp))));
+    resetTemperature(d_temp, width, height, bc);
     while (!(glfwWindowShouldClose(window))) {
       glfwPollEvents();
       auto time = glfwGetTime();
