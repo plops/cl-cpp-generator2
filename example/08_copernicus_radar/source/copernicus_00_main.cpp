8bed9d80c14ea06ac466308af34a4ef9074d53c9
Martin Kielhorn <kielhorn.martin@gmail.com>
2019-12-02 17:32:16 +0100

    count samples for each azimuth angle (satellite position)
     162561924 copernicus_00_main.cpp:46 main map_ele  elevation_beam_address=11 number_of_Mquads=63.326
     162642718 copernicus_00_main.cpp:46 main map_ele  elevation_beam_address=10 number_of_Mquads=54.9172
     162652253 copernicus_00_main.cpp:46 main map_ele  elevation_beam_address=12 number_of_Mquads=66.6062
     162658182 copernicus_00_main.cpp:46 main map_ele  elevation_beam_address=13 number_of_Mquads=56.1727
     162662934 copernicus_00_main.cpp:46 main map_ele  elevation_beam_address=8 number_of_Mquads=0.518808
     162668342 copernicus_00_main.cpp:46 main map_ele  elevation_beam_address=9 number_of_Mquads=100.009
     162673546 copernicus_00_main.cpp:46 main map_ele  elevation_beam_address=15 number_of_Mquads=0.369372
     162678324 copernicus_00_main.cpp:58 main largest ele  ma_ele=9     ma=100.009
     195593608 copernicus_00_main.cpp:83 main map_azi  azi_beam_address=897 number_of_Mquads=0.89733
     195650447 copernicus_00_main.cpp:83 main map_azi  azi_beam_address=896 number_of_Mquads=0.089733
     195663333 copernicus_00_main.cpp:83 main map_azi  azi_beam_address=895 number_of_Mquads=0.089733
     195676504 copernicus_00_main.cpp:83 main map_azi  azi_beam_address=894 number_of_Mquads=0.179466
     195688106 copernicus_00_main.cpp:83 main map_azi  azi_beam_address=893 number_of_Mquads=0.089733
     195737169 copernicus_00_main.cpp:83 main map_azi  azi_beam_address=892 number_of_Mquads=0.089733
     195749452 copernicus_00_main.cpp:83 main map_azi  azi_beam_address=891 number_of_Mquads=0.179466
     195759276 copernicus_00_main.cpp:83 main map_azi  azi_beam_address=890 number_of_Mquads=0.089733
     195771027 copernicus_00_main.cpp:83 main map_azi  azi_beam_address=889 number_of_Mquads=0.179466
     195782213 copernicus_00_main.cpp:83 main map_azi  azi_beam_address=888 number_of_Mquads=0.089733
     195792010 copernicus_00_main.cpp:83 main map_azi  azi_beam_address=887 number_of_Mquads=0.089733
     195801470 copernicus_00_main.cpp:83 main map_azi  azi_beam_address=886 number_of_Mquads=0.179466
     195810732 copernicus_00_main.cpp:83 main map_azi  azi_beam_address=885 number_of_Mquads=0.089733

--- example/08_copernicus_radar/source/copernicus_00_main.cpp
+++ example/08_copernicus_radar/source/copernicus_00_main.cpp
@@ -59,6 +59,35 @@ int main() {
               << (__func__) << (" ") << ("largest ele") << (" ")
               << (std::setw(8)) << (" ma_ele=") << (ma_ele) << (std::setw(8))
               << (" ma=") << (ma) << (std::endl);
+  {
+    std::unordered_map<int, int> map_azi;
+    auto packet_idx = 0;
+    for (auto &e : state._header_data) {
+      auto offset = state._header_offset[packet_idx];
+      auto p = ((offset) + (static_cast<uint8_t *>(state._mmap_data)));
+      auto azi = ((((0x1) * (p[61]))) + (((0x100) * (((0x3) & (p[60]))))));
+      auto number_of_quads =
+          ((((0x1) * (p[66]))) + (((0x100) * (((0xFF) & (p[65]))))));
+      (map_azi[azi]) += (number_of_quads);
+      (packet_idx)++;
+    };
+    for (auto &azi : map_azi) {
+      auto number_of_Mquads = ((azi.second) / ((1.e+6f)));
+      auto azi_beam_address = azi.first;
+      std::setprecision(3);
+      (std::cout) << (std::setw(10))
+                  << (((std::chrono::high_resolution_clock::now()
+                            .time_since_epoch()
+                            .count()) -
+                       (state._start_time)))
+                  << (" ") << (__FILE__) << (":") << (__LINE__) << (" ")
+                  << (__func__) << (" ") << ("map_azi") << (" ")
+                  << (std::setw(8)) << (" azi_beam_address=")
+                  << (azi_beam_address) << (std::setw(8))
+                  << (" number_of_Mquads=") << (number_of_Mquads)
+                  << (std::endl);
+    };
+  };
   std::array<std::complex<float>, 65535> output;
   auto n = init_decode_packet(0, output);
   destroy_mmap();
