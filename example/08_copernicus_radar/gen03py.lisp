9dc37f002766487de7f450cdb2535eacab4ffc66
Martin Kielhorn <kielhorn.martin@gmail.com>
2020-01-04 16:50:19 +0100

    speed up loading of csv file

--- example/08_copernicus_radar/gen03py.lisp
+++ example/08_copernicus_radar/gen03py.lisp
@@ -46,189 +46,203 @@
 			  cupy.fft
 			  ))
 		"# %% echo packet information"
-		(setf df (pd.read_csv (string "./o_range.csv")))
-		(setf dfa (pd.read_csv (string "./o_all.csv")))
-		(setf
-		 cal_type_desc (list ,@(loop for e in `(tx_cal
-							rx_cal
-							epdn_cal
-							ta_cal
-							apdn_cal
-							na_0
-							na_1
-							txh_iso_cal)
-					  collect
-					    `(string ,e)))
-		 pol_desc (list ,@(loop for e in `(txh
-						   txh_rxh
-						   txh_rxv
-						   txh_rxvh
-						   txv
-						   txv_rxh
-						   txv_rxv
-						   txv_rxvh)
-				     collect
-				       `(string ,e)))
-		 rx_desc (list ,@(loop for e in `(rxv
-						  rxh) collect
-				      `(string ,e)))
-		 signal_type_desc (list ,@(loop for e in `(echo
-							   noise
-							   na2
-							   na3
-							   na4
-							   na5
-							   na6
-							   na7
-							   tx_cal
-							   rx_cal
-							   epdn_cal
-							   ta_cal
-							   apdn_cal
-							   na13
-							   na14
-							   txhiso_cal) collect
-				      `(string ,e)))
-		 )
-		"# %% calibration packet information"
-		(setf dfc (pd.read_csv (string "./o_cal_range.csv"))
-		 (aref dfc (string "pcc")) (np.mod dfc.cal_iter 2)
-		 )
-		,@(loop for d in `(dfc df dfa) collect
-		       `(do0
-			 ,@(loop for e in (case d
-					    (dfc `( pol rx signal_type cal_type))
-					    (df `( pol rx signal_type))) collect
-			    (let ((name (format nil "~a_desc" e)))
-			      `(do0
-				(setf (aref ,d (string ,name))
-				
-				      ("list" (map (lambda (x)
-						     (aref ,name x)
-						     )
-						   (dot ,d ,e))))
-				(setf (dot ,d ,name) (dot ,d ,name (astype (string "category")))))))))
+		(if (dot (pathlib.Path (string "dfa.csv"))
+			 (is_file))
+		 (do0
+		  ,@(loop for e in '(dfa df dfc) collect
+			 `(setf ,e (pd.read_csv (string ,(format nil "~a.csv" e))))))
+		 (do0
+		  (setf df (pd.read_csv (string "./o_range.csv")))
+		  (setf dfa (pd.read_csv (string "./o_all.csv")))
+		  (setf
+		   cal_type_desc (list ,@(loop for e in `(tx_cal
+							  rx_cal
+							  epdn_cal
+							  ta_cal
+							  apdn_cal
+							  na_0
+							  na_1
+							  txh_iso_cal)
+					    collect
+					      `(string ,e)))
+		   pol_desc (list ,@(loop for e in `(txh
+						     txh_rxh
+						     txh_rxv
+						     txh_rxvh
+						     txv
+						     txv_rxh
+						     txv_rxv
+						     txv_rxvh)
+				       collect
+					 `(string ,e)))
+		   rx_desc (list ,@(loop for e in `(rxv
+						    rxh) collect
+					`(string ,e)))
+		   signal_type_desc (list ,@(loop for e in `(echo
+							     noise
+							     na2
+							     na3
+							     na4
+							     na5
+							     na6
+							     na7
+							     tx_cal
+							     rx_cal
+							     epdn_cal
+							     ta_cal
+							     apdn_cal
+							     na13
+							     na14
+							     txhiso_cal) collect
+						 `(string ,e)))
+		   )
+		  "# %% calibration packet information"
+		  (setf dfc (pd.read_csv (string "./o_cal_range.csv"))
+			(aref dfc (string "pcc")) (np.mod dfc.cal_iter 2))
+		  ,@(loop for d in `(dfc df dfa) collect
+			 `(do0
+			   ,@(loop for e in (case d
+					      (dfc `( pol rx signal_type cal_type))
+					      (df `( pol rx signal_type))) collect
+				  (let ((name (format nil "~a_desc" e)))
+				    `(do0
+				      (setf (aref ,d (string ,name))
+					   
+					    ("list" (map (lambda (x)
+							   (aref ,name x)
+							   )
+							 (dot ,d ,e))))
+				      (setf (dot ,d ,name) (dot ,d ,name (astype (string "category")))))))))
+
+		 
+		 
+
+		  (do0 "# %% sample rate computation using 3.2.5.4, tab5.1-1, tab5.1-2, txpl and swl description"
+		       (setf decimation_filter_bandwidth
+			     (list 100 87.71 -1 74.25 59.44
+				   50.62 44.89 22.2 56.59 42.86
+				   15.1 48.35))
+		       (setf decimation_filter_L (list 3 2 -1 5 4 3 1 1 3 5 3 4))
+		       (setf decimation_filter_M (list 4 3 -1 9 9 8 3 6 7 16 26 11))
+		       (setf decimation_filter_length_NF
+			     (list 28 28 -1 32 40 48 52  92 36 68 120 44))
+		       (setf decimation_filter_output_offset
+			     (list 87 87 -1 88 90 92 93 103 89 97 110 91))
+		       (setf decimation_filter_swath_desc
+			     (list
+			      ,@(loop for e in `(full_bandwidth
+						 s1_wv1
+						 n/a
+						 s2
+						 s3
+						 s4
+						 s5
+						 ew1
+						 iw1
+						 s6_iw3
+						 ew2_ew3_ew4_ew5
+						 iw2_wv2)
+				   collect
+				     `(string ,e))))
+		       "# table 5.1-1 D values as a function of rgdec and C. first index is rgdec"
+		       (setf decimation_filter_D
+			     (list (list 1 1 2 3)
+				   (list 1 1 2)
+				   (list -1)
+				   (list 1 1 2 2 3 3 4 4 5)
+				   (list 0 1 1 2 2 3 3 4 4)
+				   (list 0 1 1 1 2 2 3 3)
+				   (list 0 0 1)
+				   (list 0 0 0 0 0 1)
+				   (list 0 1 1 2 2 3 3)
+				   (list 0 0 1 1 1 2 2 2 2 3 3 3 4 4 4 5)
+				   (list 0 0 0 0 0 0 0 1 1 1 1 1 1 1 1 1 ;; as many as prev col
+					 2 2 2 2  2 2 2 2 ; 8 times 2
+					 3 3
+					 )
+				   (list 0 1 1 1 2 2 3 3 3 4 4))))
+		 
+		  ,@(loop for d in `(dfc df dfa) collect
+			 `(do0
+			   ,@(loop for e in `(decimation_filter_bandwidth
+					      decimation_filter_L
+					      decimation_filter_M
+					      decimation_filter_length_NF
+					      decimation_filter_output_offset
+					      decimation_filter_swath_desc)
+				collect
+				  (let ((name (format nil "~a" e)))
+				    `(do0
+				      (setf (aref ,d (string ,name))
+					    ("list" (map (lambda (x)
+							   (aref ,name x))
+							 (dot ,d rgdec)))))))))
+		  (print (string "decimation filter .."))
+		  (setf fref 37.53472224)
+		  ,@(loop for d in `(dfc df dfa) collect
+			 `(do0
+			   (setf (aref ,d (string "fdec"))
+				 (* 4 fref
+				    (/ (dot ,d decimation_filter_L)
+				       (dot ,d decimation_filter_M))))
+			   (setf (aref ,d (string "N3_tx"))
+				 (dot (np.ceil 
+				       (* (dot ,d fdec)
+					  (dot ,d txpl)))
+				      (astype np.int)))
+			   (setf (aref ,d (string "decimation_filter_B"))
+				 (- (* 2 (dot ,d swl))
+				    (+ (dot ,d decimation_filter_output_offset) 17)))
+			   (setf (aref ,d (string "decimation_filter_C"))
+				 (- (dot ,d decimation_filter_B)
+				    (* (dot ,d decimation_filter_M)
+				       (// (dot ,d decimation_filter_B)
+					   (dot ,d decimation_filter_M)))))
+			   (setf (aref ,d (string "N3_rx"))
+				 ("list"
+				  (map
+				   (lambda (idx_row)
+				     (* 2 
+					(+ (* (dot (aref idx_row 1) decimation_filter_L)
+					      (// (dot (aref idx_row 1) decimation_filter_B)
+						  (dot (aref idx_row 1) decimation_filter_M)))
+					   (aref (aref decimation_filter_D
+						       (dot (aref idx_row 1) rgdec))
+						 (dot (aref idx_row 1) decimation_filter_C))
+					   1)))
+				   (dot ,d (iterrows)))))))
+
+
+
+		  (do0 "# %% get pulse configuration that is rank pri_counts in the past"
+		       (print (string "pulse from past"))
+		       ;;  dfap.loc[dfa.pri_count-dfa['rank']].txprr
+		       (setf dfap (dot dfa (set_index (string "pri_count")))
+			     )
+		       ,@(loop for e in `(txprr
+					  txprr_
+					  txpl
+					  txpl_
+					  txpsf
+					  ses_ssb_tx_pulse_number)
+			    collect
+			      `(setf (aref dfa (string ,(format nil "ranked_~a" e)))
+				     (dot dfap
+					  (aref loc
+						(- dfa.pri_count
+						   (aref dfa (string "rank"))))
+					  (reset_index)
+					  ,e))))
+		 
+		  (do0
+		   ,@(loop for e in '(dfa df dfc) collect
+			  `(dot ,e (to_csv (string ,(format nil "~a.csv" e))))))))
+
+
 
 		
+
 		
-
-		(do0 "# %% sample rate computation using 3.2.5.4, tab5.1-1, tab5.1-2, txpl and swl description"
-		     (setf decimation_filter_bandwidth
-		       (list 100 87.71 -1 74.25 59.44
-			     50.62 44.89 22.2 56.59 42.86
-			     15.1 48.35))
-		     (setf decimation_filter_L (list 3 2 -1 5 4 3 1 1 3 5 3 4))
-		     (setf decimation_filter_M (list 4 3 -1 9 9 8 3 6 7 16 26 11))
-		     (setf decimation_filter_length_NF
-			   (list 28 28 -1 32 40 48 52  92 36 68 120 44))
-		     (setf decimation_filter_output_offset
-			   (list 87 87 -1 88 90 92 93 103 89 97 110 91))
-		     (setf decimation_filter_swath_desc
-			   (list
-			    ,@(loop for e in `(full_bandwidth
-					       s1_wv1
-					       n/a
-					       s2
-					       s3
-					       s4
-					       s5
-					       ew1
-					       iw1
-					       s6_iw3
-					       ew2_ew3_ew4_ew5
-					       iw2_wv2)
-				 collect
-				   `(string ,e))))
-		     "# table 5.1-1 D values as a function of rgdec and C. first index is rgdec"
-		     (setf decimation_filter_D
-			   (list (list 1 1 2 3)
-				 (list 1 1 2)
-				 (list -1)
-				 (list 1 1 2 2 3 3 4 4 5)
-				 (list 0 1 1 2 2 3 3 4 4)
-				 (list 0 1 1 1 2 2 3 3)
-				 (list 0 0 1)
-				 (list 0 0 0 0 0 1)
-				 (list 0 1 1 2 2 3 3)
-				 (list 0 0 1 1 1 2 2 2 2 3 3 3 4 4 4 5)
-				 (list 0 0 0 0 0 0 0 1 1 1 1 1 1 1 1 1 ;; as many as prev col
-				       2 2 2 2  2 2 2 2 ; 8 times 2
-				       3 3
-				       )
-				 (list 0 1 1 1 2 2 3 3 3 4 4))))
-		
-		,@(loop for d in `(dfc df dfa) collect
-		       `(do0
-			 ,@(loop for e in `(decimation_filter_bandwidth
-					    decimation_filter_L
-					    decimation_filter_M
-					    decimation_filter_length_NF
-					    decimation_filter_output_offset
-					    decimation_filter_swath_desc)
-			      collect
-				(let ((name (format nil "~a" e)))
-				  `(do0
-				    (setf (aref ,d (string ,name))
-					  ("list" (map (lambda (x)
-							 (aref ,name x))
-						       (dot ,d rgdec)))))))))
-		(print (string "decimation filter .."))
-		(setf fref 37.53472224)
-		,@(loop for d in `(dfc df dfa) collect
-		       `(do0
-			 (setf (aref ,d (string "fdec"))
-			       (* 4 fref
-				  (/ (dot ,d decimation_filter_L)
-				     (dot ,d decimation_filter_M))))
-			 (setf (aref ,d (string "N3_tx"))
-			       (dot (np.ceil 
-				     (* (dot ,d fdec)
-					(dot ,d txpl)))
-				    (astype np.int)))
-			 (setf (aref ,d (string "decimation_filter_B"))
-			       (- (* 2 (dot ,d swl))
-				  (+ (dot ,d decimation_filter_output_offset) 17)))
-			 (setf (aref ,d (string "decimation_filter_C"))
-			       (- (dot ,d decimation_filter_B)
-				  (* (dot ,d decimation_filter_M)
-				     (// (dot ,d decimation_filter_B)
-					 (dot ,d decimation_filter_M)))))
-			 (setf (aref ,d (string "N3_rx"))
-			       ("list"
-				(map
-				 (lambda (idx_row)
-				   (* 2 
-				      (+ (* (dot (aref idx_row 1) decimation_filter_L)
-					    (// (dot (aref idx_row 1) decimation_filter_B)
-						(dot (aref idx_row 1) decimation_filter_M)))
-					 (aref (aref decimation_filter_D
-						     (dot (aref idx_row 1) rgdec))
-					       (dot (aref idx_row 1) decimation_filter_C))
-					 1)))
-				 (dot ,d (iterrows)))))))
-
-
-
-		(do0 "# %% get pulse configuration that is rank pri_counts in the past"
-		     (print (string "pulse from past"))
-		     ;;  dfap.loc[dfa.pri_count-dfa['rank']].txprr
-		     (setf dfap (dot dfa (set_index (string "pri_count")))
-			   )
-		     ,@(loop for e in `(txprr
-					txprr_
-					txpl
-					txpl_
-					txpsf
-					ses_ssb_tx_pulse_number)
-			  collect
-			    `(setf (aref dfa (string ,(format nil "ranked_~a" e)))
-				   (dot dfap
-					(aref loc
-					      (- dfa.pri_count
-						 (aref dfa (string "rank"))))
-					(reset_index)
-					,e))))
-
 		(print (string "start mmap .."))
 		(setf s (np.memmap (next (dot (pathlib.Path (string "./"))
 					      (glob (string "o_cal*.cf"))))
--- example/08_copernicus_radar/source/run_03_compress.py
+++ example/08_copernicus_radar/source/run_03_compress.py
@@ -18,82 +18,91 @@ import numba.cuda
 import cupy as cp
 import cupy.fft
 # %% echo packet information
-df=pd.read_csv("./o_range.csv")
-dfa=pd.read_csv("./o_all.csv")
-cal_type_desc=["tx_cal", "rx_cal", "epdn_cal", "ta_cal", "apdn_cal", "na_0", "na_1", "txh_iso_cal"]
-pol_desc=["txh", "txh_rxh", "txh_rxv", "txh_rxvh", "txv", "txv_rxh", "txv_rxv", "txv_rxvh"]
-rx_desc=["rxv", "rxh"]
-signal_type_desc=["echo", "noise", "na2", "na3", "na4", "na5", "na6", "na7", "tx_cal", "rx_cal", "epdn_cal", "ta_cal", "apdn_cal", "na13", "na14", "txhiso_cal"]
-# %% calibration packet information
-dfc=pd.read_csv("./o_cal_range.csv")
-dfc["pcc"]=np.mod(dfc.cal_iter, 2)
-dfc["pol_desc"]=list(map(lambda x: pol_desc[x], dfc.pol))
-dfc.pol_desc=dfc.pol_desc.astype("category")
-dfc["rx_desc"]=list(map(lambda x: rx_desc[x], dfc.rx))
-dfc.rx_desc=dfc.rx_desc.astype("category")
-dfc["signal_type_desc"]=list(map(lambda x: signal_type_desc[x], dfc.signal_type))
-dfc.signal_type_desc=dfc.signal_type_desc.astype("category")
-dfc["cal_type_desc"]=list(map(lambda x: cal_type_desc[x], dfc.cal_type))
-dfc.cal_type_desc=dfc.cal_type_desc.astype("category")
-df["pol_desc"]=list(map(lambda x: pol_desc[x], df.pol))
-df.pol_desc=df.pol_desc.astype("category")
-df["rx_desc"]=list(map(lambda x: rx_desc[x], df.rx))
-df.rx_desc=df.rx_desc.astype("category")
-df["signal_type_desc"]=list(map(lambda x: signal_type_desc[x], df.signal_type))
-df.signal_type_desc=df.signal_type_desc.astype("category")
-# %% sample rate computation using 3.2.5.4, tab5.1-1, tab5.1-2, txpl and swl description
-decimation_filter_bandwidth=[100, (8.770999908447266e+1), -1, (7.424999999999999e+1), (5.943999862670898e+1), (5.061999893188476e+1), (4.4889999389648433e+1), (2.220000076293945e+1), (5.659000015258788e+1), (4.286000061035156e+1), (1.5100000381469728e+1), (4.834999847412109e+1)]
-decimation_filter_L=[3, 2, -1, 5, 4, 3, 1, 1, 3, 5, 3, 4]
-decimation_filter_M=[4, 3, -1, 9, 9, 8, 3, 6, 7, 16, 26, 11]
-decimation_filter_length_NF=[28, 28, -1, 32, 40, 48, 52, 92, 36, 68, 120, 44]
-decimation_filter_output_offset=[87, 87, -1, 88, 90, 92, 93, 103, 89, 97, 110, 91]
-decimation_filter_swath_desc=["full_bandwidth", "s1_wv1", "n/a", "s2", "s3", "s4", "s5", "ew1", "iw1", "s6_iw3", "ew2_ew3_ew4_ew5", "iw2_wv2"]
-# table 5.1-1 D values as a function of rgdec and C. first index is rgdec
-decimation_filter_D=[[1, 1, 2, 3], [1, 1, 2], [-1], [1, 1, 2, 2, 3, 3, 4, 4, 5], [0, 1, 1, 2, 2, 3, 3, 4, 4], [0, 1, 1, 1, 2, 2, 3, 3], [0, 0, 1], [0, 0, 0, 0, 0, 1], [0, 1, 1, 2, 2, 3, 3], [0, 0, 1, 1, 1, 2, 2, 2, 2, 3, 3, 3, 4, 4, 4, 5], [0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 2, 2, 2, 2, 2, 2, 2, 2, 3, 3], [0, 1, 1, 1, 2, 2, 3, 3, 3, 4, 4]]
-dfc["decimation_filter_bandwidth"]=list(map(lambda x: decimation_filter_bandwidth[x], dfc.rgdec))
-dfc["decimation_filter_L"]=list(map(lambda x: decimation_filter_L[x], dfc.rgdec))
-dfc["decimation_filter_M"]=list(map(lambda x: decimation_filter_M[x], dfc.rgdec))
-dfc["decimation_filter_length_NF"]=list(map(lambda x: decimation_filter_length_NF[x], dfc.rgdec))
-dfc["decimation_filter_output_offset"]=list(map(lambda x: decimation_filter_output_offset[x], dfc.rgdec))
-dfc["decimation_filter_swath_desc"]=list(map(lambda x: decimation_filter_swath_desc[x], dfc.rgdec))
-df["decimation_filter_bandwidth"]=list(map(lambda x: decimation_filter_bandwidth[x], df.rgdec))
-df["decimation_filter_L"]=list(map(lambda x: decimation_filter_L[x], df.rgdec))
-df["decimation_filter_M"]=list(map(lambda x: decimation_filter_M[x], df.rgdec))
-df["decimation_filter_length_NF"]=list(map(lambda x: decimation_filter_length_NF[x], df.rgdec))
-df["decimation_filter_output_offset"]=list(map(lambda x: decimation_filter_output_offset[x], df.rgdec))
-df["decimation_filter_swath_desc"]=list(map(lambda x: decimation_filter_swath_desc[x], df.rgdec))
-dfa["decimation_filter_bandwidth"]=list(map(lambda x: decimation_filter_bandwidth[x], dfa.rgdec))
-dfa["decimation_filter_L"]=list(map(lambda x: decimation_filter_L[x], dfa.rgdec))
-dfa["decimation_filter_M"]=list(map(lambda x: decimation_filter_M[x], dfa.rgdec))
-dfa["decimation_filter_length_NF"]=list(map(lambda x: decimation_filter_length_NF[x], dfa.rgdec))
-dfa["decimation_filter_output_offset"]=list(map(lambda x: decimation_filter_output_offset[x], dfa.rgdec))
-dfa["decimation_filter_swath_desc"]=list(map(lambda x: decimation_filter_swath_desc[x], dfa.rgdec))
-print("decimation filter ..")
-fref=(3.7534721374511715e+1)
-dfc["fdec"]=((4)*(fref)*(((dfc.decimation_filter_L)/(dfc.decimation_filter_M))))
-dfc["N3_tx"]=np.ceil(((dfc.fdec)*(dfc.txpl))).astype(np.int)
-dfc["decimation_filter_B"]=((((2)*(dfc.swl)))-(((dfc.decimation_filter_output_offset)+(17))))
-dfc["decimation_filter_C"]=((dfc.decimation_filter_B)-(((dfc.decimation_filter_M)*(((dfc.decimation_filter_B)//(dfc.decimation_filter_M))))))
-dfc["N3_rx"]=list(map(lambda idx_row: ((2)*(((((idx_row[1].decimation_filter_L)*(((idx_row[1].decimation_filter_B)//(idx_row[1].decimation_filter_M)))))+(decimation_filter_D[idx_row[1].rgdec][idx_row[1].decimation_filter_C])+(1)))), dfc.iterrows()))
-df["fdec"]=((4)*(fref)*(((df.decimation_filter_L)/(df.decimation_filter_M))))
-df["N3_tx"]=np.ceil(((df.fdec)*(df.txpl))).astype(np.int)
-df["decimation_filter_B"]=((((2)*(df.swl)))-(((df.decimation_filter_output_offset)+(17))))
-df["decimation_filter_C"]=((df.decimation_filter_B)-(((df.decimation_filter_M)*(((df.decimation_filter_B)//(df.decimation_filter_M))))))
-df["N3_rx"]=list(map(lambda idx_row: ((2)*(((((idx_row[1].decimation_filter_L)*(((idx_row[1].decimation_filter_B)//(idx_row[1].decimation_filter_M)))))+(decimation_filter_D[idx_row[1].rgdec][idx_row[1].decimation_filter_C])+(1)))), df.iterrows()))
-dfa["fdec"]=((4)*(fref)*(((dfa.decimation_filter_L)/(dfa.decimation_filter_M))))
-dfa["N3_tx"]=np.ceil(((dfa.fdec)*(dfa.txpl))).astype(np.int)
-dfa["decimation_filter_B"]=((((2)*(dfa.swl)))-(((dfa.decimation_filter_output_offset)+(17))))
-dfa["decimation_filter_C"]=((dfa.decimation_filter_B)-(((dfa.decimation_filter_M)*(((dfa.decimation_filter_B)//(dfa.decimation_filter_M))))))
-dfa["N3_rx"]=list(map(lambda idx_row: ((2)*(((((idx_row[1].decimation_filter_L)*(((idx_row[1].decimation_filter_B)//(idx_row[1].decimation_filter_M)))))+(decimation_filter_D[idx_row[1].rgdec][idx_row[1].decimation_filter_C])+(1)))), dfa.iterrows()))
-# %% get pulse configuration that is rank pri_counts in the past
-print("pulse from past")
-dfap=dfa.set_index("pri_count")
-dfa["ranked_txprr"]=dfap.loc[((dfa.pri_count)-(dfa["rank"]))].reset_index().txprr
-dfa["ranked_txprr_"]=dfap.loc[((dfa.pri_count)-(dfa["rank"]))].reset_index().txprr_
-dfa["ranked_txpl"]=dfap.loc[((dfa.pri_count)-(dfa["rank"]))].reset_index().txpl
-dfa["ranked_txpl_"]=dfap.loc[((dfa.pri_count)-(dfa["rank"]))].reset_index().txpl_
-dfa["ranked_txpsf"]=dfap.loc[((dfa.pri_count)-(dfa["rank"]))].reset_index().txpsf
-dfa["ranked_ses_ssb_tx_pulse_number"]=dfap.loc[((dfa.pri_count)-(dfa["rank"]))].reset_index().ses_ssb_tx_pulse_number
+if ( pathlib.Path("dfa.csv").is_file() ):
+    dfa=pd.read_csv("dfa.csv")
+    df=pd.read_csv("df.csv")
+    dfc=pd.read_csv("dfc.csv")
+else:
+    df=pd.read_csv("./o_range.csv")
+    dfa=pd.read_csv("./o_all.csv")
+    cal_type_desc=["tx_cal", "rx_cal", "epdn_cal", "ta_cal", "apdn_cal", "na_0", "na_1", "txh_iso_cal"]
+    pol_desc=["txh", "txh_rxh", "txh_rxv", "txh_rxvh", "txv", "txv_rxh", "txv_rxv", "txv_rxvh"]
+    rx_desc=["rxv", "rxh"]
+    signal_type_desc=["echo", "noise", "na2", "na3", "na4", "na5", "na6", "na7", "tx_cal", "rx_cal", "epdn_cal", "ta_cal", "apdn_cal", "na13", "na14", "txhiso_cal"]
+    # %% calibration packet information
+    dfc=pd.read_csv("./o_cal_range.csv")
+    dfc["pcc"]=np.mod(dfc.cal_iter, 2)
+    dfc["pol_desc"]=list(map(lambda x: pol_desc[x], dfc.pol))
+    dfc.pol_desc=dfc.pol_desc.astype("category")
+    dfc["rx_desc"]=list(map(lambda x: rx_desc[x], dfc.rx))
+    dfc.rx_desc=dfc.rx_desc.astype("category")
+    dfc["signal_type_desc"]=list(map(lambda x: signal_type_desc[x], dfc.signal_type))
+    dfc.signal_type_desc=dfc.signal_type_desc.astype("category")
+    dfc["cal_type_desc"]=list(map(lambda x: cal_type_desc[x], dfc.cal_type))
+    dfc.cal_type_desc=dfc.cal_type_desc.astype("category")
+    df["pol_desc"]=list(map(lambda x: pol_desc[x], df.pol))
+    df.pol_desc=df.pol_desc.astype("category")
+    df["rx_desc"]=list(map(lambda x: rx_desc[x], df.rx))
+    df.rx_desc=df.rx_desc.astype("category")
+    df["signal_type_desc"]=list(map(lambda x: signal_type_desc[x], df.signal_type))
+    df.signal_type_desc=df.signal_type_desc.astype("category")
+    
+    # %% sample rate computation using 3.2.5.4, tab5.1-1, tab5.1-2, txpl and swl description
+    decimation_filter_bandwidth=[100, (8.770999908447266e+1), -1, (7.424999999999999e+1), (5.943999862670898e+1), (5.061999893188476e+1), (4.4889999389648433e+1), (2.220000076293945e+1), (5.659000015258788e+1), (4.286000061035156e+1), (1.5100000381469728e+1), (4.834999847412109e+1)]
+    decimation_filter_L=[3, 2, -1, 5, 4, 3, 1, 1, 3, 5, 3, 4]
+    decimation_filter_M=[4, 3, -1, 9, 9, 8, 3, 6, 7, 16, 26, 11]
+    decimation_filter_length_NF=[28, 28, -1, 32, 40, 48, 52, 92, 36, 68, 120, 44]
+    decimation_filter_output_offset=[87, 87, -1, 88, 90, 92, 93, 103, 89, 97, 110, 91]
+    decimation_filter_swath_desc=["full_bandwidth", "s1_wv1", "n/a", "s2", "s3", "s4", "s5", "ew1", "iw1", "s6_iw3", "ew2_ew3_ew4_ew5", "iw2_wv2"]
+    # table 5.1-1 D values as a function of rgdec and C. first index is rgdec
+    decimation_filter_D=[[1, 1, 2, 3], [1, 1, 2], [-1], [1, 1, 2, 2, 3, 3, 4, 4, 5], [0, 1, 1, 2, 2, 3, 3, 4, 4], [0, 1, 1, 1, 2, 2, 3, 3], [0, 0, 1], [0, 0, 0, 0, 0, 1], [0, 1, 1, 2, 2, 3, 3], [0, 0, 1, 1, 1, 2, 2, 2, 2, 3, 3, 3, 4, 4, 4, 5], [0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 2, 2, 2, 2, 2, 2, 2, 2, 3, 3], [0, 1, 1, 1, 2, 2, 3, 3, 3, 4, 4]]
+    dfc["decimation_filter_bandwidth"]=list(map(lambda x: decimation_filter_bandwidth[x], dfc.rgdec))
+    dfc["decimation_filter_L"]=list(map(lambda x: decimation_filter_L[x], dfc.rgdec))
+    dfc["decimation_filter_M"]=list(map(lambda x: decimation_filter_M[x], dfc.rgdec))
+    dfc["decimation_filter_length_NF"]=list(map(lambda x: decimation_filter_length_NF[x], dfc.rgdec))
+    dfc["decimation_filter_output_offset"]=list(map(lambda x: decimation_filter_output_offset[x], dfc.rgdec))
+    dfc["decimation_filter_swath_desc"]=list(map(lambda x: decimation_filter_swath_desc[x], dfc.rgdec))
+    df["decimation_filter_bandwidth"]=list(map(lambda x: decimation_filter_bandwidth[x], df.rgdec))
+    df["decimation_filter_L"]=list(map(lambda x: decimation_filter_L[x], df.rgdec))
+    df["decimation_filter_M"]=list(map(lambda x: decimation_filter_M[x], df.rgdec))
+    df["decimation_filter_length_NF"]=list(map(lambda x: decimation_filter_length_NF[x], df.rgdec))
+    df["decimation_filter_output_offset"]=list(map(lambda x: decimation_filter_output_offset[x], df.rgdec))
+    df["decimation_filter_swath_desc"]=list(map(lambda x: decimation_filter_swath_desc[x], df.rgdec))
+    dfa["decimation_filter_bandwidth"]=list(map(lambda x: decimation_filter_bandwidth[x], dfa.rgdec))
+    dfa["decimation_filter_L"]=list(map(lambda x: decimation_filter_L[x], dfa.rgdec))
+    dfa["decimation_filter_M"]=list(map(lambda x: decimation_filter_M[x], dfa.rgdec))
+    dfa["decimation_filter_length_NF"]=list(map(lambda x: decimation_filter_length_NF[x], dfa.rgdec))
+    dfa["decimation_filter_output_offset"]=list(map(lambda x: decimation_filter_output_offset[x], dfa.rgdec))
+    dfa["decimation_filter_swath_desc"]=list(map(lambda x: decimation_filter_swath_desc[x], dfa.rgdec))
+    print("decimation filter ..")
+    fref=(3.7534721374511715e+1)
+    dfc["fdec"]=((4)*(fref)*(((dfc.decimation_filter_L)/(dfc.decimation_filter_M))))
+    dfc["N3_tx"]=np.ceil(((dfc.fdec)*(dfc.txpl))).astype(np.int)
+    dfc["decimation_filter_B"]=((((2)*(dfc.swl)))-(((dfc.decimation_filter_output_offset)+(17))))
+    dfc["decimation_filter_C"]=((dfc.decimation_filter_B)-(((dfc.decimation_filter_M)*(((dfc.decimation_filter_B)//(dfc.decimation_filter_M))))))
+    dfc["N3_rx"]=list(map(lambda idx_row: ((2)*(((((idx_row[1].decimation_filter_L)*(((idx_row[1].decimation_filter_B)//(idx_row[1].decimation_filter_M)))))+(decimation_filter_D[idx_row[1].rgdec][idx_row[1].decimation_filter_C])+(1)))), dfc.iterrows()))
+    df["fdec"]=((4)*(fref)*(((df.decimation_filter_L)/(df.decimation_filter_M))))
+    df["N3_tx"]=np.ceil(((df.fdec)*(df.txpl))).astype(np.int)
+    df["decimation_filter_B"]=((((2)*(df.swl)))-(((df.decimation_filter_output_offset)+(17))))
+    df["decimation_filter_C"]=((df.decimation_filter_B)-(((df.decimation_filter_M)*(((df.decimation_filter_B)//(df.decimation_filter_M))))))
+    df["N3_rx"]=list(map(lambda idx_row: ((2)*(((((idx_row[1].decimation_filter_L)*(((idx_row[1].decimation_filter_B)//(idx_row[1].decimation_filter_M)))))+(decimation_filter_D[idx_row[1].rgdec][idx_row[1].decimation_filter_C])+(1)))), df.iterrows()))
+    dfa["fdec"]=((4)*(fref)*(((dfa.decimation_filter_L)/(dfa.decimation_filter_M))))
+    dfa["N3_tx"]=np.ceil(((dfa.fdec)*(dfa.txpl))).astype(np.int)
+    dfa["decimation_filter_B"]=((((2)*(dfa.swl)))-(((dfa.decimation_filter_output_offset)+(17))))
+    dfa["decimation_filter_C"]=((dfa.decimation_filter_B)-(((dfa.decimation_filter_M)*(((dfa.decimation_filter_B)//(dfa.decimation_filter_M))))))
+    dfa["N3_rx"]=list(map(lambda idx_row: ((2)*(((((idx_row[1].decimation_filter_L)*(((idx_row[1].decimation_filter_B)//(idx_row[1].decimation_filter_M)))))+(decimation_filter_D[idx_row[1].rgdec][idx_row[1].decimation_filter_C])+(1)))), dfa.iterrows()))
+    # %% get pulse configuration that is rank pri_counts in the past
+    print("pulse from past")
+    dfap=dfa.set_index("pri_count")
+    dfa["ranked_txprr"]=dfap.loc[((dfa.pri_count)-(dfa["rank"]))].reset_index().txprr
+    dfa["ranked_txprr_"]=dfap.loc[((dfa.pri_count)-(dfa["rank"]))].reset_index().txprr_
+    dfa["ranked_txpl"]=dfap.loc[((dfa.pri_count)-(dfa["rank"]))].reset_index().txpl
+    dfa["ranked_txpl_"]=dfap.loc[((dfa.pri_count)-(dfa["rank"]))].reset_index().txpl_
+    dfa["ranked_txpsf"]=dfap.loc[((dfa.pri_count)-(dfa["rank"]))].reset_index().txpsf
+    dfa["ranked_ses_ssb_tx_pulse_number"]=dfap.loc[((dfa.pri_count)-(dfa["rank"]))].reset_index().ses_ssb_tx_pulse_number
+    dfa.to_csv("dfa.csv")
+    df.to_csv("df.csv")
+    dfc.to_csv("dfc.csv")
 print("start mmap ..")
 s=np.memmap(next(pathlib.Path("./").glob("o_cal*.cf")), dtype=np.complex64, mode="r", shape=(800,6000,))
 ss=np.memmap(next(pathlib.Path("./").glob("o_r*.cf")), dtype=np.complex64, mode="r", offset=((4)*(2)*(24890)*(10800)), shape=(7400,24890,))
