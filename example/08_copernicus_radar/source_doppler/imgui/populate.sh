2eade582161f62dc9cb446d20a1b7cca9b1b13ad
Martin Kielhorn <kielhorn.martin@gmail.com>
2020-01-09 10:49:15 +0100

    code to get imgui

---
+++ example/08_copernicus_radar/source_doppler/imgui/populate.sh
@@ -0,0 +1,3 @@
+for i in "imconfig.h imgui.cpp imgui.h imgui_demo.cpp imgui_draw.cpp examples/imgui_impl_glfw.cpp examples/imgui_impl_glfw.h examples/imgui_impl_opengl2.cpp examples/imgui_impl_opengl2.h imgui_internal.h imgui_widgets.cpp imstb_rectpack.h imstb_textedit.h imstb_truetype.h"; do
+    wget https://raw.githubusercontent.com/ocornut/imgui/master/$i
+done
