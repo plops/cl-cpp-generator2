5049beb4e92f5b2361d625a6ed239e8e1da5e264
Martin Kielhorn <kielhorn.martin@gmail.com>
2020-05-05 13:49:08 +0200

    try cuda raytracer

---
+++ example/12_business_card_ray/gen00.lisp
@@ -0,0 +1,193 @@
+(eval-when (:compile-toplevel :execute :load-toplevel)
+  (ql:quickload "cl-cpp-generator2")
+  (ql:quickload "cl-ppcre"))
+
+(in-package :cl-cpp-generator2)
+
+(setf *features* (union *features* '()))
+(setf *features* (set-difference *features* '()))
+
+
+(progn
+  ;; make sure to run this code twice during the first time, so that
+  ;; the functions are defined
+
+  (defparameter *source-dir* #P"example/12_business_card_ray/source/")
+
+  (progn
+    ;; collect code that will be emitted in utils.h
+    (defparameter *utils-code* nil)
+    (defun emit-utils (&key code)
+      (push code *utils-code*)
+      " "))
+  (progn
+    (defparameter *module-global-parameters* nil)
+    (defparameter *module* nil)
+    (defun logprint (msg &optional rest)
+      `(do0
+	#-nolog
+	(do0
+	 ("std::setprecision" 3)
+	 (<< "std::cout"
+	     ("std::setw" 10)
+	     #+nil (- (dot ("std::chrono::high_resolution_clock::now")
+		     (time_since_epoch)
+		     (count))
+		,(g `_start_time))
+	     (string " ")
+	     __FILE__
+	     (string ":")
+	     __LINE__
+	     (string " ")
+	     __func__
+	     (string " ")
+	     (string ,msg)
+	     (string " ")
+	     ,@(loop for e in rest appending
+		    `(("std::setw" 8)
+					;("std::width" 8)
+		      (string ,(format nil " ~a=" (emit-c :code e)))
+		      ,e))
+	     "std::endl"))))
+    
+    (defun emit-globals (&key init)
+      (let ((l `(#+nil (_start_time ,(emit-c :code `(typeof (dot ("std::chrono::high_resolution_clock::now")
+							   (time_since_epoch)
+							   (count)))))
+		 ,@(loop for e in *module-global-parameters* collect
+			(destructuring-bind (&key name type default)
+			    e
+			  `(,name ,type))))))
+	(if init
+	    `(curly
+	      ,@(remove-if
+		 #'null
+		 (loop for e in l collect
+		      (destructuring-bind (name type &optional value) e
+			(when value
+			  `(= ,(format nil ".~a" (elt (cl-ppcre:split "\\[" (format nil "~a" name)) 0)) ,value))))))
+	    `(do0
+	      (include <chrono>)
+	      (defstruct0 State
+		  ,@(loop for e in l collect
+ 			 (destructuring-bind (name type &optional value) e
+			   `(,name ,type))))))))
+    (defun define-module (args)
+      "each module will be written into a c file with module-name. the global-parameters the module will write to will be specified with their type in global-parameters. a file global.h will be written that contains the parameters that were defined in all modules. global parameters that are accessed read-only or have already been specified in another module need not occur in this list (but can). the prototypes of functions that are specified in a module are collected in functions.h. i think i can (ab)use gcc's warnings -Wmissing-declarations to generate this header. i split the code this way to reduce the amount of code that needs to be recompiled during iterative/interactive development. if the module-name contains vulkan, include vulkan headers. if it contains glfw, include glfw headers."
+      (destructuring-bind (module-name global-parameters module-code) args
+	(let ((header ()))
+	  (push `(do0
+		  " "
+		  (include "utils.h")
+		  " "
+		  (include "globals.h")
+		  " "
+		  (include "proto2.h")
+		  " ")
+		header)
+	  (unless (cl-ppcre:scan "main" (string-downcase (format nil "~a" module-name)))
+	    (push `(do0 "extern State state;")
+		  header))
+	  (push `(:name ,module-name :code (do0 ,@(reverse header) ,module-code))
+		*module*))
+	(loop for par in global-parameters do
+	     (destructuring-bind (parameter-name
+				  &key (direction 'in)
+				  (type 'int)
+				  (default nil)) par
+	       (push `(:name ,parameter-name :type ,type :default ,default)
+		     *module-global-parameters*))))))
+  (defun g (arg)
+    `(dot state ,arg))
+
+
+    
+  (define-module
+      `(cuda_main (
+	      )
+		  (do0
+		   
+	      
+	      (let ((state ,(emit-globals :init t)))
+		(declare (type "State" state)))
+	      
+
+	      "using namespace std::chrono_literals;"
+	      ; "using namespace filament;"
+	      
+	      	      
+	      (defun main ()
+		(declare (values int))
+		(return EXIT_SUCCESS)))))
+
+  
+  
+  (progn
+    (with-open-file (s (asdf:system-relative-pathname 'cl-cpp-generator2
+						 (merge-pathnames #P"proto2.h"
+								  *source-dir*))
+		       :direction :output
+		       :if-exists :supersede
+		       :if-does-not-exist :create)
+      (loop for e in (reverse *module*) and i from 0 do
+	   (destructuring-bind (&key name code) e
+	     (let ((cuda (cl-ppcre:scan "cuda" (string-downcase (format nil "~a" name)))))
+	       (unless cuda
+		(emit-c :code code :hook-defun 
+			#'(lambda (str)
+			    (format s "~a~%" str))))
+	       
+	       (write-source (asdf:system-relative-pathname
+			      'cl-cpp-generator2
+			      (format nil
+				      "~a/nvcut_~2,'0d_~a.~a"
+				      *source-dir* i name
+				      (if cuda
+					  "cu"
+					  "cpp")))
+			     code)))))
+    (write-source (asdf:system-relative-pathname
+		   'cl-cpp-generator2
+		   (merge-pathnames #P"utils.h"
+				    *source-dir*))
+		  `(do0
+		    "#ifndef UTILS_H"
+		    " "
+		    "#define UTILS_H"
+		    " "
+		    (include <iostream>
+			     ;<iomanip>
+			     )
+		    		    
+		    " "
+		    (do0
+		     
+		    " "
+		    ,@(loop for e in (reverse *utils-code*) collect
+			 e)
+		    " "
+		    
+		    )
+		    " "
+		    "#endif"
+		    " "))
+
+    
+    (write-source (asdf:system-relative-pathname 'cl-cpp-generator2 (merge-pathnames
+								     #P"globals.h"
+								     *source-dir*))
+		  `(do0
+		    "#ifndef GLOBALS_H"
+		    " "
+		    "#define GLOBALS_H"
+		    " "
+
+		    " "
+
+		    
+		    " "
+		    ,(emit-globals)
+		    " "
+		    "#endif"
+		    " "))))
+
---
+++ example/12_business_card_ray/source/nvcut_00_cuda_main.cu
@@ -0,0 +1,10 @@
+
+#include "utils.h"
+
+#include "globals.h"
+
+#include "proto2.h"
+;
+State state = {};
+using namespace std::chrono_literals;
+int main() { return EXIT_SUCCESS; };
