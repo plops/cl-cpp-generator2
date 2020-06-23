1ae11bac414537db72592104164c4b9ed0bdeb23
Martin Kielhorn <kielhorn.martin@gmail.com>
2020-06-23 21:53:51 +0200

    it works
    martin@labolg ~/stage/cl-cpp-generator2/example/20_tbb/source $ make
    c++ -Ofast -march=native -Wall -std=gnu++20    -c -o vis_00_main.o vis_00_main.cpp
    ./vic++ -Ofast -march=native -Wall -std=gnu++20  -ltbb -o vis vis_00_main.cpp
    s
    martin@labolg ~/stage/cl-cpp-generator2/example/20_tbb/source $ ./vis
    1592942022726621003 139867523776064 vis_00_main.cpp:64 main start main  state._main_version='18607e22d96984df97ea47782f75aebdccc998ca' state._code_repository='https://github.com/plops/cl-cpp-generator2/tree/master/example/19_nvrtc' state._code_generation_time='21:53:37 of Tuesday, 2020-06-23 (GMT+1)'
    1592942022814273814 139867523776064 vis_00_main.cpp:48 run elapsed time (us)  duration='62778'
    1592942022814508444 139867523776064 vis_00_main.cpp:76 main end main
    martin@labolg ~/stage/cl-cpp-generator2/example/20_tbb/source $ ./vis
    1592942023349450501 140006121631296 vis_00_main.cpp:64 main start main  state._main_version='18607e22d96984df97ea47782f75aebdccc998ca' state._code_repository='https://github.com/plops/cl-cpp-generator2/tree/master/example/19_nvrtc' state._code_generation_time='21:53:37 of Tuesday, 2020-06-23 (GMT+1)'
    1592942023437966370 140006121631296 vis_00_main.cpp:48 run elapsed time (us)  duration='63869'
    1592942023438194127 140006121631296 vis_00_main.cpp:76 main end main
    martin@labolg ~/stage/cl-cpp-generator2/example/20_tbb/source $ ./vis
    1592942023605188421 140655514357312 vis_00_main.cpp:64 main start main  state._main_version='18607e22d96984df97ea47782f75aebdccc998ca' state._code_repository='https://github.com/plops/cl-cpp-generator2/tree/master/example/19_nvrtc' state._code_generation_time='21:53:37 of Tuesday, 2020-06-23 (GMT+1)'
    1592942023692993988 140655514357312 vis_00_main.cpp:48 run elapsed time (us)  duration='63306'
    1592942023693214842 140655514357312 vis_00_main.cpp:76 main end main
    martin@labolg ~/stage/cl-cpp-generator2/example/20_tbb/source $ ./vis
    1592942023829143898 140108110782016 vis_00_main.cpp:64 main start main  state._main_version='18607e22d96984df97ea47782f75aebdccc998ca' state._code_repository='https://github.com/plops/cl-cpp-generator2/tree/master/example/19_nvrtc' state._code_generation_time='21:53:37 of Tuesday, 2020-06-23 (GMT+1)'
    1592942023917772088 140108110782016 vis_00_main.cpp:48 run elapsed time (us)  duration='63917'
    1592942023917993573 140108110782016 vis_00_main.cpp:76 main end main
    martin@labolg ~/stage/cl-cpp-generator2/example/20_tbb/source $ ./vis
    1592942024045086858 140519016377920 vis_00_main.cpp:64 main start main  state._main_version='18607e22d96984df97ea47782f75aebdccc998ca' state._code_repository='https://github.com/plops/cl-cpp-generator2/tree/master/example/19_nvrtc' state._code_generation_time='21:53:37 of Tuesday, 2020-06-23 (GMT+1)'
    1592942024131621402 140519016377920 vis_00_main.cpp:48 run elapsed time (us)  duration='62243'
    1592942024131833920 140519016377920 vis_00_main.cpp:76 main end main

--- example/20_tbb/source/vis_00_main.cpp
+++ example/20_tbb/source/vis_00_main.cpp
@@ -26,17 +26,34 @@ State state = {};
 auto get_time() { return std::chrono::high_resolution_clock::now(); }
 void run() {
   constexpr int N = 1048576;
-  std::vector<int> v1(N);
-  std::vector<int> v2(N);
-  std::mt19937 rng;
+  auto v1 = std::vector<int>(N);
+  auto v2 = std::vector<int>(N);
+  auto rng = std::mt19937();
   rng.seed((std::random_device())());
   auto dist = std::uniform_int_distribution<int>(0, 255);
+  std::generate(begin(v1), end(v1), [&]() { return dist(rng); });
+  std::generate(begin(v2), end(v2), [&]() { return dist(rng); });
+  auto start = get_time();
+  std::sort(begin(v1), end(v1));
+  std::sort(begin(v2), end(v2));
+  auto end = get_time();
+  auto duration =
+      std::chrono::duration_cast<std::chrono::microseconds>(((end) - (start)))
+          .count();
+
+  (std::cout)
+      << (std::setw(10))
+      << (std::chrono::high_resolution_clock::now().time_since_epoch().count())
+      << (" ") << (std::this_thread::get_id()) << (" ") << (__FILE__) << (":")
+      << (__LINE__) << (" ") << (__func__) << (" ") << ("elapsed time (us)")
+      << (" ") << (std::setw(8)) << (" duration='") << (duration) << ("'")
+      << (std::endl) << (std::flush);
 }
 int main() {
-  state._main_version = "fca99dd50b4490add292d996046f986de347e618";
+  state._main_version = "18607e22d96984df97ea47782f75aebdccc998ca";
   state._code_repository =
       "https://github.com/plops/cl-cpp-generator2/tree/master/example/19_nvrtc";
-  state._code_generation_time = "21:48:12 of Tuesday, 2020-06-23 (GMT+1)";
+  state._code_generation_time = "21:53:37 of Tuesday, 2020-06-23 (GMT+1)";
   state._start_time =
       std::chrono::high_resolution_clock::now().time_since_epoch().count();
 
