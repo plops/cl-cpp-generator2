3231d5fdfafad61f5a10b1b524f26c9e54e3d888
Martin Kielhorn <kielhorn.martin@gmail.com>
2020-06-24 08:52:09 +0200

    02 cuda device

---
+++ example/19_nvrtc/source/vis_02_cu_A_device.hpp
@@ -0,0 +1,40 @@
+#ifndef VIS_02_CU_A_DEVICE_H
+#define VIS_02_CU_A_DEVICE_H
+#include "utils.h"
+;
+#include "globals.h"
+;
+#include <cuda.h>
+#include <cuda_runtime.h>
+;
+#include "vis_02_cu_A_device.hpp"
+;
+class CudaDeviceProperties  {
+            cudaDeviceProp _props ;
+        explicit  CudaDeviceProperties (const cudaDeviceProp& props)  ;  
+        public:
+         CudaDeviceProperties (int device)  ;  
+        static CudaDeviceProperties FromExistingProperties (const cudaDeviceProp& props)  ;  
+        static CudaDeviceProperties ByIntegratedType (bool integrated)  ;  
+        const auto& getRawStruct () const ;  
+        int major () const ;  
+        int minor () const ;  
+        bool integrated () const ;  
+        const char* name () const ;  
+};
+class CudaDevice  {
+            int _device ;
+    CudaDeviceProperties _props ;
+        public:
+        explicit  CudaDevice (int device)  ;  
+        inline CUdevice handle () const ;  
+        static CudaDevice FindByProperties (const CudaDeviceProperties& props)  ;  
+        static int NumberOfDevices ()  ;  
+        void setAsCurrent ()  ;  
+        const auto & properties () const ;  
+        const char* name () const ;  
+        static CudaDevice FindByName (std::string name)  ;  
+        static std::vector<CudaDevice> EnumerateDevices ()  ;  
+        static CudaDevice CurrentDevice ()  ;  
+};
+#endif
