043ad2cda851046249e1c5524b4c0f94dd29aaae
Martin Kielhorn <kielhorn.martin@gmail.com>
2020-06-24 08:45:10 +0200

    code

---
+++ example/19_nvrtc/source/vis_01_cu_A_rtc_code.cpp
@@ -0,0 +1,31 @@
+
+#include "utils.h"
+
+#include "globals.h"
+
+;
+extern State state;
+#include "vis_01_cu_A_rtc_code.hpp"
+#include <fstream>
+#include <streambuf>
+#include <string>
+// Code c{ <params> };  .. initialize
+// Code c = Code::FromFile(fname);  .. load contents of file
+// auto& code = c.code() .. get reference to internal string
+;
+template <typename... ARGS>
+Code::Code(ARGS &&... args) : _code(std::forward<ARGS>(args)...) {}
+Code Code::FromFile(const std::string &name) {
+  auto input = std::ifstream(name);
+  if (!(input.good())) {
+    throw std::runtime_error("can't read file");
+  };
+  input.seekg(0, std::ios::end);
+  std::string str;
+  str.reserve(input.tellg());
+  input.seekg(0, std::ios::beg);
+  str.assign(std::istreambuf_iterator<char>(input),
+             std::istreambuf_iterator<char>());
+  return Code{std::move(str)};
+}
+const auto &Code::code() const { return _code; };
---
+++ example/19_nvrtc/source/vis_01_cu_A_rtc_code.hpp
@@ -0,0 +1,20 @@
+#ifndef VIS_01_CU_A_RTC_CODE_H
+#define VIS_01_CU_A_RTC_CODE_H
+#include "utils.h"
+;
+#include "globals.h"
+;
+#include <string>
+#include <fstream>
+#include <streambuf>
+;
+#include "vis_01_cu_A_rtc_code.hpp"
+;
+class Code  {
+            const std::string _code ;
+        public:
+        template<typename... ARGS> explicit  Code (ARGS&& ...args)  ;  
+        static Code FromFile (const std::string& name)  ;  
+        const auto& code () const ;  
+};
+#endif
