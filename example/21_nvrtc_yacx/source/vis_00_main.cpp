b3853afbe618a2435612527d232d33a535513f97
Martin Kielhorn <kielhorn.martin@gmail.com>
2020-06-27 15:03:45 +0200

    add generated source

---
+++ example/21_nvrtc_yacx/source/vis_00_main.cpp
@@ -0,0 +1,59 @@
+
+#include "utils.h"
+
+#include "globals.h"
+
+;
+// g++ -march=native -Ofast --std=gnu++20 vis_00_main.cpp
+// -I/media/sdb4/cuda/11.0.1/include/ -L /media/sdb4/cuda/11.0.1/lib -lcudart
+// -lcuda
+#include <cassert>
+#include <chrono>
+#include <cstdio>
+#include <fstream>
+#include <iostream>
+#include <string>
+#include <thread>
+#include <vector>
+
+#include <cuda.h>
+#include <cuda_runtime.h>
+#include <nvrtc.h>
+
+#include <yacx/main.hpp>
+
+using namespace std::chrono_literals;
+State state = {};
+int main() {
+  state._main_version = "d99031a25c898c0dffdc86d9c73344ca2e2fd8ad";
+  state._code_repository =
+      "https://github.com/plops/cl-cpp-generator2/tree/master/example/19_nvrtc";
+  state._code_generation_time = "15:03:28 of Saturday, 2020-06-27 (GMT+1)";
+  state._start_time =
+      std::chrono::high_resolution_clock::now().time_since_epoch().count();
+
+  (std::cout)
+      << (std::setw(10))
+      << (std::chrono::high_resolution_clock::now().time_since_epoch().count())
+      << (" ") << (std::this_thread::get_id()) << (" ") << (__FILE__) << (":")
+      << (__LINE__) << (" ") << (__func__) << (" ") << ("start main") << (" ")
+      << (std::setw(8)) << (" state._main_version='") << (state._main_version)
+      << ("'") << (std::setw(8)) << (" state._code_repository='")
+      << (state._code_repository) << ("'") << (std::setw(8))
+      << (" state._code_generation_time='") << (state._code_generation_time)
+      << ("'") << (std::endl) << (std::flush);
+  auto dev = CudaDevice::FindByProperties(
+      CudaDeviceProperties::ByIntegratedType(false));
+  dev.setAsCurrent();
+  auto ctx = CudaContext(dev);
+  auto code = Code::FromFile("bla.cu");
+  auto program = Program("myprog", code);
+
+  (std::cout)
+      << (std::setw(10))
+      << (std::chrono::high_resolution_clock::now().time_since_epoch().count())
+      << (" ") << (std::this_thread::get_id()) << (" ") << (__FILE__) << (":")
+      << (__LINE__) << (" ") << (__func__) << (" ") << ("end main") << (" ")
+      << (std::endl) << (std::flush);
+  return 0;
+};
---
+++ example/21_nvrtc_yacx/source/vis_00_main.hpp
@@ -0,0 +1,23 @@
+#ifndef VIS_00_MAIN_H
+#define VIS_00_MAIN_H
+#include "utils.h"
+;
+#include "globals.h"
+;
+#include <iostream>
+#include <chrono>
+#include <cstdio>
+#include <cassert>
+#include <string>
+#include <fstream>
+#include <thread>
+#include <vector>
+;
+#include <nvrtc.h>
+#include <cuda.h>
+#include <cuda_runtime.h>
+;
+#include <yacx/main.hpp>
+;
+int main ()  ;  
+#endif
