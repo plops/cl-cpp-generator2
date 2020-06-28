2a5ac73f13e7bcd7e1a2a0ca2386fc54146c7d65
Martin Kielhorn <kielhorn.martin@gmail.com>
2020-06-28 08:57:12 +0200

    start with a lmdb example

---
+++ example/22_lmdb/source/vis_00_main.cpp
@@ -0,0 +1,56 @@
+
+#include "utils.h"
+
+#include "globals.h"
+
+;
+// g++ -march=native -Ofast --std=gnu++20 vis_00_main.cpp
+// -I/media/sdb4/cuda/11.0.1/include/ -L /media/sdb4/cuda/11.0.1/lib -lcudart
+// -lcuda
+#include <cassert>
+#include <chrono>
+#include <cstdio>
+#include <iostream>
+
+using namespace std::chrono_literals;
+State state = {};
+int main(int argc, char const *const *const argv) {
+  state._main_version = "d3843e8c4f9afd0b2297d0add586c0590bba986c";
+  state._code_repository =
+      "https://github.com/plops/cl-cpp-generator2/tree/master/example/19_nvrtc";
+  state._code_generation_time = "08:56:59 of Sunday, 2020-06-28 (GMT+1)";
+  state._start_time =
+      std::chrono::high_resolution_clock::now().time_since_epoch().count();
+
+  (std::cout)
+      << (std::setw(10))
+      << (std::chrono::high_resolution_clock::now().time_since_epoch().count())
+      << (" ") << (std::this_thread::get_id()) << (" ") << (__FILE__) << (":")
+      << (__LINE__) << (" ") << (__func__) << (" ") << ("start main") << (" ")
+      << (std::setw(8)) << (" state._main_version='") << (state._main_version)
+      << ("'") << (std::setw(8)) << (" state._code_repository='")
+      << (state._code_repository) << ("'") << (std::setw(8))
+      << (" state._code_generation_time='") << (state._code_generation_time)
+      << ("'") << (std::endl) << (std::flush);
+  try {
+    ;
+  } catch (const std::exception &e) {
+
+    (std::cout) << (std::setw(10))
+                << (std::chrono::high_resolution_clock::now()
+                        .time_since_epoch()
+                        .count())
+                << (" ") << (std::this_thread::get_id()) << (" ") << (__FILE__)
+                << (":") << (__LINE__) << (" ") << (__func__) << (" ")
+                << ("error") << (" ") << (std::setw(8)) << (" e.what()='")
+                << (e.what()) << ("'") << (std::endl) << (std::flush);
+  };
+
+  (std::cout)
+      << (std::setw(10))
+      << (std::chrono::high_resolution_clock::now().time_since_epoch().count())
+      << (" ") << (std::this_thread::get_id()) << (" ") << (__FILE__) << (":")
+      << (__LINE__) << (" ") << (__func__) << (" ") << ("end main") << (" ")
+      << (std::endl) << (std::flush);
+  return 0;
+};
---
+++ example/22_lmdb/source/vis_00_main.hpp
@@ -0,0 +1,13 @@
+#ifndef VIS_00_MAIN_H
+#define VIS_00_MAIN_H
+#include "utils.h"
+;
+#include "globals.h"
+;
+#include <iostream>
+#include <chrono>
+#include <cstdio>
+#include <cassert>
+;
+int main (int argc, char const *const *const argv)  ;  
+#endif
