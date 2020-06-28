807def34d89d3198dbc2bc76507ff37e5079c273
Martin Kielhorn <kielhorn.martin@gmail.com>
2020-06-28 22:12:18 +0200

    include

---
+++ example/23_lockfree_queue/source/rwqueue/populate.sh
@@ -0,0 +1,2 @@
+wget https://raw.githubusercontent.com/cameron314/readerwriterqueue/master/atomicops.h
+wget https://raw.githubusercontent.com/cameron314/readerwriterqueue/master/readerwriterqueue.h
