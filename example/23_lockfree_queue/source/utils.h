435598615c67be613526572ee0ac2bbef3e05b48
Martin Kielhorn <kielhorn.martin@gmail.com>
2020-06-28 21:49:47 +0200

    add boilerplate

---
+++ example/23_lockfree_queue/source/utils.h
@@ -0,0 +1,12 @@
+#ifndef UTILS_H
+
+#define UTILS_H
+
+#include <array>
+#include <iomanip>
+#include <iostream>
+#include <vector>
+
+;
+
+#endif
---
+++ example/23_lockfree_queue/source/vis_00_main.cpp
@@ -0,0 +1,56 @@
+
+#include "utils.h"
+
+#include "globals.h"
+
+;
+#include <cassert>
+#include <chrono>
+#include <cstdio>
+#include <iostream>
+#include <thread>
+
+#include "lmdbpp/lmdb++.h"
+
+using namespace std::chrono_literals;
+State state = {};
+int main(int argc, char const *const *const argv) {
+  state._main_version = "61d5b9ffe0139a3774e65b1bb94190b42888ca7b";
+  state._code_repository =
+      "https://github.com/plops/cl-cpp-generator2/tree/master/example/19_nvrtc";
+  state._code_generation_time = "21:49:36 of Sunday, 2020-06-28 (GMT+1)";
+  state._start_time =
+      std::chrono::high_resolution_clock::now().time_since_epoch().count();
+
+  (std::cout)
+      << (std::setw(10))
+      << (std::chrono::high_resolution_clock::now().time_since_epoch().count())
+      << (" ") << (std::this_thread::get_id()) << (" ") << (__FILE__) << (":")
+      << (__LINE__) << (" ") << (__func__) << (" ") << ("start main") << (" ")
+      << (std::setw(8)) << (" state._main_version='") << (state._main_version)
+      << ("'") << (std::setw(8)) << (" state._code_repository='")
+      << (state._code_repository) << ("'") << (std::setw(8))
+      << (" state._code_generation_time='") << (state._code_generation_time)
+      << ("'") << (std::endl) << (std::flush);
+  try {
+    ;
+  } catch (const std::exception &e) {
+
+    (std::cout) << (std::setw(10))
+                << (std::chrono::high_resolution_clock::now()
+                        .time_since_epoch()
+                        .count())
+                << (" ") << (std::this_thread::get_id()) << (" ") << (__FILE__)
+                << (":") << (__LINE__) << (" ") << (__func__) << (" ")
+                << ("error") << (" ") << (std::setw(8)) << (" e.what()='")
+                << (e.what()) << ("'") << (std::endl) << (std::flush);
+  };
+
+  (std::cout)
+      << (std::setw(10))
+      << (std::chrono::high_resolution_clock::now().time_since_epoch().count())
+      << (" ") << (std::this_thread::get_id()) << (" ") << (__FILE__) << (":")
+      << (__LINE__) << (" ") << (__func__) << (" ") << ("end main") << (" ")
+      << (std::endl) << (std::flush);
+  return 0;
+};
