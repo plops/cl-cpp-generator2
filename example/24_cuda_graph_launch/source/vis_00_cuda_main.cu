1241fd794521009e31113c7f79c2bdc52a63d548
Martin Kielhorn <kielhorn.martin@gmail.com>
2020-07-05 12:47:48 +0200

    another cuda test

---
+++ example/24_cuda_graph_launch/source/vis_00_cuda_main.cu
@@ -0,0 +1,52 @@
+
+#include "utils.h"
+
+#include "globals.h"
+
+;
+// g++ -march=native -Ofast --std=gnu++20 vis_00_main.cpp
+// -I/media/sdb4/cuda/11.0.1/include/ -L /media/sdb4/cuda/11.0.1/lib -lcudart
+// -lcuda
+#include <algorithm>
+#include <cassert>
+#include <chrono>
+#include <cstdio>
+#include <experimental/iterator>
+#include <fstream>
+#include <iostream>
+#include <string>
+#include <thread>
+#include <vector>
+
+#include <cuda.h>
+#include <cuda_runtime.h>
+
+using namespace std::chrono_literals;
+State state = {};
+int main(int argc, char const *const *const argv) {
+  state._main_version = "2b18b4f0796d0c2e05322bfae91210158241bed9";
+  state._code_repository =
+      "https://github.com/plops/cl-cpp-generator2/tree/master/example/19_nvrtc";
+  state._code_generation_time = "12:47:33 of Sunday, 2020-07-05 (GMT+1)";
+  state._start_time =
+      std::chrono::high_resolution_clock::now().time_since_epoch().count();
+
+  (std::cout)
+      << (std::setw(10))
+      << (std::chrono::high_resolution_clock::now().time_since_epoch().count())
+      << (" ") << (std::this_thread::get_id()) << (" ") << (__FILE__) << (":")
+      << (__LINE__) << (" ") << (__func__) << (" ") << ("start main") << (" ")
+      << (std::setw(8)) << (" state._main_version='") << (state._main_version)
+      << ("'") << (std::setw(8)) << (" state._code_repository='")
+      << (state._code_repository) << ("'") << (std::setw(8))
+      << (" state._code_generation_time='") << (state._code_generation_time)
+      << ("'") << (std::endl) << (std::flush);
+
+  (std::cout)
+      << (std::setw(10))
+      << (std::chrono::high_resolution_clock::now().time_since_epoch().count())
+      << (" ") << (std::this_thread::get_id()) << (" ") << (__FILE__) << (":")
+      << (__LINE__) << (" ") << (__func__) << (" ") << ("end main") << (" ")
+      << (std::endl) << (std::flush);
+  return 0;
+};
