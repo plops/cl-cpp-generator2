11c3540f8d082bc82145be251efc7892115e0512
Martin Kielhorn <kielhorn.martin@gmail.com>
2020-07-30 18:59:29 +0200

    start adding protobuf

---
+++ example/29_stm32nucleo/source/nanopb/populate.sh
@@ -0,0 +1,4 @@
+wget https://raw.githubusercontent.com/nanopb/nanopb/master/pb_common.c
+#wget https://raw.githubusercontent.com/nanopb/nanopb/master/pb_common.h
+wget https://raw.githubusercontent.com/nanopb/nanopb/master/pb_encode.c
+wget https://raw.githubusercontent.com/nanopb/nanopb/master/pb_decode.c
