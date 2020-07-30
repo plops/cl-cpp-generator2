9bb26e78df50ba04d119315583da88f2a684122a
Martin Kielhorn <kielhorn.martin@gmail.com>
2020-07-30 19:16:26 +0200

    eclipse seems clever enough to include the protobuf files into the
    build after i copy them to Core/Src and Core/Inc

---
+++ example/29_stm32nucleo/source/copy_protobuf.sh
@@ -0,0 +1,3 @@
+cp /home/martin/stage/cl-cpp-generator2/example/29_stm32nucleo/source/simple.pb.{c,h} \
+   /home/martin/stage/cl-cpp-generator2/example/29_stm32nucleo/source/nanopb/*.{c,h} \
+   /home/martin/STM32CubeIDE/workspace_1.4.0/nucleo_l476rg_dual_adc_dac/Core/Src
