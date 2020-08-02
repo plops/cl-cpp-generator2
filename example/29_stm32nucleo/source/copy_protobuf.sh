be0476bcc469ba1a8871e659d61042dc551b52e1
Martin Kielhorn <kielhorn.martin@gmail.com>
2020-08-02 14:43:30 +0200

    start adding nanopb code to qt project

--- example/29_stm32nucleo/source/copy_protobuf.sh
+++ example/29_stm32nucleo/source/copy_protobuf.sh
@@ -4,3 +4,9 @@ cp /home/martin/stage/cl-cpp-generator2/example/29_stm32nucleo/source/simple.pb.
 
 cp /home/martin/stage/cl-cpp-generator2/example/29_stm32nucleo/source/nanopb/*.h \
     /home/martin/STM32CubeIDE/workspace_1.4.0/nucleo_l476rg_dual_adc_dac/Core/Inc
+
+
+cp /home/martin/stage/cl-cpp-generator2/example/29_stm32nucleo/source/simple.pb.{c,h} \
+   /home/martin/stage/cl-cpp-generator2/example/29_stm32nucleo/source/nanopb/*.c \
+   /home/martin/stage/cl-cpp-generator2/example/29_stm32nucleo/source/nanopb/*.h \
+   /home/martin/stage/cl-cpp-generator2/example/29_stm32nucleo/source1/
