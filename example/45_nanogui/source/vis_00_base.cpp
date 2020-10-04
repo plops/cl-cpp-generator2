3bc68608948534c2119a7ce931a5fe8458d91c81
Martin Kielhorn <kielhorn.martin@gmail.com>
2020-10-04 16:47:29 +0200

    more code for nanogui

---
+++ example/45_nanogui/source/vis_00_base.cpp
@@ -0,0 +1,24 @@
+
+#include "utils.h"
+
+#include "globals.h"
+
+extern State state;
+#include <chrono>
+#include <iostream>
+#include <nanogui/nanogui.h>
+#include <thread>
+
+// implementation
+std::vector<char> buffer(20 * 1024);
+int main(int argc, char **argv) {
+
+  (std::cout)
+      << (std::setw(10))
+      << (std::chrono::high_resolution_clock::now().time_since_epoch().count())
+      << (" ") << (std::this_thread::get_id()) << (" ") << (__FILE__) << (":")
+      << (__LINE__) << (" ") << (__func__) << (" ") << ("start") << (" ")
+      << (std::setw(8)) << (" argc='") << (argc) << ("'") << (std::setw(8))
+      << (" argv[0]='") << (argv[0]) << ("'") << (std::endl) << (std::flush);
+  return 0;
+}
