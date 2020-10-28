a8c5c5cfbe7e3fde42b7e14dd9b2432a18dc5ae5
martin <martin@localhost.localdomain>
2020-10-28 18:41:16 +0100

    start terminal image viewer

---
+++ example/46_terminal_img/source/vis_00_base.cpp
@@ -0,0 +1,24 @@
+
+#include "utils.h"
+
+#include "globals.h"
+
+extern State state;
+#include "vis_01_message.hpp"
+#include <chrono>
+#include <iostream>
+#include <thread>
+using namespace std::chrono_literals;
+
+// implementation
+int main(int argc, char **argv) {
+
+  (std::cout)
+      << (std::setw(10))
+      << (std::chrono::high_resolution_clock::now().time_since_epoch().count())
+      << (" ") << (std::this_thread::get_id()) << (" ") << (__FILE__) << (":")
+      << (__LINE__) << (" ") << (__func__) << (" ") << ("start") << (" ")
+      << (std::setw(8)) << (" argc='") << (argc) << ("'") << (std::setw(8))
+      << (" argv[0]='") << (argv[0]) << ("'") << (std::endl) << (std::flush);
+  return 0;
+}
---
+++ example/46_terminal_img/source/vis_00_base.hpp
@@ -0,0 +1,16 @@
+#ifndef VIS_00_BASE_H
+#define VIS_00_BASE_H
+#include "utils.h"
+;
+#include "globals.h"
+;
+#include <chrono>
+#include <iostream>
+#include <thread>
+;
+#include "vis_01_message.hpp"
+;
+// header
+;
+int main(int argc, char **argv);
+#endif
