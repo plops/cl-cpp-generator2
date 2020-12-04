75049c5d5fc6c62f22e482a464cedf9c7bce6779
Martin Kielhorn <kielhorn.martin@gmail.com>
2020-12-04 21:40:57 +0100

    add cmake file

---
+++ example/49_pybind/source/vis_00_base.cpp
@@ -0,0 +1,40 @@
+
+#include "utils.h"
+
+#include "globals.h"
+
+extern State state;
+#include <chrono>
+#include <iostream>
+#include <pybind11/embed.h>
+#include <thread>
+using namespace std::chrono_literals;
+
+// implementation
+State state = {};
+int main(int argc, char **argv) {
+  state._main_version = "208a658f25fb56beee9f50f23298edfdb2be096b";
+  state._code_repository = "https://github.com/plops/cl-cpp-generator2/tree/"
+                           "master/example/48_future";
+  state._code_generation_time = "21:40:41 of Friday, 2020-12-04 (GMT+1)";
+  state._start_time =
+      std::chrono::high_resolution_clock::now().time_since_epoch().count();
+  {
+
+    auto lock = std::unique_lock<std::mutex>(state._stdout_mutex);
+    (std::cout) << (std::setw(10))
+                << (std::chrono::high_resolution_clock::now()
+                        .time_since_epoch()
+                        .count())
+                << (" ") << (std::this_thread::get_id()) << (" ") << (__FILE__)
+                << (":") << (__LINE__) << (" ") << (__func__) << (" ")
+                << ("start main") << (" ") << (std::setw(8))
+                << (" state._main_version='") << (state._main_version) << ("'")
+                << (std::setw(8)) << (" state._code_repository='")
+                << (state._code_repository) << ("'") << (std::setw(8))
+                << (" state._code_generation_time='")
+                << (state._code_generation_time) << ("'") << (std::endl)
+                << (std::flush);
+  }
+  return 0;
+}
