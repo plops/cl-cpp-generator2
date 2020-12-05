ad222b2c7559a0febb80d0cca7e9bd1613f7cf1a
Martin Kielhorn <kielhorn.martin@gmail.com>
2020-12-05 20:27:59 +0100

    more code

---
+++ example/50_pybind_cgal/source/vis_01_mesher_module.cpp
@@ -0,0 +1,24 @@
+
+#include "utils.h"
+
+#include "globals.h"
+
+extern State state;
+#include <chrono>
+#include <iostream>
+#include <thread>
+
+// implementation
+#include "vis_01_mesher_module.hpp"
+using K = CGAL::Exact_predicates_inexact_constructions_kernel;
+using Fb = CGAL::Delaunay_mesh_face_base_2<K>;
+using Vb = CGAL::Delaunay_mesh_vertex_base_2<K>;
+using Tds = CGAL::Triangulation_data_structure_2<Vb, Fb>;
+using CDT = CGAL::Constrained_Delaunay_triangulation_2<K, Tds>;
+using Criteria = CGAL::Delaunay_mesh_size_criteria_2<CDT>;
+using Mesher = CGAL::Delaunay_mesher_2<CDT, Criteria>;
+using Vertex_handle = CDT::Vertex_handle;
+using Point = CDT::Point;
+using namespace std::chrono_literals;
+
+PYBIND11_MODULE(cgal_mesher, m){};
---
+++ example/50_pybind_cgal/source/vis_01_mesher_module.hpp
@@ -0,0 +1,47 @@
+#ifndef VIS_01_MESHER_MODULE_H
+#define VIS_01_MESHER_MODULE_H
+#include "utils.h"
+;
+#include "globals.h"
+;
+#include <chrono>
+#include <iostream>
+#include <thread>
+;
+#include <pybind11/pybind11.h>
+;
+#include <CGAL/Exact_predicates_inexact_constructions_kernel.h>
+;
+#include <CGAL/Delaunay_mesh_face_base_2.h>
+;
+#include <CGAL/Delaunay_mesh_vertex_base_2.h>
+;
+#include <CGAL/Constrained_Delaunay_triangulation_2.h>
+;
+#include <CGAL/Delaunay_mesh_size_criteria_2.h>
+;
+#include <CGAL/Delaunay_mesher_2.h>
+;
+#include <CGAL/Triangulation_conformer_2.h>
+;
+#include <CGAL/lloyd_optimize_mesh_2.h>
+;
+// header
+#include <CGAL/Exact_predicates_inexact_constructions_kernel.h>
+#include <pybind11/pybind11.h>
+
+#include <CGAL/Delaunay_mesh_face_base_2.h>
+
+#include <CGAL/Delaunay_mesh_vertex_base_2.h>
+
+#include <CGAL/Constrained_Delaunay_triangulation_2.h>
+
+#include <CGAL/Delaunay_mesh_size_criteria_2.h>
+
+#include <CGAL/Delaunay_mesher_2.h>
+
+#include <CGAL/Triangulation_conformer_2.h>
+
+#include <CGAL/lloyd_optimize_mesh_2.h>
+;
+#endif
