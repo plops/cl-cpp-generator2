b0305709c3b8603d7d09f66e76124747dbef5e2a
Martin Kielhorn <kielhorn.martin@gmail.com>
2020-12-11 02:01:40 +0100

    sdl code works native, draws into window.
    i just need to pause

--- example/53_emscripten/source/vis_00_base.cpp
+++ example/53_emscripten/source/vis_00_base.cpp
@@ -18,10 +18,10 @@ using namespace std::chrono_literals;
 
 State state = {};
 int main(int argc, char **argv) {
-  state._main_version = "42910754b58e4911f88bf6264ac3ed86f7180305";
+  state._main_version = "bb33a36076175b8d21328cdc235c88e75c9fad03";
   state._code_repository = "https://github.com/plops/cl-cpp-generator2/tree/"
                            "master/example/53_emscripten/source/";
-  state._code_generation_time = "01:52:03 of Friday, 2020-12-11 (GMT+1)";
+  state._code_generation_time = "02:01:04 of Friday, 2020-12-11 (GMT+1)";
   state._start_time =
       std::chrono::high_resolution_clock::now().time_since_epoch().count();
   {
@@ -48,5 +48,34 @@ int main(int argc, char **argv) {
   auto renderer = static_cast<SDL_Renderer *>(nullptr);
   SDL_CreateWindowAndRenderer(width, height, SDL_WINDOW_OPENGL, &window,
                               &renderer);
+  enum { MAX_ITER_COUNT = 256 };
+  SDL_Color palette[MAX_ITER_COUNT];
+  for (auto i = 0; (i) < (MAX_ITER_COUNT); (i) += (1)) {
+    palette[i] = {.r = uint8_t(rand()),
+                  .g = uint8_t(rand()),
+                  .b = uint8_t(rand()),
+                  .a = 255};
+  }
+  auto center = std::complex<double>((0.50), (0.50));
+  auto scale = (4.0);
+  for (auto y = 0; (y) < (height); (y) += (1)) {
+    for (auto x = 0; (x) < (width); (x) += (1)) {
+      auto point = std::complex<double>((((((1.0)) * (x))) / (width)),
+                                        (((((1.0)) * (y))) / (height)));
+      auto c = std::complex<double>(((((point) - (center))) * (scale)));
+      auto z = std::complex<double>((0.), (0.));
+      auto i = 0;
+      for (; (i) < (((MAX_ITER_COUNT) - (1))); (i)++) {
+        z = ((((z) * (z))) + (c));
+        if (((2.0f)) < (abs(z))) {
+          break;
+        }
+      }
+      auto color = palette[i];
+      SDL_SetRenderDrawColor(renderer, color.r, color.g, color.b, color.a);
+      SDL_RenderDrawPoint(renderer, x, y);
+    }
+  }
+  SDL_RenderPresent(renderer);
   return 0;
 }
