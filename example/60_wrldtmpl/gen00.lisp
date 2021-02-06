d5e4e9e4caef75107edae86739603b7f535d64b3
Martin Kielhorn <kielhorn.martin@gmail.com>
2021-02-06 11:57:07 +0100

    add cl error

--- example/60_wrldtmpl/gen00.lisp
+++ example/60_wrldtmpl/gen00.lisp
@@ -1162,7 +1162,7 @@
 					       (return (s.good))))
 			       ))
 		       )))
-
+    
     (define-module
 	`(cl_buffer ()
 		    (do0
@@ -1830,6 +1830,33 @@
 		       "public:"
 		       "inline static bool candoInterop = false;"
 		       ))))
+
+    (define-module
+	`(cl_kernel ()
+		    (do0
+		     (split-header-and-code
+		      (do0 (comments "header")
+			   "#define CHECKCL(r) CheckCL(r, __FILE__, __LINE__ )"
+			   )
+		      (do0 (comments "implementation")
+			   
+			   ))
+		     (defun CheckCL (result file line)
+		       (declare (type cl_int result)
+				(type "const char*" file)
+				(type int line)
+				(values bool))
+		       (case result
+			 (CL_SUCCESS (return true))
+			 ,@(loop for e in `(DEVICE_NOT_FOUND DEVICE_NOT_AVAILABLE COMPILER_NOT_AVAILABLE MEM_OBJECT_ALLOCATION_FAILURE OUT_OF_RESOURCES OUT_OF_HOST_MEMORY PROFILING_INFO_NOT_AVAILABLE MEM_COPY_OVERLAP IMAGE_FORMAT_MISMATCH IMAGE_FORMAT_NOT_SUPPORTED BUILD_PROGRAM_FAILURE MAP_FAILURE MISALIGNED_SUB_BUFFER_OFFSET EXEC_STATUS_ERROR_FOR_EVENTS_IN_WAIT_LIST INVALID_VALUE INVALID_DEVICE_TYPE INVALID_PLATFORM INVALID_DEVICE INVALID_CONTEXT INVALID_QUEUE_PROPERTIES INVALID_COMMAND_QUEUE INVALID_HOST_PTR INVALID_MEM_OBJECT INVALID_IMAGE_FORMAT_DESCRIPTOR INVALID_IMAGE_SIZE INVALID_SAMPLER INVALID_BINARY INVALID_BUILD_OPTIONS INVALID_PROGRAM INVALID_PROGRAM_EXECUTABLE INVALID_KERNEL_NAME INVALID_KERNEL_DEFINITION INVALID_KERNEL INVALID_ARG_INDEX INVALID_ARG_VALUE INVALID_ARG_SIZE INVALID_KERNEL_ARGS INVALID_WORK_DIMENSION INVALID_WORK_GROUP_SIZE INVALID_WORK_ITEM_SIZE INVALID_GLOBAL_OFFSET INVALID_EVENT_WAIT_LIST INVALID_EVENT INVALID_OPERATION INVALID_GL_OBJECT INVALID_BUFFER_SIZE INVALID_MIP_LEVEL INVALID_GLOBAL_WORK_SIZE)
+				 collect
+				 `(,(format nil "CL_~a" e)
+				   ,(logprint (format nil "CL error: ~a" e)
+					      `(file line))))
+			
+			 )
+		       (return false))
+		   )))
     )
   
   (progn
