e051e14d73c55a3640c8f6674b22120f05e0eb1c
Martin Kielhorn <kielhorn.martin@gmail.com>
2021-12-24 20:38:12 +0100

    sample

--- example/61_cuda_code/gen00.lisp
+++ example/61_cuda_code/gen00.lisp
@@ -83,7 +83,7 @@
 			(const)
 			(values "__host__ __device__ bool"))
 		       (return
-			 (&& ,@(loop for e in `(x y)
+			 (and ,@(loop for e in `(x y)
 				     appending
 				     `((<= (dot m_p_min ,e)
 					   (dot p ,e))
@@ -120,7 +120,7 @@
 				 (:name set_bounding_box :params (xmin ymin xmax ymax) :param-types (float float float float) :const nil :code  (m_bounding_box.set   xmin ymin xmax ymax) :values "__host__ __device__ __forceinline__ void")
 				 (:name num_points :params () :param-types () :const t :code  (return (- m_end m_begin)) :values "__host__ __device__ __forceinline__ int")
 				 (:name points_begin :params () :param-types () :const t :code  (return m_begin) :values "__host__ __device__ __forceinline__ int")
-				 (:name point_end :params () :param-types () :const t :code  (return m_end) :values "__host__ __device__ __forceinline__ int")
+				 (:name points_end :params () :param-types () :const t :code  (return m_end) :values "__host__ __device__ __forceinline__ int")
 				 (:name set_range :params (begin end) :param-types (int int) :const nil :code  (setf m_begin begin
 														     m_end end)
 				  :values "__host__ __device__ __forceinline__ void"))
@@ -137,11 +137,12 @@
 				    )
 			   ,code
 			   ))))
-	     (defclass Parameters ()
+	    (defclass Parameters ()
+	       "public:"
 	       "int m_point_selector, m_num_nodes_at_this_level, m_depth;"
 	       "const int m_max_depth, m_min_points_per_node;"
 	       
-	      "public:"
+	       
 	      (defmethod Parameters (max_depth min_points_per_node)
 		(declare
 		 (type int max_depth min_points_per_node)
@@ -156,12 +157,49 @@
 		 (type "const Parameters&" params)
 		 (type bool flag)
 		 (values "__host__ __device__")
-		 (construct (m_point_selector (% (+ params.point_selector 1)
+		 (construct (m_point_selector (% (+ params.m_point_selector 1)
 						 2))
-			    (m_num_nodes_at_this_level (* 4 params.num_nodes_at_this_level))
-			    (m_depth (+ 1 params.depth))
-			    (m_max_depth params.max_depth)
-			    (m_min_points_per_node params.min_points_per_node))))
+			    (m_num_nodes_at_this_level (* 4 params.m_num_nodes_at_this_level))
+			    (m_depth (+ 1 params.m_depth))
+			    (m_max_depth params.m_max_depth)
+			    (m_min_points_per_node params.m_min_points_per_node))))
+	      )
+	    (defclass Random_generator ()
+	      "public:"
+	      "int count;"
+	      
+	      (defmethod Random_generator ()
+		(declare
+		 (values "__host__ __device__")
+		 (construct (count 0))))
+	      (defmethod hash (a)
+		(declare (type "unsigned int" a)
+			 (values "__host__ __device__ unsigned int"))
+		,@(loop for e in `((+ #x7ed55d16 + << 12)
+				   (^ #xc761c23c ^ >> 19)
+				   (+ #x165667b1 + << 5)
+				   (+ #xd3a2646c ^  << 9)
+				   (+ #xfd7046c5 + << 3)
+				   (^ #xb55a4f09 ^ >> 16))
+			collect
+			(destructuring-bind (op val op2 sh len) e
+			  `(setf a (,op2 (,op a ,(format nil "0b~32,'0b" val))
+					(,sh a ,len)))))
+		(return a))
+	      (defmethod "operator()" ()
+		(declare (values "__host__ __device__ __forceinline__ thrust::tuple<float, float>"))
+		"#ifdef __CUDA_ARCH__"
+		(let (
+		      (seed (hash (+ count
+				     threadIdx.x
+				     (* blockDim.x blockIdx.x)
+				     ))))
+		  (incf count (* blockDim.x gridDim.x)))
+		"#else"
+		(let ((seed (hash 0))))
+		"#endif"
+		(let (())))
+	      
 	      ))))
 
     (let ((fn-h (asdf:system-relative-pathname
@@ -178,7 +216,9 @@
 		 (include <thrust/random.h>
 			    <thrust/device_vector.h>
 			    <thrust/host_vector.h>
-			    <cooperative_groups.h>)
+			    <cooperative_groups.h>
+
+			    )
 		 ,type-definitions)
 	       :hook-defun #'(lambda (str)
                                (format sh "~a~%" str))
@@ -197,6 +237,8 @@
 		   
 		   ,type-definitions
 
+		   "namespace cg = cooperative_groups;"
+		   (include <helper_cuda.h>)
 		   (defun build_quadtree_kernel (nodes
 						 points
 						 params)
@@ -220,7 +262,7 @@
 		       (dotimes (i 4)
 			 (setf (aref s_num_pts i)
 			       (cast "volatile int*"
-				     (ref (aref smem (* i NUM_WARS_PER_BLOCK))))))
+				     (ref (aref smem (* i NUM_WARPS_PER_BLOCK))))))
 		       (do0
 			(comments "stop recursion here, points[0] contains all points")
 			(let ((center)
@@ -230,11 +272,11 @@
 			  (declare (type float2 center)
 				   (type int range_begin range_end)
 				   (type (array int 4) warp_counts))
-			  (when (or (<= params.max_depth
-					    params.depth)
+			  (when (or (<= params.m_max_depth
+					    params.m_depth)
 					(<= num_points
-					    params.min_points_per_node))
-			    (when (== 1 params.point_selector)
+					    params.m_min_points_per_node))
+			    (when (== 1 params.m_point_selector)
 			      (let ((it (node.points_begin))
 				    (end (node.points_end)))
 				"#pragma unroll"
@@ -260,17 +302,18 @@
 					     -1)
 					  NUM_WARPS_PER_BLOCK
 					  )))
-				(range_begin (+ (node.points_begin)
-						(* warp_id num_points_per_warp)))
-				(range_end (min (+ range_begin
-						   num_points_per_warp)
-						(node.points_end)))))))
+				)
+			    (setf range_begin (+ (node.points_begin)
+						     (* warp_id num_points_per_warp))
+				      range_end (min (+ range_begin
+							num_points_per_warp)
+						     (node.points_end))))))
 		       (do0
 			(comments "count points in each child")
-			(let ((&in_points (aref points params.point_selector))
+			(let ((&in_points (aref points params.m_point_selector))
 			      (tile32 (cg--tiled_partition<32> cta)))
 			  "#pragma unroll"
-			  (for ((= range_it
+			  (for ((= "int range_it"
 				   (+ range_begin
 				      (tile32.thread_rank)))
 				(tile32.any (< range_it
@@ -279,7 +322,7 @@
 			       (let ((is_active (< range_it
 						   range_end))
 				     (p (? is_active
-					   (in_points.get_points range_it)
+					   (in_points.get_point range_it)
 					   (make_float2 0s0 0s0)))
 				     )
 				 ,@(loop for flag in `((and (< p.x center.x) (<= center.y p.y))
@@ -288,32 +331,224 @@
 						       (and (<= center.x p.x) (< p.y center.y)))
 					 and i from 0
 					 collect
-					 `(let ((num_pts (__popc (tile32.ballot
-								  (and is_active ,flag)
-								  ))))
-					    (incf (aref warp_counts ,i)
-						  (tile32.shfl num_pts 0))))
+					 `(progn
+					   (let ((num_pts (__popc (tile32.ballot
+								   (and is_active ,flag)
+								   ))))
+					     (incf (aref warp_counts ,i)
+						   (tile32.shfl num_pts 0)))))
 				 ))
 			  (when (== 0 (tile32.thread_rank))
 			    ,@(loop for i below 4 collect
 				    `(setf (aref (aref s_num_pts ,i)
 						 warp_id)
-					   (aref warp_cnts ,i))))
+					   (aref warp_counts ,i))))
 			  (cg--sync cta)
-			  (let ((val 0))
-			    (when (< threadIdx.x (* 4 NUM_WARPS_PER_BLOCK))
-			      (setf val (? (== 0 threadIdx.x)
-					   0 (aref smem (- threadIdx.x 1))))
-			      (incf val (node.points_begin))))
+			  (do0
+			   (comments "scan warps' results")
+			   (when (< warp_id 4)
+			     (let ((num_pts (? (< (tile32.thread_rank)
+						  NUM_WARPS_PER_BLOCK)
+					       (aref (aref s_num_pts warp_id)
+						     (tile32.thread_rank))
+					       0)))
+			       "#pragma unroll"
+			       (for ((= "int offset" 1)
+				     (< offset NUM_WARPS_PER_BLOCK)
+				     (*= offset 2))
+				    (let ((n (tile32.shfl_up num_pts offset)))
+				      (when (<= offset (tile32.thread_rank))
+					(incf num_pts n))))
+			       (when (< (tile32.thread_rank)
+					NUM_WARPS_PER_BLOCK)
+				 (setf (aref (aref s_num_pts
+						   warp_id)
+					     (tile32.thread_rank))
+				       num_pts)))))
+			  (cg--sync cta)
+			  (do0
+			   (comments "global offset")
+			   (when (== 0 warp_id)
+			     (let ((sum (aref s_num_pts 0
+					      (- NUM_WARPS_PER_BLOCK 1))))
+			       (for ((= "int row" 1)
+				     (< row 4)
+				     (incf row))
+				    (let ((tmp (aref s_num_pts
+						     row (- NUM_WARPS_PER_BLOCK 1)) ))
+				      (cg--sync tile32)
+				      (when (< (tile32.thread_rank)
+					       NUM_WARPS_PER_BLOCK)
+					(incf
+					 (aref s_num_pts
+					       row
+					       (tile32.thread_rank))
+					 sum))
+				      (cg--sync tile32)
+				      (incf sum tmp))))))
+
+			  (cg--sync cta)
+			  (do0
+			   (comments "scan exclusive")
+			   (let ((val 0))
+			     (when (< threadIdx.x
+				      (* 4 NUM_WARPS_PER_BLOCK))
+			       (setf val (? (== 0 threadIdx.x)
+					    0
+					    (aref smem (- threadIdx.x 1))))
+			       (incf val (node.points_begin)))))
+			  (cg--sync cta)
+			  (do0
+			   (comments "move points")
+			   (unless (or (<= params.m_max_depth
+					   params.m_depth)
+				       (<= num_points
+					   params.m_min_points_per_node))
+			     (let ((&out_points
+				     (aref points (% (+ params.m_point_selector 1)
+						     2))))
+			       ,@(loop for i below 4
+				       collect
+				       `(setf (aref warp_counts ,i)
+					      (aref s_num_pts ,i warp_id)))
+			       (let ((&in_points (aref points params.m_point_selector)))
+				 (do0
+				  (comments "reorder points")
+				  (for ((= "int range_it"
+					   (+ range_begin
+					      (tile32.thread_rank))
+					   )
+					(tile32.any (< range_it
+						       range_end))
+					(incf range_it warpSize))
+				       (let ((is_active (< range_it range_end))
+					     (p (? is_active
+						    (in_points.get_point range_it)
+						    (make_float2 0s0 0s0))))
+					 ,@(loop for pred in `((and (< p.x center.x)
+								    (<= center.y p.y))
+							       (and (<= center.x p.x)
+								    (<= center.y p.y))
+							       (and (< p.x center.x)
+								    (< p.y center.y))
+							       (and (<= center.x p.x)
+								    (< p.y center.y)))
+						 and i from 0
+						 collect
+						 `(progn
+						    (let ((pred (and is_active
+								     ,pred))
+							  (vote (tile32.ballot pred))
+							  (dest (+ (aref warp_counts ,i)
+								   (__popc (logand vote
+										   lane_mask_lt)))))
+						      (when pred
+							(out_points.set_point dest p))
+						      (incf (aref warp_counts ,i)
+							    (tile32.shfl (__popc vote)
+									 0)))))
+					 )))))))
+			  (cg--sync cta)
+			  (when (== 0 (tile32.thread_rank))
+			    ,@(loop for i below 4
+				    collect
+				    `(setf (aref s_num_pts ,i warp_id)
+					  (aref warp_counts ,i)))
+			    )
+			  (cg--sync cta)
+			  (do0
+			   (comments "launch new blocks")
+			   (unless (or
+				    (<= params.m_max_depth
+					params.m_depth)
+				    (<= num_points
+					params.m_min_points_per_node))
+			     (when (== (- NUM_THREADS_PER_BLOCK 1)
+				       threadIdx.x)
+			       (let ((*children
+				       (ref
+					(aref nodes
+					      (- params.m_num_nodes_at_this_level
+						 (logand (node.id)
+							 ~3))
+					      )))
+				     (child_offset (* 4 (node.id)))
+				     )
+				 ,@(loop for i below 4
+					 collect
+					 `(dot (aref children (+ child_offset ,i))
+					       (set_id (+ ,i (* 4 (node.id))))))
+				 (let ((&bbox (node.bounding_box))
+				       (&p_min (bbox.get_min)
+					       )
+				       (&p_max (bbox.get_max)))
+				   (do0
+				    (comments "set bboxes of the children")
+				    ,@(loop for e in `((p_min.x center.y center.x p_max.y)
+						       (center.x center.y p_max.x p_max.y)
+						       (p_min.x p_min.y center.x center.y)
+						       (center.x p_min.y p_max.x center.y))
+					    and i from 0
+					    collect
+					    `(dot (aref children (+ child_offset ,i))
+						  (set_bounding_box ,@e))))
+				   (do0
+				    (comments "set ranges of children")
+				    ,@ (let ((old `(node.points_begin)))
+					 (loop for i below 4
+					       
+					       collect
+					       (let ((new `(aref s_num_pts ,i   warp_id)))
+						(prog1
+						    `(dot
+						      (aref children (+ ,i child_offset))
+						      (set_range ,old ,new))
+						  (setf old new))))))
+				   (do0
+				    (comments "launch children")
+				    ("build_quadtree_kernel<NUM_THREADS_PER_BLOCK><<<4,NUM_THREADS_PER_BLOCK,4*NUM_WARPS_PER_BLOCK*sizeof(int)>>>"
+				     (ref (aref children child_offset))
+				     points
+				     (Parameters params true)))
+				   )))))
 			  ))
 		       )
 		     )
-		   
+		   (defun cdpQuadTree (warp_size)
+		     (declare (type int warp_size)
+			      (values bool))
+		     (let ((num_points 1000)
+			   (max_depth 8)
+			   (min_points_per_node 16))
+		       ,@(loop for e in `(x y)
+			       collect
+			       `(do0
+				 ,@(loop for f in `(0 1)
+				       collect
+				       `(,(format nil "thrust::device_vector<float> ~a_d~a" e f) num_points ))))))
 		   (defun main (argc argv)
 		     (declare (type int argc)
 			      (type char** argv)
 			      (values int))
-		     (return EXIT_SUCCESS)
+		     (let ((cuda_device (findCudaDevice argc
+							(cast "const char**" argv)))
+			   (deviceProps)
+			   )
+		       (declare (type cudaDeviceProp deviceProps))
+		       (checkCudaErrors
+			(cudaGetDeviceProperties &deviceProps
+						 cuda_device))
+		       #+nil (let ((cdpCapable (or
+					  (<= 4 deviceProps.major)
+					  (and (== 3 deviceProps.major)
+					       (<= 5 deviceProps.minor)
+					       ))))
+			 )
+		       (let ((ok (cdpQuadTree deviceProps.warpSize))))
+		       )
+		     
+		     (return (? ok EXIT_SUCCESS
+				 EXIT_FAILURE))
 		     )
 		   )))
   #+nil
