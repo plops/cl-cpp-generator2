2ba9097ac70a603ee3a3b6e9d9beb95409a3f0e4
Martin Kielhorn <kielhorn.martin@gmail.com>
2021-12-25 09:02:10 +0100

    can't compile on fedora maybe the g++ is too new?

--- example/61_cuda_code/source/nvcompile.sh
+++ example/61_cuda_code/source/nvcompile.sh
@@ -5,6 +5,6 @@
 
  #-dlto -arch=sm_75
 
-#/usr/local/cuda/bin/nvcc --cbin g++ -I. quadtree.cu -dc --std=c++14 --threads 0 -gencode arch=compute_75,code=compute_75
+# /usr/local/cuda/bin/nvcc --ccbin g++ -I. quadtree.cu -dc --std=c++14 --threads 0 -gencode arch=compute_75,code=compute_75
 
-#/usr/local/cuda/bin/nvcc -ccbin g++ -I../../Common -m64 -dc --std=c++14 --threads 0 -gencode arch=compute_75,code=sm_75 -o cdpQuadtree.o -c cdpQuadtree.cu
+# /usr/local/cuda/bin/nvcc -ccbin g++ -I../../Common -m64 -dc --std=c++14 --threads 0 -gencode arch=compute_75,code=sm_75 -o cdpQuadtree.o -c cdpQuadtree.cu
