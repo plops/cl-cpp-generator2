77e2d4892f09fc7a5d0a4a49c9a878774b91b610
Martin Kielhorn <kielhorn.martin@gmail.com>
2021-12-30 23:27:40 +0100

    srcipt

--- example/65_filament/source/build.sh
+++ example/65_filament/source/build.sh
@@ -1,4 +1,15 @@
+/home/martin/src/filament/bin/cmgen \
+ -x \
+ --quiet --format=ktx --size=256 --extract-blur=0.1 \
 
+
+mkdir generated
+/home/martin/src/filament/bin/matc \
+     --api all \
+     -f header \
+     -o generated/bakedColor.inc \
+     bakedColor.mat
+ 
 clang++ star_tracker.cpp \
 	-std=c++17 \
 	-I/home/martin/src/filament/include \
