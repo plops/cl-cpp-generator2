1529e4ea8d234dbd9fd50b5aa7e2a677870a560d
Martin Kielhorn <kielhorn.martin@gmail.com>
2021-12-31 00:27:25 +0100

    triangle example compiled and works
    -rwxrwxr-x. 1 martin martin 3.1K Dec 31 00:27 build.sh
    -rwxrwxr-x. 1 martin martin 4.5M Dec 31 00:27 a.out
    drwxrwxr-x. 3 martin martin 4.0K Dec 31 00:27 generated
    [martin@localhost source]$ ./a.out
    FEngine (64 bits) created at 0x7fef036e7010 (threading is enabled)
    FEngine resolved backend: Vulkan
    MESA-INTEL: warning: Performance support disabled, consider sysctl dev.i915.perf_stream_paranoid=0
    
    Vulkan device driver: Intel open-source Mesa driver Mesa 21.3.2
    Selected physical device 'Intel(R) HD Graphics 520 (SKL GT2)' from 2 physical devices. (vendor 0x8086, device 0x1916, driver 0x5403002, api 1.2)
    vkCreateSwapchain: 1024x640, 50, 0, 4, 1
    ^C^C

--- example/65_filament/source/build.sh
+++ example/65_filament/source/build.sh
@@ -19,6 +19,10 @@ mkdir -p generated/materials
  generated/materials/bakedColor.filamat
 
 cd generated
+ln -s /home/martin/src/b/filament-1.15.1/out/cmake-release/third_party .
+ln -s /home/martin/src/b/filament-1.15.1/out/cmake-release/libs .
+ln -s /home/martin/src/b/filament-1.15.1/out/cmake-release/filament .
+ln -s /home/martin/src/b/filament-1.15.1/out/cmake-release/shaders .
 clang++ ../star_tracker.cpp \
 	-std=c++17 \
 	-I/home/martin/src/b/filament-1.15.1/out/cmake-release/libs/filamentapp/include/ \
@@ -29,9 +33,31 @@ clang++ ../star_tracker.cpp \
 	-ffast-math \
 	resources.S \
 	-L/home/martin/src/b/filament-1.15.1/out/release/filament/lib/x86_64 \
-	-lbluevk -lfilament -lutils \
+	-lbluevk -lfilament -lutils -lvkshaders -lbackend \
 	-L/home/martin/src/b/filament-1.15.1/out/cmake-release/libs/filamentapp \
 	-lfilamentapp \
 	-L/home/martin/src/b/filament-1.15.1/out/cmake-release/third_party/libsdl2/tnt \
-	-lsdl2
- 
+	-lsdl2 \
+	-fstrict-aliasing -Wno-unknown-pragmas -Wno-unused-function -fPIC \
+	-fcolor-diagnostics -fvisibility=hidden -O3 -DNDEBUG \
+	-fomit-frame-pointer -ffunction-sections -fdata-sections \
+	-Wl,--gc-sections \
+	-L/home/martin/src/b/filament-1.15.1/out/cmake-release/ libs/filamentapp/libfilamentapp.a \
+	third_party/libassimp/tnt/libassimp.a \
+	third_party/libz/tnt/libz.a  libs/iblprefilter/libfilament-iblprefilter.a  \
+	third_party/libsdl2/tnt/libsdl2.a  libs/filamat/libfilamat.a  shaders/libshaders.a \
+	-Wl,--start-group  third_party/glslang/tnt/SPIRV/libSPIRV.a  \
+	third_party/glslang/tnt/glslang/libglslang.a  third_party/glslang/tnt/OGLCompilersDLL/libOGLCompiler.a  \
+	third_party/glslang/tnt/glslang/OSDependent/Unix/libOSDependent.a  \
+	third_party/glslang/tnt/SPIRV/libSPVRemapper.a  third_party/spirv-tools/source/opt/libSPIRV-Tools-opt.a \
+	third_party/spirv-tools/source/libSPIRV-Tools.a  /usr/lib64/librt.a \
+	third_party/spirv-cross/tnt/libspirv-cross-glsl.a  third_party/spirv-cross/tnt/libspirv-cross-msl.a  \
+	third_party/spirv-cross/tnt/libspirv-cross-core.a \
+	-Wl,--end-group  third_party/getopt/libgetopt.a  libs/filagui/libfilagui.a  filament/libfilament.a \
+	filament/backend/libbackend.a  libs/bluegl/libbluegl.a  libs/bluevk/libbluevk.a  \
+	filament/backend/libvkshaders.a  libs/filaflat/libfilaflat.a  third_party/smol-v/tnt/libsmol-v.a \
+	libs/filabridge/libfilabridge.a  libs/ibl/libibl-lite.a  -Wl,--exclude-libs,bluegl  \
+	third_party/imgui/tnt/libimgui.a  libs/image/libimage.a  libs/camutils/libcamutils.a  \
+	libs/geometry/libgeometry.a  libs/math/libmath.a  libs/utils/libutils.a \
+	-ldl  libs/filamentapp/libfilamentapp-resources.a 
+mv a.out ..
