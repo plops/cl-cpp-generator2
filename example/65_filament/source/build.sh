0b203806ec7942e468ac50c4b29d9854689b9d05
Martin Kielhorn <kielhorn.martin@gmail.com>
2021-12-30 23:45:38 +0100

    i think i managed to build filament on fedora, now try to link with it

--- example/65_filament/source/build.sh
+++ example/65_filament/source/build.sh
@@ -1,6 +1,6 @@
-/home/martin/src/filament/bin/cmgen \
- -x \
- --quiet --format=ktx --size=256 --extract-blur=0.1 \
+#/home/martin/src/filament/bin/cmgen \
+# -x \
+# --quiet --format=ktx --size=256 --extract-blur=0.1 \
 
 
 mkdir generated
@@ -12,7 +12,10 @@ mkdir generated
  
 clang++ star_tracker.cpp \
 	-std=c++17 \
-	-I/home/martin/src/filament/include \
+	-I/home/martin/src/b/filament-1.15.1/out/cmake-release/libs/filamentapp/include/ \
+	-I/home/martin/src/b/filament-1.15.1/libs/filamentapp/include/ \
+	-I/home/martin/src/b/filament-1.15.1/out/release/filament/include \
+	-I/home/martin/src/b/filament-1.15.1/third_party/libsdl2/include \
 	-Wno-extern-c-compat \
 	-ffast-math
  
