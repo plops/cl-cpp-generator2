1825c9b92c4684f86a26cba59e7dc62406ed7ba6
Martin Kielhorn <kielhorn.martin@gmail.com>
2022-02-19 14:19:18 +0100

    release build is 1.4M
    [martin@localhost 72_emsdk]$ ls -ltrh 02source/b/
    total 1.4M
    -rw-rw-r--. 1 martin martin    0 Feb 19 14:15 favicon.ico
    -rw-rw-r--. 1 martin martin  14K Feb 19 14:18 CMakeCache.txt
    -rw-rw-r--. 1 martin martin 1.6K Feb 19 14:18 cmake_install.cmake
    drwxrwxr-x. 4 martin martin 4.0K Feb 19 14:18 CMakeFiles
    -rw-rw-r--. 1 martin martin  17K Feb 19 14:18 build.ninja
    -rwxrwxr-x. 1 martin martin 1.2M Feb 19 14:18 index.wasm
    -rw-rw-r--. 1 martin martin  85K Feb 19 14:18 index.js
    -rw-rw-r--. 1 martin martin  92K Feb 19 14:18 index.html

---
+++ example/72_emsdk/release02.sh
@@ -0,0 +1,14 @@
+export NUM="02"
+export ODIR=/home/martin/stage/cl-cpp-generator2/example/72_emsdk/$NUM"source/"
+
+mkdir -p $ODIR/b
+
+cd $ODIR/b
+
+source "/home/martin/src/emsdk/emsdk_env.sh"
+emcmake cmake \
+	-G Ninja \
+	-DCMAKE_BUILD_TYPE=Release \
+	..
+
+cmake --build .
