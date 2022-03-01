432607a292071a4a68d151d54e3f5db6cb425b6b
Martin Kielhorn <kielhorn.martin@gmail.com>
2022-03-01 23:06:32 +0100

    link to js code

--- example/72_emsdk/08source/run03b_cmake_emscripten_release.sh
+++ example/72_emsdk/08source/run03b_cmake_emscripten_release.sh
@@ -1,5 +1,8 @@
-mkdir be
-cd be
+mkdir ber
+cd ber
 source "/home/martin/src/emsdk/emsdk_env.sh"
-emcmake cmake .. -GNinja -DCMAKE_BUILD_TYPE=Debug
-
+emcmake cmake .. -GNinja -DCMAKE_BUILD_TYPE=Release
+ninja -v
+# sudo dnf install binaryen
+mv index.wasm index0.wasm
+wasm-opt index0.wasm -o index.wasm --fast-math -O --intrinsic-lowering -O --vacuum --strip-debug
