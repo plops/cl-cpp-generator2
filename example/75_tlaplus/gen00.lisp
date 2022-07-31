1dc25347f3e44f0ae1b95f9efa18d56fd5fed8d3
Martin Kielhorn <kielhorn.martin@gmail.com>
2022-07-31 11:18:40 +0200

    add more code

--- example/75_tlaplus/gen00.lisp
+++ example/75_tlaplus/gen00.lisp
@@ -1,6 +1,10 @@
 (eval-when (:compile-toplevel :execute :load-toplevel)
   (ql:quickload "cl-cpp-generator2"))
 
+;; convert this code
+;; https://github.com/lemmy/BlockingQueue/blob/5917d5621d7ba5f2b8d66cd6180a058e5e929a87/impl/producer_consumer.c
+;; into s-expressions (and maybe use c++ so that i can use my logging functions)
+
 (in-package :cl-cpp-generator2)
 
 (let ((log-preamble `(do0 (include <iostream>
@@ -9,192 +13,66 @@
 				   <thread>
 				   )
 			  "extern std::chrono::time_point<std::chrono::high_resolution_clock> g_start_time;")))
+  (defun lprint (&key (msg "") (vars nil))
+  #+nil `(comments ,msg)
+  #-nil`(progn				;do0
+	  " "
+	  (do0				;let
+	   #+nil ((lock (std--unique_lock<std--mutex> ,(g `_stdout_mutex)))
+		  )
+
+	   (do0
+					;("std::setprecision" 3)
+	    ;; https://stackoverflow.com/questions/7889136/stdchrono-and-cout
+
+	    "std::chrono::duration<double>  timestamp = std::chrono::high_resolution_clock::now() - g_start_time;"
+	    (<< "std::cout"
+		;;"std::endl"
+		("std::setw" 10)
+		#+nil (dot ("std::chrono::high_resolution_clock::now")
+			   (time_since_epoch)
+			   (count)
+			   )
+		(dot timestamp
+		     (count))
+					;,(g `_start_time)
+
+					;(string " ")
+					;(dot now (period))
+		(string " ")
+		("std::this_thread::get_id")
+		(string " ")
+		__FILE__
+		(string ":")
+		__LINE__
+		(string " ")
+		__func__
+		(string " ")
+		(string ,msg)
+		(string " ")
+		,@(loop for e in vars appending
+			`(("std::setw" 8)
+					;("std::width" 8)
+			  (string ,(format nil " ~a='" (emit-c :code e)))
+			  ,e
+			  (string "'")))
+		"std::endl"
+		"std::flush")))))
   (progn
     ;; for classes with templates use write-source and defclass+
     ;; for cpp files without header use write-source
     ;; for class definitions and implementation in separate h and cpp file
 
-    (defparameter *source-dir* #P"example/71_imgui/source/")
-    (load "util.lisp")
-    (write-class
-     :dir (asdf:system-relative-pathname
-	   'cl-cpp-generator2
-	   *source-dir*)
-     :name `MainWindow
-     :headers `()
-     :header-preamble `(do0
-			(include <vector>
-;;;<GLFW/glfw3.h>
-				 )
-			"class GLFWwindow;"
-			"class ImVec4;"
-			"class ImGuiIO;"
-
-			)
-     :implementation-preamble `(do0
-				,log-preamble
-				(include "imgui_impl_opengl3_loader.h"
-
-					 "imgui.h"
-					 "imgui_impl_glfw.h"
-					 "imgui_impl_opengl3.h"
-
-					 <GLFW/glfw3.h>
-					 )
-				(include "implot.h")
-				)
-     :code `(do0
-	     (defclass MainWindow ()
-	       "public:"
-	       "bool show_demo_window_;"
-					;"ImVec4* clear_color_;"
-	       "ImGuiIO& io;"
-	       (defmethod MainWindow ()
-		 (declare
-		  (explicit)
-		  (construct
-		   (show_demo_window_ true)
-		   (io ((lambda ()
-			  (declare (values ImGuiIO&))
-			  (do0 (IMGUI_CHECKVERSION)
-			       (ImGui--CreateContext)
-			       (ImPlot--CreateContext)
-			       (return (ImGui--GetIO))))))
-		   #+nil (clear_color_ (new (ImVec4 .45s0
-						    .55s0
-						    .6s0
-						    1s0)))
-		   )
-		  (values :constructor))
-
-		 ,(lprint)
-		 )
-	       (defmethod ~MainWindow ()
-		 (declare
-		  (values :constructor))
-		 ,(lprint)
-		 #+nil (do0
-			(ImPlot--DestroyContext)
-			(ImGui--DestroyContext)))
-	       (defmethod Init (window glsl_version )
-		 (declare (type GLFWwindow* window)
-			  (type "const char*" glsl_version))
-		 ,(lprint)
-		 (do0
-		  #+nil(do0 (IMGUI_CHECKVERSION)
-			    (ImGui--CreateContext)
-			    (ImGui--GetIO))
-		  (do0			;let ((&io (ImGui--GetIO)))
-		   (comments "enable keyboard controls, docking multi-viewport")
-		   ,@(loop for e in `(NavEnableKeyboard
-				      DockingEnable
-					;ViewportsEnable
-				      )
-			   collect
-			   `(do0
-			     (setf io.ConfigFlags
-				   (logior io.ConfigFlags
-					   ,(format nil "ImGuiConfigFlags_~a" e)))
-			     ,(lprint :msg (format nil "~a" e)
-				      :vars `(,(format nil "ImGuiConfigFlags_~a" e)
-					       io.ConfigFlags
-					       (dot (ImGui--GetIO)
-						    ConfigFlags)))))
-		   )
-		  (do0
-		   ,(lprint :msg "setup ImGUI style")
-		   (ImGui--StyleColorsDark)
-					;(ImGui--StyleColorsClassic)
-		   (let ((style (ImGui--GetStyle)))
-		     (when
-			 (logand
-			  io.ConfigFlags
-			  ImGuiConfigFlags_ViewportsEnable)
-		       (setf style.WindowRounding 0s0
-			     (dot style (aref Colors ImGuiCol_WindowBg)
-				  w)
-			     1s0)))
-		   (ImGui_ImplGlfw_InitForOpenGL window true)
-		   (ImGui_ImplOpenGL3_Init glsl_version)
-		   (let ((font_fn (string
-				   ,(format nil "~a"
-					    (elt
-					     (directory "/home/martin/src/vcpkg/buildtrees/imgui/src/*/misc/fonts/DroidSans.ttf")
-					     0))))
-			 (font_size 16s0))
-		     (let ((*font (-> io.Fonts (AddFontFromFileTTF font_fn font_size))))
-		       (if (== font nullptr)
-			   ,(lprint :msg "loading font failed" :vars `(font_fn font_size))
-			   ,(lprint :msg "loaded font" :vars `(font_fn font_size)))))))
-		 )
-	       (defmethod NewFrame ()
-		 (do0 (ImGui_ImplOpenGL3_NewFrame)
-		      (ImGui_ImplGlfw_NewFrame)
-		      (ImGui--NewFrame)
-		      (ImGui--DockSpaceOverViewport)))
-	       (defmethod Update ()
-		 (do0
-		  (when show_demo_window_
-		    (ImGui--ShowDemoWindow &show_demo_window_)
-		    (ImPlot--ShowDemoWindow))
-		  (progn
-		    (ImGui--Begin (string "hello"))
-		    (ImGui--Checkbox (string "demo window")
-				     &show_demo_window_)
-		    (ImGui--Text (string "Application average %.3f ms/frame (%.1f FPS)")
-				 (/ 1000s0 (dot (ImGui--GetIO) Framerate))
-				 (dot (ImGui--GetIO) Framerate))
-		    (ImGui--End))
-		  )
-		 (ImGui--EndFrame)
-		 )
-	       (defmethod Render (window)
-		 (declare (type GLFWwindow* window))
-		 (do0
-
-		  (let ((screen_width (int 0))
-			(screen_height (int 0)))
-		    (glfwGetFramebufferSize window
-					    &screen_width
-					    &screen_height)
-					;,(lprint :msg "framebuffer" :vars `(screen_width screen_height))
-		    (glViewport 0 0 screen_width screen_height)
-		    #+nil(glClearColor (* clear_color_->x clear_color_->w)
-				       (* clear_color_->y clear_color_->w)
-				       (* clear_color_->z clear_color_->w)
-				       clear_color_->w)
-		    (glClear GL_COLOR_BUFFER_BIT)
-		    (ImGui--Render)
-		    (ImGui_ImplOpenGL3_RenderDrawData
-		     (ImGui--GetDrawData)))
-		  (do0
-		   (comments "update and render additional platform windows")
-		   (when
-		       (logand
-			io.ConfigFlags
-			ImGuiConfigFlags_ViewportsEnable)
-		     (let ((*backup_current_context
-			    (glfwGetCurrentContext)))
-		       (ImGui--UpdatePlatformWindows)
-		       (ImGui--RenderPlatformWindowsDefault)
-		       (glfwMakeContextCurrent backup_current_context)))))
-		 (glfwSwapBuffers window)
-		 )
-	       (defmethod Shutdown ()
-		 (do0
-		  ,(lprint)
-		  (ImGui_ImplOpenGL3_Shutdown)
-		  (ImGui_ImplGlfw_Shutdown)
-		  (ImGui--DestroyContext))
-		 ))))
-
-
+    (defparameter *source-dir* #P"example/75_tlaplus/source/")
+    (ensure-directories-exist (asdf:system-relative-pathname
+			       'cl-cpp-generator2
+			       *source-dir*))
     (write-source (asdf:system-relative-pathname
 		   'cl-cpp-generator2
 		   (merge-pathnames #P"main.cpp"
 				    *source-dir*))
 		  `(do0
-		    (include "MainWindow.h")
+		    
 		    (include
 					;<tuple>
 					;<mutex>
@@ -206,25 +84,45 @@
 		     <cassert>
 					;  <memory>
 		     )
+
+		    (include <stdio.h>
+			     <stdlib.h>
+			     <stdint.h>
+			     <pthread.h>)
+		    "uint32_t buf_size, numProducers, numConsumers, *buffer, fillIndex, useIndex, count=0;"
+		    "pthread_cond_t modify;"
+		    "pthread_mutex_t mutex;"
+
+
 		    "std::chrono::time_point<std::chrono::high_resolution_clock> g_start_time;"
-		    (do0
-		     (include "imgui_impl_opengl3_loader.h"
 
-			      "imgui.h"
-			      "imgui_impl_glfw.h"
-			      "imgui_impl_opengl3.h"
 
-			      <GLFW/glfw3.h>)
-		     (include "implot.h"
-			      )
-		     (comments "https://gist.github.com/TheOpenDevProject/1662fa2bfd8ef087d94ad4ed27746120")
-		     (defclass+ DestroyGLFWwindow ()
-		       "public:"
-		       (defmethod "operator()" (ptr)
-			 (declare (type GLFWwindow* ptr))
-			 ,(lprint :msg "Destroy GLFW window context.")
-			 (glfwDestroyWindow ptr)
-			 (glfwTerminate))))
+		    (defun append (value)
+		      (declare (type uint32_t value))
+		      (setf (aref buffer fillIndex)
+			    value
+			    fillIndex (% (+ fillIndex 1)
+					 buf_size))
+		      (incf count))
+		    (defun head ()
+		      (declare (values uint32_t))
+		      (let ((tmp (aref buffer useIndex)))
+			(setf 
+			 useIndex (% (+ useIndex 1)
+				     buf_size))
+			(decf count)
+			(return tmp)))
+
+		    (defun producer (arg)
+		      (declare (type void* arg)
+			       (values void*))
+		      (while 1
+			     (pthread_mutex_lock &mutex)
+			     (while (== count buf_size)
+				    (pthread_cond_wait &modify &mutex))
+			     (append (% (rand) 10))
+			     (pthread_cond_signal &modify)
+			     (pthread_mutex_unlock &mutex)))
 
 		    (defun main (argc argv)
 		      (declare (type int argc)
@@ -232,45 +130,7 @@
 			       (values int))
 		      (setf g_start_time ("std::chrono::high_resolution_clock::now"))
 		      ,(lprint :msg "start" :vars `(argc (aref argv 0)))
-		      (do0
-		       (comments "glfw initialization")
-		       (comments "https://github.com/ocornut/imgui/blob/docking/examples/example_glfw_opengl3/main.cpp")
-		       (glfwSetErrorCallback (lambda (err description)
-					       (declare (type int err)
-							(type "const char*" description))
-					       ,(lprint :msg "glfw error" :vars `(err description))))
-		       (unless (glfwInit)
-			 ,(lprint :msg "glfwInit failed."))
-		       "const char* glsl_version = \"#version 130\";"
-		       (glfwWindowHint GLFW_CONTEXT_VERSION_MAJOR 3)
-		       (glfwWindowHint GLFW_CONTEXT_VERSION_MINOR 0)
-		       "std::unique_ptr<GLFWwindow,DestroyGLFWwindow> window;"
-		       (let ((w (glfwCreateWindow 1280 720
-						  (string "dear imgui example")
-						  nullptr nullptr)))
-			 (when (== nullptr w)
-			   ,(lprint :msg "glfwCreatWindow failed."))
-			 (window.reset w)
-			 (glfwMakeContextCurrent (window.get))
-			 ,(lprint :msg "enable vsync")
-			 (glfwSwapInterval 1))
-		       (comments "imgui brings its own opengl loader"
-				 "https://github.com/ocornut/imgui/issues/4445")
-
-		       "MainWindow M;"
-		       (M.Init (window.get) glsl_version)
-		       (while (!glfwWindowShouldClose (window.get))
-			 (do0
-			  (glfwPollEvents)
-			  (M.NewFrame)
-			  (M.Update)
-			  (M.Render  (window.get))
-			  )))
-		      (do0
-		       ,(lprint :msg "cleanup")
-		       (M.Shutdown)
-		       #+nil (do0 (glfwDestroyWindow window)
-				  (glfwTerminate)))
+		      
 		      ,(lprint :msg "leave program")
 		      (return 0))))
 
@@ -299,19 +159,16 @@
 
 	  (out "set( SRCS ~{~a~^~%~} )"
 	       (append
-		(directory "source/*.cpp")
-		(directory "/home/martin/src/vcpkg/buildtrees/implot/src/*/implot_demo.cpp")
-					;(directory "/home/martin/src/vcpkg/buildtrees/imgui/src/*/backends/imgui_impl_opengl3.cpp")
-		))
+		(directory "source/*.cpp")))
 
 	  (out "add_executable( mytest ${SRCS} )")
 	  (out "target_compile_features( mytest PUBLIC cxx_std_17 )")
 
-	  (loop for e in `(imgui implot)
+	  #+nil (loop for e in `(imgui implot)
 		do
 		(out "find_package( ~a CONFIG REQUIRED )" e))
 
-	  (out "target_link_libraries( mytest PRIVATE ~{~a~^ ~} )"
+	  #+nil (out "target_link_libraries( mytest PRIVATE ~{~a~^ ~} )"
 	       `("imgui::imgui"
 		 "implot::implot"))
 
