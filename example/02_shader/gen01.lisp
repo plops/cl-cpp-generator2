1c3301808dda370e29e915dcfa29e4896cb6b37d
Martin Kielhorn <kielhorn.martin@gmail.com>
2022-08-18 17:58:17 +0200

    draw circle

--- example/02_shader/gen01.lisp
+++ example/02_shader/gen01.lisp
@@ -9,21 +9,29 @@
     (ensure-directories-exist (asdf:system-relative-pathname
 			       'cl-cpp-generator2
 			       *source-dir*))
+    
     (write-source (asdf:system-relative-pathname
 		   'cl-cpp-generator2
 		   (merge-pathnames #P"main.cpp"
 				    *source-dir*))
 		  `(do0
+		    "// try to learn how to write shaders based on https://www.youtube.com/watch?v=pmS-F6RJhAk"
 		    (defun Truchet (p)
 		      (declare (type vec2 p)
 			       (values vec4))
 		      (setf p (- (fract p)
 				 .5d0))
 		      (let ((d 0d0)
+			    ;; edge blur
+			    (w .01d0)
+			     ;;distance to center
+			    (cd (length p))
+			    
 			    (col (vec3 0d0)))
-			(declare (type float d)
+			(declare (type float d cd w)
 				 (type vec3 col)))
-		      (setf col.rg p)
+					;(setf col.rg p)
+		      (incf col (smoothstep w -w (- cd .5d0)))
 		      (return (vec4 col d)))
 		    (defun mainImage (fragColor fragCoord)
 		      (declare (type "out vec4" fragColor)
