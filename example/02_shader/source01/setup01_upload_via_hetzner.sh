383eedc166cfb4d0eb67a7aafe56cf3d043240e8
Martin Kielhorn <kielhorn.martin@gmail.com>
2022-08-18 08:06:04 +0200

    trim whitespace

--- example/02_shader/source01/setup01_upload_via_hetzner.sh
+++ example/02_shader/source01/setup01_upload_via_hetzner.sh
@@ -1,3 +1,4 @@
-scp main.cpp hetzner:/dev/shm/
+cat main.cpp | sed 's/^[ \t]*//' > main_trim.cpp
+scp main_trim.cpp hetzner:/dev/shm/
 # upload with python script from cl-py-generator/example/95_shadertoy
-ssh hetzner /home/martin/st.sh /dev/shm/main.cpp
+ssh hetzner /home/martin/st.sh /dev/shm/main_trim.cpp
