ed8157316bbb65083ea37a9f8707222139bce9a8
Martin Kielhorn <kielhorn.martin@gmail.com>
2022-09-14 07:19:47 +0200

    fully static build (on arch linux) fails in termux with bad system call

--- example/82_static_cpp/gen00.lisp
+++ example/82_static_cpp/gen00.lisp
@@ -3,63 +3,54 @@
 
 (in-package :cl-cpp-generator2)
 
-(let ((log-preamble `(do0 (include <iostream>
+(let (#+nil (log-preamble `(do0 (include <iostream>
 				   <iomanip>
 				   <chrono>
 				   <thread>
 				   )
 			  "extern std::chrono::time_point<std::chrono::high_resolution_clock> g_start_time;")))
   (defun lprint (&key (msg "") (vars nil))
-    #+nil `(comments ,msg)
-    #-nil`(progn				;do0
-	    " "
-	    (do0				;let
-	     #+nil ((lockxxxxxxx+ (std--unique_lock<std--mutex> ,(g `_stdout_mutex)))
-		    )
-
-	     (do0
-					;("std::setprecision" 3)
-	      ;; https://stackoverflow.com/questions/7889136/stdchrono-and-cout
-
-	      "std::chrono::duration<double>  timestamp = std::chrono::high_resolution_clock::now() - g_start_time;"
-	      (<< "std::cout"
-		  ;;"std::endl"
-		  ("std::setw" 10)
-		  #+nil (dot ("std::chrono::high_resolution_clock::now")
-			     (time_since_epoch)
-			     (count)
-			     )
-		  (dot timestamp
-		       (count))
-					;,(g `_start_time)
-
-					;(string " ")
-					;(dot now (period))
-		  (string " ")
-		  ("std::this_thread::get_id")
-		  (string " ")
-		  __FILE__
-		  (string ":")
-		  __LINE__
-		  (string " ")
-		  __func__
-		  (string " ")
-		  (string ,msg)
-		  (string " ")
-		  ,@(loop for e in vars appending
-			  `(("std::setw" 8)
+    `(lprint (curly  __FILE__
+		     (string ":")
+		     ("std::to_string" __LINE__)
+		     (string " ")
+		     __func__
+		     (string " ")
+		     (string ,msg)
+		     (string " ")
+		     ,@(loop for e in vars appending
+			     `(;("std::setw" 8)
 					;("std::width" 8)
-			    (string ,(format nil " ~a='" (emit-c :code e)))
-			    ,e
-			    (string "'")))
-		  "std::endl"
-		  "std::flush")))))
+			       (string ,(format nil " ~a='" (emit-c :code e)))
+			       ("std::to_string" ,e)
+			       (string "'"))))))
+  (defun init-lprint ()
+    `(defun lprint (il)
+	  (declare (type "std::initializer_list<std::string>" il))
+
+	  "std::chrono::duration<double>  timestamp = std::chrono::high_resolution_clock::now() - g_start_time;"
+	  (<< "std::cout"
+	      ("std::setw" 10)
+	      (dot timestamp
+		   (count))
+	      (string " ")
+	      ("std::this_thread::get_id")
+	      (string " ")
+	      )
+	  (for-range ((elem :type "const auto&")
+		      il)
+		     (<< "std::cout"
+
+			 elem) )
+	  (<< "std::cout"
+	      "std::endl"
+	      "std::flush")))
   (progn
     ;; for classes with templates use write-source and defclass+
     ;; for cpp files without header use write-source
     ;; for class definitions and implementation in separate h and cpp file
 
-    (defparameter *source-dir* #P"example/80_xtensor/source/")
+    (defparameter *source-dir* #P"example/82_static_cpp/source/")
     (defparameter *full-source-dir* (asdf:system-relative-pathname
 				     'cl-cpp-generator2
 				     *source-dir*))
@@ -79,17 +70,14 @@
 		     <chrono>
 					;  <memory>
 		     )
-		    (include
-					;<vector>
-		     <xtensor/xbuilder.hpp>
-		     <xtensor/xarray.hpp>
-		     <xtensor/xio.hpp>
-		     )
+		   
 
 					;"namespace stdex = std::experimental;"
 
 
 		    "std::chrono::time_point<std::chrono::high_resolution_clock> g_start_time;"
+
+		    ,(init-lprint)
 
 		    (defun main (argc argv)
 		      (declare (type int argc)
@@ -99,13 +87,8 @@
 
 		      (setf g_start_time ("std::chrono::high_resolution_clock::now"))
 
-		      ,(lprint :msg "start" :vars `(argc (aref argv 0)))
-		      (let ((ar (xt--linspace<double> .0 10.0 12)))
-			(ar.reshape (curly 4 3))
-			,(lprint :msg "" :vars `(ar))
-			#+nil (<< "std::cout"
-				  ar)
-			)
+		      ,(lprint :msg "start" :vars `(argc))
+		   
 
 		      (return 0))))
 
@@ -126,18 +109,19 @@
 	      ))
 	(macrolet ((out (fmt &rest rest)
 		     `(format s ,(format nil "~&~a~%" fmt) ,@rest)))
-	  (out "cmake_minimum_required( VERSION 3.19 )")
+	  (out "cmake_minimum_required( VERSION 3.13 )")
 	  (out "project( mytest LANGUAGES CXX )")
-	  (loop for e in `(xtl xsimd xtensor)
+	  #+nil(loop for e in `(xtl xsimd xtensor)
 		do
 		(format s "find_package( ~a REQUIRED )~%" e))
-	  (out "set( CMAKE_CXX_COMPILER clang++ )")
-					;(out "set( CMAKE_CXX_COMPILER g++ )")
+	  ;(out "set( CMAKE_CXX_COMPILER clang++ )")
+	  (out "set( CMAKE_CXX_COMPILER g++ )")
 	  (out "set( CMAKE_VERBOSE_MAKEFILE ON )")
 	  (out "set (CMAKE_CXX_FLAGS_DEBUG \"${CMAKE_CXX_FLAGS_DEBUG} ~a ~a ~a \")" dbg asan show-err)
 	  (out "set (CMAKE_LINKER_FLAGS_DEBUG \"${CMAKE_LINKER_FLAGS_DEBUG} ~a ~a \")" dbg show-err )
 					;(out "set( CMAKE_CXX_STANDARD 23 )")
 
+	  
 
 	  (out "set( SRCS ~{~a~^~%~} )"
 	       (append
@@ -146,12 +130,13 @@
 	  (out "add_executable( mytest ${SRCS} )")
 					;(out "include_directories( /usr/local/include/  )")
 
-					;(out "target_compile_features( mytest PUBLIC cxx_std_23 )")
+	  (out "target_compile_features( mytest PUBLIC cxx_std_23 )")
 
+	  (out "target_link_options( mytest PRIVATE -static -static-libgcc -static-libstdc++  )")
 	  #+nil (loop for e in `(imgui implot)
 		      do
 		      (out "find_package( ~a CONFIG REQUIRED )" e))
-
+	  #+nil
 	  (out "target_link_libraries( mytest PRIVATE ~{~a~^ ~} )"
 	       `(			;"imgui::imgui"
 					; "implot::implot"
