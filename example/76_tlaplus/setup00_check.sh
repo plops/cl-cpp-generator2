cdc39e985debb0ef044469c3e7f2830cdb2342ac
Martin Kielhorn <kielhorn.martin@gmail.com>
2022-09-23 22:53:28 +0200

    convert graph to pdf

--- example/76_tlaplus/setup00_check.sh
+++ example/76_tlaplus/setup00_check.sh
@@ -1,1 +1,12 @@
-java -XX:+UseParallelGC -jar ~/tla2tools.jar -deadlock -dump dot p1c1b1.dot queue 
+#!/bin/bash
+
+# print the commands
+set -x
+
+# run tla simulation
+java -XX:+UseParallelGC \
+     -jar ./tla2tools.jar \
+     -deadlock -dump dot p1c1b1.dot queue 
+
+# convert graph to pdf
+dot -Tpdf p1c1b1.dot -o p1c1b1.pdf
