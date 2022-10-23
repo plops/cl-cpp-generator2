db19427f01f6d67a33806644d2c09ff9e6814d82
Martin Kielhorn <kielhorn.martin@gmail.com>
2022-10-23 20:47:59 +0200

    more code

--- example/83_glfw_bgfx/gen00.lisp
+++ example/83_glfw_bgfx/gen00.lisp
@@ -23,188 +23,241 @@
     (ensure-directories-exist *full-source-dir*)
     (load "../83_glfw_bgfx/util.lisp")
 
-    #+nil (write-class
-	   :dir (asdf:system-relative-pathname
-		 'cl-cpp-generator2
-		 *source-dir*)
-	   :name `MainWindow
-	   :headers `()
-	   :header-preamble `(do0
-			      (include <vector>
+    #+nil
+    (write-class
+     :dir (asdf:system-relative-pathname
+	   'cl-cpp-generator2
+	   *source-dir*)
+     :name `MainWindow
+     :headers `()
+     :header-preamble `(do0
+			(include <vector>
 ;;;<GLFW/glfw3.h>
-				       <functional>
-				       <memory>
-				       )
-			      "class GLFWwindow;"
-			      "class ImVec4;"
-			      "class ImGuiIO;"
+				 <functional>
+				 <memory>
+				 )
+			"class GLFWwindow;"
+			"class ImVec4;"
+			"class ImGuiIO;"
 
-			      )
-	   :implementation-preamble `(do0
-				      ,log-preamble
-				      (include "imgui_impl_opengl3_loader.h"
+			)
+     :implementation-preamble `(do0
+				,log-preamble
+				(include "imgui_impl_opengl3_loader.h"
 
-					       "imgui.h"
-					       "imgui_impl_glfw.h"
-					       "imgui_impl_opengl3.h"
+					 "imgui.h"
+					 "imgui_impl_glfw.h"
+					 "imgui_impl_opengl3.h"
 
-					       <GLFW/glfw3.h>
-					       )
-				      (include "implot.h")
-				      )
-	   :code `(do0
-		   (defclass MainWindow ()
-		     "public:"
-		     "bool show_demo_window_;"
+					 <GLFW/glfw3.h>
+					 )
+				(include "implot.h")
+				)
+     :code `(do0
+	     (defclass MainWindow ()
+	       "public:"
+	       "bool show_demo_window_;"
 					;"ImVec4* clear_color_;"
-		     "ImGuiIO& io;"
-		     (defmethod MainWindow ()
-		       (declare
-			(explicit)
-			(construct
-			 (show_demo_window_ true)
-			 (io ((lambda ()
-				(declare (values ImGuiIO&))
-				(do0 (IMGUI_CHECKVERSION)
-				     (ImGui--CreateContext)
-				     (ImPlot--CreateContext)
-				     (return (ImGui--GetIO))))))
-			 #+nil (clear_color_ (new (ImVec4 .45s0
-							  .55s0
-							  .6s0
-							  1s0)))
-			 )
-			(values :constructor))
+	       "ImGuiIO& io;"
+	       (defmethod MainWindow ()
+		 (declare
+		  (explicit)
+		  (construct
+		   (show_demo_window_ true)
+		   (io ((lambda ()
+			  (declare (values ImGuiIO&))
+			  (do0 (IMGUI_CHECKVERSION)
+			       (ImGui--CreateContext)
+			       (ImPlot--CreateContext)
+			       (return (ImGui--GetIO))))))
+		   #+nil (clear_color_ (new (ImVec4 .45s0
+						    .55s0
+						    .6s0
+						    1s0)))
+		   )
+		  (values :constructor))
 
-		       ,(lprint)
-		       )
-		     (defmethod ~MainWindow ()
-		       (declare
-			(values :constructor))
-		       ,(lprint)
-		       #+nil (do0
-			      (ImPlot--DestroyContext)
-			      (ImGui--DestroyContext)) )
-		     (defmethod Init (window glsl_version )
-		       (declare ;(type GLFWwindow* window)
-			(type "std::shared_ptr< GLFWwindow >" window)
-			(type "const char*" glsl_version)
-			)
-		       ,(lprint)
-		       (do0
-
-			(do0
-			 (comments "enable keyboard controls, docking multi-viewport")
-			 ,@(loop for e in `(NavEnableKeyboard
-					    DockingEnable
-					;ViewportsEnable
-					    )
-				 collect
-				 `(do0
-				   (setf io.ConfigFlags
-					 (logior io.ConfigFlags
-						 ,(format nil "ImGuiConfigFlags_~a" e)))
-				   ,(lprint :msg (format nil "~a" e)
-					    :vars `(,(format nil "ImGuiConfigFlags_~a" e)
-						     io.ConfigFlags
-						     (dot (ImGui--GetIO)
-							  ConfigFlags)))))
-			 )
-			(do0
-			 ,(lprint :msg "setup ImGUI style")
-			 (ImGui--StyleColorsDark)
-					;(ImGui--StyleColorsClassic)
-			 (let ((style (ImGui--GetStyle)))
-			   #+nil (when
-				     (logand
-				      io.ConfigFlags
-				      ImGuiConfigFlags_ViewportsEnable)
-				   (setf style.WindowRounding 0s0
-					 (dot style (aref Colors ImGuiCol_WindowBg)
-					      w)
-					 1s0)))
-			 (ImGui_ImplGlfw_InitForOpenGL (window.get) true)
-			 (ImGui_ImplOpenGL3_Init glsl_version)
-			 (let ((font_fn (string
-					 ,(format nil "~a"
-						  (elt
-						   (directory "/home/martin/src/vcpkg/buildtrees/imgui/src/*/misc/fonts/DroidSans.ttf")
-						   0))))
-			       (font_size 16s0))
-			   (let ((*font (-> io.Fonts (AddFontFromFileTTF font_fn font_size))))
-			     (if (== font nullptr)
-				 ,(lprint :msg "loading font failed" :vars `(font_fn font_size))
-				 ,(lprint :msg "loaded font" :vars `(font_fn font_size)))))))
-		       )
-		     (defmethod NewFrame ()
-		       (do0 (ImGui_ImplOpenGL3_NewFrame)
-			    (ImGui_ImplGlfw_NewFrame)
-			    (ImGui--NewFrame)
-			    (ImGui--DockSpaceOverViewport)))
-		     (defmethod Update (fun)
-		       (declare (type "std::function<void(void)>" fun))
-		       (do0
-			(when show_demo_window_
-			  (ImGui--ShowDemoWindow &show_demo_window_)
-			  (ImPlot--ShowDemoWindow)
-			  )
-			(progn
-			  (ImGui--Begin (string "hello"))
-			  (ImGui--Checkbox (string "demo window")
-					   &show_demo_window_)
-			  (ImGui--Text (string "Application average %.3f ms/frame (%.1f FPS)")
-				       (/ 1000s0 (dot (ImGui--GetIO) Framerate))
-				       (dot (ImGui--GetIO) Framerate))
-			  (ImGui--End))
-			(fun)
-			)
-		       (ImGui--EndFrame)
-		       )
-		     (defmethod Render (window)
-		       (declare ; (type GLFWwindow* window)
-			(type "std::shared_ptr< GLFWwindow >" window)
-			)
-		       (do0
-
-			(let ((screen_width (int 0))
-			      (screen_height (int 0)))
-			  (glfwGetFramebufferSize (window.get)
-						  &screen_width
-						  &screen_height)
-					;,(lprint :msg "framebuffer" :vars `(screen_width screen_height))
-			  (glViewport 0 0 screen_width screen_height)
-			  #+nil(glClearColor (* clear_color_->x clear_color_->w)
-					     (* clear_color_->y clear_color_->w)
-					     (* clear_color_->z clear_color_->w)
-					     clear_color_->w)
-			  (glClear GL_COLOR_BUFFER_BIT)
-			  (ImGui--Render)
-			  (ImGui_ImplOpenGL3_RenderDrawData
-			   (ImGui--GetDrawData)))
-			#+nil (do0
-			       (comments "update and render additional platform windows")
-			       (when
-				   (logand
-				    io.ConfigFlags
-				    ImGuiConfigFlags_ViewportsEnable)
-				 (let ((*backup_current_context
-					(glfwGetCurrentContext)))
-				   (ImGui--UpdatePlatformWindows)
-				   (ImGui--RenderPlatformWindowsDefault)
-				   (glfwMakeContextCurrent backup_current_context)))))
-		       (glfwSwapBuffers (window.get))
-		       )
-		     (defmethod Shutdown ()
-		       (do0
-			,(lprint :msg "destroy ImPlot Context")
+		 ,(lprint)
+		 )
+	       (defmethod ~MainWindow ()
+		 (declare
+		  (values :constructor))
+		 ,(lprint)
+		 #+nil (do0
 			(ImPlot--DestroyContext)
-			,(lprint :msg "delete ImGui buffers and textures")
-			(ImGui_ImplOpenGL3_Shutdown)
-			,(lprint :msg "delete ImGui callbacks and mouse cursor from GLFW")
-			(ImGui_ImplGlfw_Shutdown)
-			,(lprint :msg "destroy ImGui Context")
-			(ImGui--DestroyContext))
-		       ))))
+			(ImGui--DestroyContext)) )
+	       (defmethod Init (window glsl_version )
+		 (declare ;(type GLFWwindow* window)
+		  (type "std::shared_ptr< GLFWwindow >" window)
+		  (type "const char*" glsl_version)
+		  )
+		 ,(lprint)
+		 (do0
+
+		  (do0
+		   (comments "enable keyboard controls, docking multi-viewport")
+		   ,@(loop for e in `(NavEnableKeyboard
+				      DockingEnable
+					;ViewportsEnable
+				      )
+			   collect
+			   `(do0
+			     (setf io.ConfigFlags
+				   (logior io.ConfigFlags
+					   ,(format nil "ImGuiConfigFlags_~a" e)))
+			     ,(lprint :msg (format nil "~a" e)
+				      :vars `(,(format nil "ImGuiConfigFlags_~a" e)
+					       io.ConfigFlags
+					       (dot (ImGui--GetIO)
+						    ConfigFlags)))))
+		   )
+		  (do0
+		   ,(lprint :msg "setup ImGUI style")
+		   (ImGui--StyleColorsDark)
+					;(ImGui--StyleColorsClassic)
+		   (let ((style (ImGui--GetStyle)))
+		     #+nil (when
+			       (logand
+				io.ConfigFlags
+				ImGuiConfigFlags_ViewportsEnable)
+			     (setf style.WindowRounding 0s0
+				   (dot style (aref Colors ImGuiCol_WindowBg)
+					w)
+				   1s0)))
+		   (ImGui_ImplGlfw_InitForOpenGL (window.get) true)
+		   (ImGui_ImplOpenGL3_Init glsl_version)
+		   (let ((font_fn (string
+				   ,(format nil "~a"
+					    (elt
+					     (directory "/home/martin/src/vcpkg/buildtrees/imgui/src/*/misc/fonts/DroidSans.ttf")
+					     0))))
+			 (font_size 16s0))
+		     (let ((*font (-> io.Fonts (AddFontFromFileTTF font_fn font_size))))
+		       (if (== font nullptr)
+			   ,(lprint :msg "loading font failed" :vars `(font_fn font_size))
+			   ,(lprint :msg "loaded font" :vars `(font_fn font_size)))))))
+		 )
+	       (defmethod NewFrame ()
+		 (do0 (ImGui_ImplOpenGL3_NewFrame)
+		      (ImGui_ImplGlfw_NewFrame)
+		      (ImGui--NewFrame)
+		      (ImGui--DockSpaceOverViewport)))
+	       (defmethod Update (fun)
+		 (declare (type "std::function<void(void)>" fun))
+		 (do0
+		  (when show_demo_window_
+		    (ImGui--ShowDemoWindow &show_demo_window_)
+		    (ImPlot--ShowDemoWindow)
+		    )
+		  (progn
+		    (ImGui--Begin (string "hello"))
+		    (ImGui--Checkbox (string "demo window")
+				     &show_demo_window_)
+		    (ImGui--Text (string "Application average %.3f ms/frame (%.1f FPS)")
+				 (/ 1000s0 (dot (ImGui--GetIO) Framerate))
+				 (dot (ImGui--GetIO) Framerate))
+		    (ImGui--End))
+		  (fun)
+		  )
+		 (ImGui--EndFrame)
+		 )
+	       (defmethod Render (window)
+		 (declare ; (type GLFWwindow* window)
+		  (type "std::shared_ptr< GLFWwindow >" window)
+		  )
+		 (do0
+
+		  (let ((screen_width (int 0))
+			(screen_height (int 0)))
+		    (glfwGetFramebufferSize (window.get)
+					    &screen_width
+					    &screen_height)
+					;,(lprint :msg "framebuffer" :vars `(screen_width screen_height))
+		    (glViewport 0 0 screen_width screen_height)
+		    #+nil(glClearColor (* clear_color_->x clear_color_->w)
+				       (* clear_color_->y clear_color_->w)
+				       (* clear_color_->z clear_color_->w)
+				       clear_color_->w)
+		    (glClear GL_COLOR_BUFFER_BIT)
+		    (ImGui--Render)
+		    (ImGui_ImplOpenGL3_RenderDrawData
+		     (ImGui--GetDrawData)))
+		  #+nil (do0
+			 (comments "update and render additional platform windows")
+			 (when
+			     (logand
+			      io.ConfigFlags
+			      ImGuiConfigFlags_ViewportsEnable)
+			   (let ((*backup_current_context
+				  (glfwGetCurrentContext)))
+			     (ImGui--UpdatePlatformWindows)
+			     (ImGui--RenderPlatformWindowsDefault)
+			     (glfwMakeContextCurrent backup_current_context)))))
+		 (glfwSwapBuffers (window.get))
+		 )
+	       (defmethod Shutdown ()
+		 (do0
+		  ,(lprint :msg "destroy ImPlot Context")
+		  (ImPlot--DestroyContext)
+		  ,(lprint :msg "delete ImGui buffers and textures")
+		  (ImGui_ImplOpenGL3_Shutdown)
+		  ,(lprint :msg "delete ImGui callbacks and mouse cursor from GLFW")
+		  (ImGui_ImplGlfw_Shutdown)
+		  ,(lprint :msg "destroy ImGui Context")
+		  (ImGui--DestroyContext))
+		 ))))
+
+    (write-class
+     :dir (asdf:system-relative-pathname
+	   'cl-cpp-generator2
+	   *source-dir*)
+     :name `PosColorVertex
+     :headers `()
+     :header-preamble `(include <bgfx/bgfx.h>
+
+				)
+     :implementation-preamble `(include <bgfx/bgfx.h>
+
+					)
+     :code `(do0
+	     (defclass PosColorVertex ()
+	       "public:"
+	       ,@(loop for e in `(x y z)
+		       collect
+		       (format nil "float m_~a;" e))
+	       ,@(loop for e in `(abgr)
+		       collect
+		       (format nil "uint32_t m_~a;" e))
+	       "static bgfx::VertexLayout ms_decl;"
+
+	       (defmethod init ()
+		 (declare
+		  (static))
+		 (dot ms_decl
+		      (begin)
+		      (add bgfx--Attrib--Position 3 bgfx--AttribType--Float)
+		      (add bgfx--Attrib--Color0 4 bgfx--AttribType--Uint8 true)
+		      (end)))
+
+	       )))
+
+    (write-source
+     (asdf:system-relative-pathname
+      'cl-cpp-generator2
+      (merge-pathnames #P"v_simple.sc"
+		       *source-dir*))
+     `(do0
+       "$input a_position, a_color0
+"
+       "$output v_color0"
+       (include <bgfx_shader.sh>)
+
+       (defun main ()
+	 (setf gl_Position (mul u_modelViewProj
+				(vec4 a_position
+				      1s0))
+	       v_color a_color0)))
+     :format nil)
 
     (write-source (asdf:system-relative-pathname
 		   'cl-cpp-generator2
@@ -228,7 +281,9 @@
 					;(include <bx/bx.h>)
 		     (include <bgfx/bgfx.h>
 			      <bgfx/platform.h>
-			      ))
+			      )
+		     (include "PosColorVertex.h")
+		     (include <fstream>))
 		    (do0
 		     (include <GLFW/glfw3.h>
 			      )
@@ -243,7 +298,58 @@
 
 		    "std::chrono::time_point<std::chrono::high_resolution_clock> g_start_time;"
 
+		    "bgfx::VertexLayout PosColorVertex::ms_decl;"
+
+		    (let (("s_cubeVertices[]"
+			   (curly ,@(loop for e in `((.5 .5 .0 #xff0000ff)
+						     (.5 -.5 .0 #xff0000ff)
+						     (-.5 -.5 .0 #xff00ff00)
+						     (-.5 .5 .0 #xff00ff00))
+					  collect
+					  (destructuring-bind (x y z col) e
+					    `(curly ,x ,y ,z
+						    (hex ,col)))))))
+		      (declare (type "static PosColorVertex" "s_cubeVertices[]")))
+
+		    (let (("s_cubeTriList[]"
+			   (curly 0 1 3
+				  1 2 3)))
+		      (declare (type "static const uint16_t" "s_cubeTriList[]")))
+		    ,@(loop for (e f) in `((VertexBufferHandle vbh)
+					   (IndexBufferHandle ibh)
+					   (ProgramHandle program))
+			    collect
+			    (format nil "bgfx::~a m_~a;"
+				    e f))
+
 		    ,(init-lprint)
+
+		    (defun loadShader (_name)
+		      (declare (type "const char*" _name)
+			       (values "bgfx::ShaderHandle"))
+		      ,(lprint :msg "loadShader" :vars `())
+		      (let ((*data (new "char[2048]"))
+			    (file (std--ifstream))
+			    (fileSize (size_t 0)))
+			(file.open _name)
+			(if (file.is_open)
+			    (do0
+			     ,(lprint :msg "load shader from file")
+			     (file.seekg 0 std--ios--end)
+			     (setf fileSize (file.tellg))
+			     (file.seekg 0 std--ios--beg)
+			     (assert (< fileSize 2048))
+			     (file.read data fileSize)
+			     (file.close))
+			    (do0
+			     ,(lprint :msg "warning: can't open shader file")))
+			(let ((mem (bgfx--copy data (+ fileSize 1))))
+			  (setf (aref mem->data (- mem->size
+						   1))
+				(char "\\0"))
+			  (let ((handle (bgfx--createShader mem)))
+			    (bgfx--setName handle _name)
+			    (return handle)))))
 
 		    (defun main (argc argv)
 		      (declare (type int argc)
@@ -267,69 +373,88 @@
 					    ,(lprint :msg "can't create glfw window"))
 					  (return window))
 					)))))
-		      (let ((width (int 0)
-			      )
+		      (let ((width (int 0))
 			    (height (int 0)))
-			)
-		      ((lambda ()
-			 (declare (capture &width &height window ))
-			 (do0
-			  (comments "call renderFrame before bgfx::init to signal to bgfx not to create a render thread")
-			  (bgfx--renderFrame))
-			 (let ((bi (bgfx--Init)))
-			   (do0
-			    (setf bi.platformData.ndt (glfwGetX11Display))
-			    (setf bi.platformData.nwh (reinterpret_cast<void*> (static_cast<uintptr_t> (glfwGetX11Window window)))))
-			   (glfwGetWindowSize window &width &height)
-			   ,@(loop for (e f)
-				   in `((type bgfx--RendererType--Count)
-					(resolution.width width)
-					(resolution.height height)
-					(resolution.reset BGFX_RESET_VSYNC))
-				   collect
-				   `(setf (dot bi ,e)
-					  ,f))
-			   (unless (bgfx--init bi)
-			     ,(lprint :msg "bgfx init failed"))
-			   (let ((debug BGFX_DEBUG_TEXT))
-			     (bgfx--setDebug debug)
-			     (bgfx--setViewClear 0
-						 (logior BGFX_CLEAR_COLOR
-							 BGFX_CLEAR_DEPTH)
-						 (hex #x303030ff)
-						 1s0
-						 0)
-			     (bgfx--setViewRect 0
-						0 0
-						bgfx--BackbufferRatio--Equal)
-			     (imguiCreate))
-			   )))
-
-		      (while (not (glfwWindowShouldClose window))
-			(glfwPollEvents)
-
-			#+nil ((lambda ()
-				 (imguiBeginFrame)
-				 (showExampleDialog )
-				 (imguiEndFrame)))
 
 			((lambda ()
-			   (declare (capture &width &height window))
-			   (let ((oldwidth width)
-				 (oldheight height))
+			   (declare (capture &width &height window ))
+			   (comments "initialize bgfx")
+			   (do0
+			    (comments "call renderFrame before bgfx::init to signal to bgfx not to create a render thread")
+			    (bgfx--renderFrame))
+			   (let ((bi (bgfx--Init)))
+			     (do0
+			      (setf bi.platformData.ndt (glfwGetX11Display))
+			      (setf bi.platformData.nwh (reinterpret_cast<void*> (static_cast<uintptr_t> (glfwGetX11Window window)))))
 			     (glfwGetWindowSize window &width &height)
-			     (when (or (!= width oldwidth)
-				       (!= height oldheight))
-			       (bgfx--reset width height BGFX_RESET_VSYNC)
+			     ,@(loop for (e f)
+				     in `((type bgfx--RendererType--Count)
+					  (resolution.width width)
+					  (resolution.height height)
+					  (resolution.reset BGFX_RESET_VSYNC))
+				     collect
+				     `(setf (dot bi ,e)
+					    ,f))
+			     (unless (bgfx--init bi)
+			       ,(lprint :msg "bgfx init failed"))
+
+			     (do0
+			      (PosColorVertex--init)
+			      (setf m_vbh (bgfx--createVertexBuffer
+					   (bgfx--makeRef s_cubeVertices
+							  (sizeof s_cubeVertices))
+					   PosColorVertex--ms_decl))
+			      (setf m_ibh (bgfx--createIndexBuffer
+					   (bgfx--makeRef s_cubeTriList
+							  (sizeof s_cubeTriList))))
+			      (let ((vsh (bgfx--ShaderHandle (loadShader (string "v_simple.bin"))))
+				    (fsh (bgfx--ShaderHandle (loadShader (string "f_simple.bin"))))
+				    )
+				(setf m_program (bgfx--createProgram vsh fsh true))))
+
+
+			     (let ((debug BGFX_DEBUG_TEXT))
+			       (bgfx--setDebug debug)
+			       (bgfx--setViewClear 0
+						   (logior BGFX_CLEAR_COLOR
+							   BGFX_CLEAR_DEPTH)
+						   (hex #x303030ff)
+						   1s0
+						   0)
 			       (bgfx--setViewRect 0
 						  0 0
-						  bgfx--BackbufferRatio--Equal)))))
-			(bgfx--touch 0)
-			(bgfx--dbgTextClear)
-			(bgfx--dbgTextPrintf 0 0 #xf (string "press F1 to toggle stats"))
-			(bgfx--setDebug BGFX_DEBUG_STATS ;TEXT
-					)
-			(bgfx--frame))
+						  bgfx--BackbufferRatio--Equal)
+			       (imguiCreate))
+			     )))
+
+			(while (not (glfwWindowShouldClose window))
+			  (glfwPollEvents)
+
+			  #+nil ((lambda ()
+				   (imguiBeginFrame)
+				   (showExampleDialog )
+				   (imguiEndFrame)))
+
+			  ((lambda ()
+			     (declare (capture &width &height window))
+			     (comments "react to changing window size")
+			     (let ((oldwidth width)
+				   (oldheight height))
+			       (glfwGetWindowSize window &width &height)
+			       (when (or (!= width oldwidth)
+					 (!= height oldheight))
+				 (bgfx--reset width height BGFX_RESET_VSYNC)
+				 (bgfx--setViewRect 0
+						    0 0
+						    bgfx--BackbufferRatio--Equal)))))
+			  (do0
+			   (comments "draw frame")
+			   (bgfx--touch 0)
+			   (bgfx--dbgTextClear)
+			   (bgfx--dbgTextPrintf 0 0 #xf (string "press F1 to toggle stats"))
+			   (bgfx--setDebug BGFX_DEBUG_STATS ;TEXT
+					   )
+			   (bgfx--frame))))
 		      (bgfx--shutdown)
 		      (glfwTerminate)
 
--- example/83_glfw_bgfx/source00/main.cpp
+++ example/83_glfw_bgfx/source00/main.cpp
@@ -1,7 +1,9 @@
+#include "PosColorVertex.h"
 #include <GLFW/glfw3.h>
 #include <bgfx/bgfx.h>
 #include <bgfx/platform.h>
 #include <chrono>
+#include <fstream>
 #include <iomanip>
 #include <iostream>
 #include <thread>
@@ -9,6 +11,16 @@
 #include <GLFW/glfw3native.h>
 #include <imgui/imgui.h>
 std::chrono::time_point<std::chrono::high_resolution_clock> g_start_time;
+bgfx::VertexLayout PosColorVertex::ms_decl;
+static PosColorVertex s_cubeVertices[] = {
+    {(0.50f), (0.50f), (0.f), 0xFF0000FF},
+    {(0.50f), (-0.50f), (0.f), 0xFF0000FF},
+    {(-0.50f), (-0.50f), (0.f), 0xFF00FF00},
+    {(-0.50f), (0.50f), (0.f), 0xFF00FF00}};
+static const uint16_t s_cubeTriList[] = {0, 1, 3, 1, 2, 3};
+bgfx::VertexBufferHandle m_vbh;
+bgfx::IndexBufferHandle m_ibh;
+bgfx::ProgramHandle m_program;
 void lprint(std::initializer_list<std::string> il) {
   std::chrono::duration<double> timestamp =
       std::chrono::high_resolution_clock::now() - g_start_time;
@@ -18,6 +30,32 @@ void lprint(std::initializer_list<std::string> il) {
     (std::cout) << (elem);
   }
   (std::cout) << (std::endl) << (std::flush);
+}
+bgfx::ShaderHandle loadShader(const char *_name) {
+  lprint({__FILE__, ":", std::to_string(__LINE__), " ", __func__, " ",
+          "loadShader", " "});
+  auto *data = new char[2048];
+  auto file = std::ifstream();
+  auto fileSize = size_t(0);
+  file.open(_name);
+  if (file.is_open()) {
+    lprint({__FILE__, ":", std::to_string(__LINE__), " ", __func__, " ",
+            "load shader from file", " "});
+    file.seekg(0, std::ios::end);
+    fileSize = file.tellg();
+    file.seekg(0, std::ios::beg);
+    assert((fileSize) < (2048));
+    file.read(data, fileSize);
+    file.close();
+  } else {
+    lprint({__FILE__, ":", std::to_string(__LINE__), " ", __func__, " ",
+            "warning: can't open shader file", " "});
+  }
+  auto mem = bgfx::copy(data, ((fileSize) + (1)));
+  mem->data[((mem->size) - (1))] = '\0';
+  auto handle = bgfx::createShader(mem);
+  bgfx::setName(handle, _name);
+  return handle;
 }
 int main(int argc, char **argv) {
   g_start_time = std::chrono::high_resolution_clock::now();
@@ -38,6 +76,8 @@ int main(int argc, char **argv) {
   auto width = int(0);
   auto height = int(0);
   ([&width, &height, window]() {
+    // initialize bgfx
+    ;
     // call renderFrame before bgfx::init to signal to bgfx not to create a
     // render thread
     ;
@@ -55,6 +95,15 @@ int main(int argc, char **argv) {
       lprint({__FILE__, ":", std::to_string(__LINE__), " ", __func__, " ",
               "bgfx init failed", " "});
     }
+    PosColorVertex::init();
+    m_vbh = bgfx::createVertexBuffer(
+        bgfx::makeRef(s_cubeVertices, sizeof(s_cubeVertices)),
+        PosColorVertex::ms_decl);
+    m_ibh = bgfx::createIndexBuffer(
+        bgfx::makeRef(s_cubeTriList, sizeof(s_cubeTriList)));
+    auto vsh = bgfx::ShaderHandle(loadShader("v_simple.bin"));
+    auto fsh = bgfx::ShaderHandle(loadShader("f_simple.bin"));
+    m_program = bgfx::createProgram(vsh, fsh, true);
     auto debug = BGFX_DEBUG_TEXT;
     bgfx::setDebug(debug);
     bgfx::setViewClear(0, ((BGFX_CLEAR_COLOR) | (BGFX_CLEAR_DEPTH)), 0x303030FF,
@@ -65,6 +114,8 @@ int main(int argc, char **argv) {
   while (!(glfwWindowShouldClose(window))) {
     glfwPollEvents();
     ([&width, &height, window]() {
+      // react to changing window size
+      ;
       auto oldwidth = width;
       auto oldheight = height;
       glfwGetWindowSize(window, &width, &height);
@@ -73,6 +124,8 @@ int main(int argc, char **argv) {
         bgfx::setViewRect(0, 0, 0, bgfx::BackbufferRatio::Equal);
       }
     })();
+    // draw frame
+    ;
     bgfx::touch(0);
     bgfx::dbgTextClear();
     bgfx::dbgTextPrintf(0, 0, 15, "press F1 to toggle stats");
