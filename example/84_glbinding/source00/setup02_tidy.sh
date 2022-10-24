2b11e2722b669107910f473ba79f32f8370dba66
Martin Kielhorn <kielhorn.martin@gmail.com>
2022-10-24 13:23:23 +0200

    more code

---
+++ example/84_glbinding/source00/setup02_tidy.sh
@@ -0,0 +1,9 @@
+clang-tidy \
+    main.cpp \
+    -checks=-*,cppcoreguidelines-*,clang-analyzer-*,-clang-analyzer-cplusplus* \
+    -extra-arg=-std=c++20 \
+    -- \
+    -I/usr/local/include
+
+   
+    
