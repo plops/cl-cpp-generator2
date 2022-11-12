5096cebdec478b3e20ada2f3226c42bab71f79ca
Martin Kielhorn <kielhorn.martin@gmail.com>
2022-11-12 15:50:14 +0100

    create video class

---
+++ example/85_glbinding_av/source01/Video.cpp
@@ -0,0 +1,78 @@
+// no preamble
+#include <chrono>
+#include <iomanip>
+#include <iostream>
+#include <thread>
+void lprint(std::initializer_list<std::string> il, std::string file, int line,
+            std::string fun);
+extern std::chrono::time_point<std::chrono::high_resolution_clock> g_start_time;
+#include "Video.h"
+#include <avcpp/av.h>
+#include <avcpp/codec.h>
+#include <avcpp/codeccontext.h>
+#include <avcpp/ffmpeg.h>
+#include <avcpp/formatcontext.h>
+Video::Video(std::string filename) : ctx(av::FormatContext()) {
+  fn = filename;
+  lprint({"open video file", " ", " fn='", fn, "'"}, __FILE__, __LINE__,
+         &(__PRETTY_FUNCTION__[0]));
+  ctx.openInput(fn);
+  ctx.findStreamInfo();
+  lprint({"stream info", " ", " ctx.seekable()='",
+          std::to_string(ctx.seekable()), "'", " ctx.startTime().seconds()='",
+          std::to_string(ctx.startTime().seconds()), "'",
+          " ctx.duration().seconds()='",
+          std::to_string(ctx.duration().seconds()), "'",
+          " ctx.streamsCount()='", std::to_string(ctx.streamsCount()), "'"},
+         __FILE__, __LINE__, &(__PRETTY_FUNCTION__[0]));
+  ctx.seek({static_cast<long int>(
+                floor(((100) * ((((0.50f)) * (ctx.duration().seconds())))))),
+            {1, 100}});
+  for (auto i = 0; (i) < (ctx.streamsCount()); (i) += (1)) {
+    auto st = ctx.stream(i);
+    if ((AVMEDIA_TYPE_VIDEO) == (st.mediaType())) {
+      videoStream = i;
+      vst = st;
+      break;
+    }
+  }
+  if (vst.isNull()) {
+    lprint({"Video stream not found", " "}, __FILE__, __LINE__,
+           &(__PRETTY_FUNCTION__[0]));
+  }
+  if (vst.isValid()) {
+    vdec = av::VideoDecoderContext(vst);
+    codec = av::findDecodingCodec(vdec.raw()->codec_id);
+    vdec.setCodec(codec);
+    vdec.setRefCountedFrames(true);
+    vdec.open({{"threads", "1"}}, av::Codec(), ec);
+    if (ec) {
+      lprint({"can't open codec", " "}, __FILE__, __LINE__,
+             &(__PRETTY_FUNCTION__[0]));
+    }
+  }
+}
+av::Packet Video::readPacket() {
+  pkt = ctx.readPacket(ec);
+  if (ec) {
+    lprint({"packet reading error", " ", " ec.message()='", ec.message(), "'"},
+           __FILE__, __LINE__, &(__PRETTY_FUNCTION__[0]));
+  }
+  return pkt;
+}
+av::VideoFrame Video::decode() {
+  auto frame = vdec.decode(pkt, ec);
+  if (ec) {
+    lprint({"error", " ", " ec.message()='", ec.message(), "'"}, __FILE__,
+           __LINE__, &(__PRETTY_FUNCTION__[0]));
+  }
+  return frame;
+}
+void Video::seek(float val) {
+  ctx.seek({static_cast<long int>(floor(((1000) * (val)))), {1, 1000}});
+}
+float Video::startTime() {
+  return static_cast<float>(ctx.startTime().seconds());
+}
+float Video::duration() { return static_cast<float>(ctx.duration().seconds()); }
+Video::~Video() {}
---
+++ example/85_glbinding_av/source01/Video.h
@@ -0,0 +1,29 @@
+#ifndef VIDEO_H
+#define VIDEO_H
+
+class GLFWwindow;
+#include <avcpp/av.h>
+#include <avcpp/ffmpeg.h>
+#include <avcpp/formatcontext.h>
+#include <avcpp/codec.h>
+#include <avcpp/codeccontext.h>
+class Video  {
+        std::string fn;
+        av::FormatContext ctx;
+        av::Stream vst;
+        av::Codec codec;
+        std::error_code ec;
+        av::VideoDecoderContext vdec;
+        av::Packet pkt;
+        public:
+        ssize_t videoStream = -1;
+        explicit  Video (std::string filename)     ;  
+        av::Packet readPacket ()     ;  
+        av::VideoFrame decode ()     ;  
+        void seek (float val)     ;  
+        float startTime ()     ;  
+        float duration ()     ;  
+         ~Video ()     ;  
+};
+
+#endif /* !VIDEO_H */
