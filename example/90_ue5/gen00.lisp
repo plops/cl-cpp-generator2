58baca91cfb0ffe306ca2bf59e97ac841051cb02
Martin Kielhorn <kielhorn.martin@gmail.com>
2022-12-03 19:32:57 +0100

    more code

--- example/90_ue5/gen00.lisp
+++ example/90_ue5/gen00.lisp
@@ -6,7 +6,7 @@
 
 (let ((log-preamble
        `(do0
-	 (include ;<iostream>
+	 (include			;<iostream>
 					;<iomanip>
 					;<chrono>
 					;<thread>
@@ -17,23 +17,14 @@
     ;; for classes with templates use write-source and defclass+
     ;; for cpp files without header use write-source
     ;; for class definitions and implementation in separate h and cpp file
-    (defparameter *source-dir* #P"example/89_jpeg/source00/")
+    (defparameter *source-dir* #P"example/90_ue5/source00/")
     (defparameter *full-source-dir* (asdf:system-relative-pathname
 				     'cl-cpp-generator2
 				     *source-dir*))
-
     (ensure-directories-exist *full-source-dir*)
     (load "util.lisp")
-
-    (let ((name `JxlEncode)
-	  (types `(;(:var enc :name JxlEncoderPtr :member t :ptr nil)
-		   #+nil
-		   (:var runner :name JxlThreadParallelRunnerPtr
-			 :member t :ptr nil)
-					;(:name JxlEncoderFrameSettings :member t)
-					;  (:var options :name JxlEncoderOptions :member t)
-					;(:name JxlEncoderError :member t)
-		   )))
+    (let ((name `AGameCharacter)
+	  )
       (write-class
        :dir (asdf:system-relative-pathname
 	     'cl-cpp-generator2
@@ -41,122 +32,54 @@
        :name name
        :headers `()
        :header-preamble `(do0
-			  (include <jxl/encode_cxx.h>
-				   <vector>)
-			  #+nil
-			  ,@(loop for e in types
-				  collect
-				  (destructuring-bind (&key var name member) e
-				    (format nil "struct ~a;" name)))
-			  )
+			  (include "CoreMinimal.h"
+				   "GameFramework/Pawn.h"
+				   "GameCharacter.generated.h"))
+       ;; note: the cpp implementation does not have to (or should
+       ;; not?) include AGameCharacter.h
+
        :implementation-preamble `(do0
-				  ,log-preamble
-				  " "
-
-				  (include <jxl/encode.h>
-					   <jxl/encode_cxx.h>
-					   <jxl/thread_parallel_runner.h>
-					   <jxl/thread_parallel_runner_cxx.h>
-
-					   <vector>)
-				  " "
-				  )
+				  (include "GameCharacter.h"))
        :code `(do0
-	       (defclass ,name ()
-		 "public:"
-					;"std::vector<uint8_t> compressed;"
-					;"size_t numWorkerThreads;"
+	       (defclass ,name (space public APawn)
+		 "GENERATED_BODY()"
 
-		 "JxlThreadParallelRunnerPtr runner;"
-		 ,@(loop for e in types
-			 collect
-			 (destructuring-bind (&key var name member (ptr t)) e
-			   (when member
-			     (format nil "~a ~:[~;*~]~a;"
-				     name
-				     ptr
-				     (if var
-					 var
-					 (string-downcase name))))))
+		 "public:"
+
 
 		 (defmethod ,name ()
 		   (declare
 					;  (explicit)
-		    #+nil (construct
-			   (enc (JxlEncoderMake nullptr))
-			   (numWorkerThreads (JxlThreadParallelRunnerDefaultNumWorkerThreads))
-			   (runner
-			    (JxlThreadParallelRunnerMake
-			     nullptr
-			     numWorkerThreads
-			     )))
+		    (construct)
 		    (values :constructor))
-		   ,(lprint :msg (format nil "~a constructor" name))
-		   (let ((enc (JxlEncoderMake nullptr))
+		   (setf PrimaryActorTick.bCanEveryTick true))
+		 "protected:"
 
-			 (runner (JxlThreadParallelRunnerMake nullptr
-							      4)))
-		     #+nil (setf runner (JxlThreadParallelRunnerMake nullptr
-								     4))
-		     (unless (== JXL_ENC_SUCCESS
-				 (JxlEncoderSetParallelRunner
-				  (enc.get)
-				  JxlThreadParallelRunner
-				  (runner.get)))
-		       ,(lprint :msg "parallel runner setting failed"))
-		     (progn
-		       "JxlBasicInfo basic_info;"
-		       (JxlEncoderInitBasicInfo
-			&basic_info)
-		       ,@(loop for (e f) in `((xsize 512)
-					      (ysize 256)
-					      (bits_per_sample 32)
-					      (exponent_bits_per_sample 8)
-					      (uses_original_profile
-					       JXL_FALSE))
-			       collect
-			       `(setf (dot basic_info ,e)
-				      ,f))
-		       (unless (== JXL_ENC_SUCCESS
-				   (JxlEncoderSetBasicInfo
-				    (enc.get)
-				    &basic_info))
-			 ,(lprint :msg "basic info failed")
-			 )
-		       (let ((*frame_settings (JxlEncoderFrameSettingsCreate
-					       (enc.get)
-					       nullptr)))
-			 (let ((pixel_format
-				(JxlPixelFormat (curly 3
-						       JXL_TYPE_FLOAT
-						       JXL_NATIVE_ENDIAN
-						       0)))))
-			 (unless (== JXL_ENC_SUCCESS
-				     (JxlEncoderAddImageFrame
-				      frame_settings
-				      &pixel_format
-				      nullptr 0))
-			   ,(lprint :msg "adding image frame failed")
-			   ))
-		       (JxlEncoderCloseInput (enc.get))
-		       ))
-		   )
-		 (defmethod Encode (;pixels
-					; width height
-				    )
-		   (declare (type "std::vector<float>" pixels)
-			    (type int width height))
+		 (defmethod BeginPlay ()
+		   (declare (values "virtual void")
+			    (override))
+		   (Super--BeginPlay))
 
-		   )
-		 #+nil (defmethod Reset ()
-			 (JxlEncoderReset (jxlencoderptr.get)))
-		 (defmethod ,(format nil "~~~a" name) ()
-		   (declare
-		    (values :constructor))
+		 "public:"
+		 (defmethod Tick (DeltaTime)
+		   (declare (type float DeltaTime)
+			    (values void)
+			    (virtual)
+			    (override))
+		   (Super--Tick DeltaTime))
 
-		   #+nil (JxlEncoderDestroy jxlencoder)))
+		 (defmethod SetupPlayerInputComponent (PlayerInputComponent)
+		   (declare (type "class UInputComponent*" PlayerInputComponent)
+			    (values void)
+			    (virtual)
+			    (override))
+		   (Super--SetupPlayerInputComponent PlayerInputComponent))
+
+
+		 )
 	       )))
 
+    #+nil
     (write-source
      (asdf:system-relative-pathname
       'cl-cpp-generator2
@@ -183,61 +106,16 @@
 					;(include "DCGANGeneratorImpl.h")
 	)
        (do0
-	(include "JxlEncode.h"))
+	)
 
        (defun main (argc argv)
 	 (declare (type int argc)
 		  (type char** argv)
 		  (values int))
 	 ,(lprint :msg "start" :vars `(argc))
-	 ,(let ((l `(;(:name kNoiseSize :default 100 :short n)
-		     )))
-	    `(let ((op (popl--OptionParser (string "allowed opitons")))
-		   ,@(loop for e in l collect
-			   (destructuring-bind (&key name default short) e
-			     `(,name (int ,default))))
-		   ,@(loop for e in `((:long help :short h :type Switch :msg "produce help message")
-				      (:long verbose :short v :type Switch :msg "produce verbose output")
-				      ,@(loop for f in l
-					      collect
-					      (destructuring-bind (&key name default short) f
-						`(:long ,name
-							:short ,short
-							:type int :msg "parameter"
-							:default ,default :out ,(format nil "&~a" name))))
-
-				      )
-			   appending
-			   (destructuring-bind (&key long short type msg default out) e
-			     `((,(format nil "~aOption" long)
-				 ,(let ((cmd `(,(format nil "add<~a>"
-							(if (eq type 'Switch)
-							    "popl::Switch"
-							    (format nil "popl::Value<~a>" type)))
-						(string ,short)
-						(string ,long)
-						(string ,msg))))
-				    (when default
-				      (setf cmd (append cmd `(,default)))
-				      )
-				    (when out
-				      (setf cmd (append cmd `(,out)))
-				      )
-				    `(dot op ,cmd)
-				    ))))
-			   ))
-	       (op.parse argc argv)
-	       (when (helpOption->count)
-		 (<< std--cout
-		     op
-		     std--endl)
-		 (exit 0))
-
-	       (let ((jxl (JxlEncode))))
-
-	       ))
 	 )))
 
+    #+nil
     (with-open-file (s (format nil "~a/CMakeLists.txt" *full-source-dir*)
 		       :direction :output
 		       :if-exists :supersede
@@ -254,7 +132,7 @@
 	    ;; https://stackoverflow.com/questions/8487986/file-macro-shows-full-path
 	    (short-file "" ;"-ffile-prefix-map=/home/martin/stage/cl-cpp-generator2/example/86_glbinding_av/source01/="
 	      )
-	    (show-err "-Wall -Wextra";
+	    (show-err "-Wall -Wextra"	;
 					;" -Wall -Wextra -Wcast-align -Wcast-qual -Wctor-dtor-privacy -Wdisabled-optimization -Wformat=2 -Winit-self  -Wmissing-declarations -Wmissing-include-dirs -Wold-style-cast -Woverloaded-virtual -Wredundant-decls -Wshadow -Wsign-conversion -Wswitch-default -Wundef -Werror -Wno-unused"
 					;"-Wlogical-op -Wnoexcept  -Wstrict-null-sentinel  -Wsign-promo-Wstrict-overflow=5  "
 
