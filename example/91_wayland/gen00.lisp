04dbb2bb0c994ba6ac18482788c364d0336f2ecd
Martin Kielhorn <kielhorn.martin@gmail.com>
2022-12-04 20:43:30 +0100

    wayland example builds

---
+++ example/91_wayland/gen00.lisp
@@ -0,0 +1,216 @@
+(eval-when (:compile-toplevel :execute :load-toplevel)
+  (ql:quickload "cl-cpp-generator2")
+  (ql:quickload "cl-ppcre"))
+
+(in-package :cl-cpp-generator2)
+
+(let ((log-preamble
+       `(do0
+	 (include			;<iostream>
+					;<iomanip>
+					;<chrono>
+					;<thread>
+	  <spdlog/spdlog.h>
+	  ))))
+
+  (progn
+    ;; for classes with templates use write-source and defclass+
+    ;; for cpp files without header use write-source
+    ;; for class definitions and implementation in separate h and cpp file
+    (defparameter *source-dir* #P"example/90_ue5/source00/")
+    (defparameter *full-source-dir* (asdf:system-relative-pathname
+				     'cl-cpp-generator2
+				     *source-dir*))
+    (ensure-directories-exist *full-source-dir*)
+    (load "util.lisp")
+    (let ((name `AGameCharacter)
+	  )
+      (write-class
+       :dir (asdf:system-relative-pathname
+	     'cl-cpp-generator2
+	     *source-dir*)
+       :name name
+       :headers `()
+       :header-preamble `(do0
+			  (include "CoreMinimal.h"
+				   "GameFramework/Pawn.h"
+				   "Camera/CameraComponent.h"
+				   )
+			  " "
+			  (comments "the ...generated.h file must always be included last")
+			  (include "GameCharacter.generated.h")
+			  )
+       ;; note: the cpp implementation does not have to (or should
+       ;; not?) include AGameCharacter.h
+
+       :implementation-preamble `(do0
+				  (include "GameCharacter.h"))
+       :code `(do0
+	       (defclass ,name (space public APawn)
+		 "GENERATED_BODY()"
+
+		 "public:"
+
+
+		 (defmethod ,name ()
+		   (declare
+					;  (explicit)
+		    (construct
+		     (Camera
+		      (CreateDefaultSubobject<UCameraComponent>
+		       (TEXT (string "Camera")))))
+		    (values :constructor))
+		   (comments "don't perform instance dependent things in constructor. unreal may not call the constructor for each instance")
+		   (setf PrimaryActorTick.bCanEveryTick true)
+
+		   #+nil
+		   (setf Camera
+			 (CreateDefaultSubobject<UCameraComponent>
+			  (TEXT (string "Camera"))))
+		   )
+		 "protected:"
+		 (comments "Main Pawn Camera, https://docs.unrealengine.com/5.0/en-US/API/Runtime/Engine/Camera/UCameraComponent/")
+
+		 (defmethod BeginPlay ()
+		   (declare (values "virtual void")
+			    (override))
+		   (Super--BeginPlay))
+
+		 "UPROPERTY(EditAnywhere)"
+		 "UCameraComponent* Camera;"
+
+		 "public:"
+		 (defmethod Tick (DeltaTime)
+		   (declare (type float DeltaTime)
+			    (values void)
+			    (virtual)
+			    (override))
+		   (Super--Tick DeltaTime))
+
+		 (defmethod SetupPlayerInputComponent (PlayerInputComponent)
+		   (declare (type "class UInputComponent*" PlayerInputComponent)
+			    (values void)
+			    (virtual)
+			    (override))
+		   (Super--SetupPlayerInputComponent PlayerInputComponent))
+
+
+		 )
+	       )))
+
+    #+nil
+    (write-source
+     (asdf:system-relative-pathname
+      'cl-cpp-generator2
+      (merge-pathnames #P"main.cpp"
+		       *source-dir*))
+     `(do0
+
+
+       (include
+					;<tuple>
+					;<mutex>
+					;<thread>
+	<iostream>
+					;<iomanip>
+					;<chrono>
+					;<cassert>
+					;  <memory>
+	)
+
+       (do0
+	(include <spdlog/spdlog.h>)
+	(include <popl.hpp>)
+					;(include <torch/torch.h>)
+					;(include "DCGANGeneratorImpl.h")
+	)
+       (do0
+	)
+
+       (defun main (argc argv)
+	 (declare (type int argc)
+		  (type char** argv)
+		  (values int))
+	 ,(lprint :msg "start" :vars `(argc))
+	 )))
+
+    #+nil
+    (with-open-file (s (format nil "~a/CMakeLists.txt" *full-source-dir*)
+		       :direction :output
+		       :if-exists :supersede
+		       :if-does-not-exist :create)
+      ;;https://clang.llvm.org/docs/AddressSanitizer.html
+      ;; cmake -DCMAKE_BUILD_TYPE=Debug -GNinja ..
+      ;;
+      (let ((dbg "-ggdb -O0 ")
+	    (asan ""
+					;"-fno-omit-frame-pointer -fsanitize=address -fsanitize-address-use-after-return=always -fsanitize-address-use-after-scope"
+	      )
+	    ;; make __FILE__ shorter, so that the log output is more readable
+	    ;; note that this can interfere with debugger
+	    ;; https://stackoverflow.com/questions/8487986/file-macro-shows-full-path
+	    (short-file "" ;"-ffile-prefix-map=/home/martin/stage/cl-cpp-generator2/example/86_glbinding_av/source01/="
+	      )
+	    (show-err "-Wall -Wextra"	;
+					;" -Wall -Wextra -Wcast-align -Wcast-qual -Wctor-dtor-privacy -Wdisabled-optimization -Wformat=2 -Winit-self  -Wmissing-declarations -Wmissing-include-dirs -Wold-style-cast -Woverloaded-virtual -Wredundant-decls -Wshadow -Wsign-conversion -Wswitch-default -Wundef -Werror -Wno-unused"
+					;"-Wlogical-op -Wnoexcept  -Wstrict-null-sentinel  -Wsign-promo-Wstrict-overflow=5  "
+
+	      ))
+	(macrolet ((out (fmt &rest rest)
+		     `(format s ,(format nil "~&~a~%" fmt) ,@rest)))
+	  (out "cmake_minimum_required( VERSION 3.0 FATAL_ERROR )")
+	  (out "project( mytest LANGUAGES CXX )")
+
+	  ;;(out "set( CMAKE_CXX_COMPILER clang++ )")
+					;(out "set( CMAKE_CXX_COMPILER g++ )")
+	  (out "set( CMAKE_VERBOSE_MAKEFILE ON )")
+	  (out "set (CMAKE_CXX_FLAGS_DEBUG \"${CMAKE_CXX_FLAGS_DEBUG} ~a ~a ~a ~a \")" dbg asan show-err short-file)
+	  (out "set (CMAKE_CXX_FLAGS \"${CMAKE_CXX_FLAGS} ~a ~a ~a ~a \")" dbg asan show-err short-file)
+	  (out "set (CMAKE_LINKER_FLAGS_DEBUG \"${CMAKE_LINKER_FLAGS_DEBUG} ~a ~a \")" dbg show-err )
+					;(out "set( CMAKE_CXX_STANDARD 23 )")
+
+
+
+	  (out "set( SRCS ~{~a~^~%~} )"
+	       (append
+		(directory (format nil "~a/*.cpp" *full-source-dir*))
+		))
+
+	  (out "add_executable( mytest ${SRCS} )")
+
+
+
+
+	  (out "set_property( TARGET mytest PROPERTY CXX_STANDARD 20 )")
+
+	  (out "target_link_options( mytest PRIVATE -static-libgcc -static-libstdc++   )")
+
+	  (out "find_package( PkgConfig REQUIRED )")
+	  (out "pkg_check_modules( spdlog REQUIRED spdlog )")
+	  (out "pkg_check_modules( jxl REQUIRED libjxl )")
+	  (out "pkg_check_modules( jxlt REQUIRED libjxl_threads )")
+
+	  (out "target_include_directories( mytest PRIVATE
+/usr/local/include/
+/home/martin/src/popl/include/
+ )")
+	  #+nil (progn
+		  (out "add_library( libnc SHARED IMPORTED )")
+		  (out "set_target_properties( libnc PROPERTIES IMPORTED_LOCATION /home/martin/stage/cl-cpp-generator2/example/88_libnc/dep/libnc-2021-04-24/libnc.so
+ )")
+		  )
+
+	  (out "target_link_libraries( mytest PRIVATE ~{~a~^ ~} )"
+	       `(spdlog
+		 jxl
+		 jxl_threads
+		 ))
+
+	  #+nil
+	  (out "target_compile_options( mytest PRIVATE ~{~a~^ ~} )"
+	       `())
+
+					;(out "target_precompile_headers( mytest PRIVATE vis_00_base.hpp )")
+	  ))
+      )))
+
