86aa0548f0089b5f9eb4c0b8406a2db20a4fb620
Martin Kielhorn <kielhorn.martin@gmail.com>
2023-01-16 22:57:39 +0100

    write some test code for the recent changes

---
+++ example/102_simple_test/gen00.lisp
@@ -0,0 +1,32 @@
+(eval-when (:compile-toplevel :execute :load-toplevel)
+	   (ql:quickload "cl-cpp-generator2"))
+
+(in-package :cl-cpp-generator2)
+
+(progn
+  (defparameter *source-dir* #P"example/102_simple_test/source00/")
+  (defparameter *full-source-dir* (asdf:system-relative-pathname
+				   'cl-cpp-generator2
+				   *source-dir*))
+  (ensure-directories-exist *full-source-dir*)
+
+  (write-source
+   (asdf:system-relative-pathname
+    'cl-cpp-generator2
+    (merge-pathnames #P"main.cpp"
+		     *source-dir*))
+   `(do0
+     (include
+      ,@(loop for e in `(iostream)
+	      collect
+	      (format nil "<~a>" e)))
+     (defun main (argc argv)
+       (declare (type int argc)
+		(type **char argv)
+		(values int))
+       (for-range (th threads)
+		  (declare (type "auto&" th))
+		  (th.join)
+		  (<< std--cout (string "bla")))
+       ))))
+
