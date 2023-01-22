f344ef5a9337bf4d91a2d1a4084d16ddf5d37084
Martin Kielhorn <kielhorn.martin@gmail.com>
2023-01-22 02:22:11 +0100

    code compiles but only opens a non-functioning window

--- example/103_xcb/gen00.lisp
+++ example/103_xcb/gen00.lisp
@@ -28,12 +28,17 @@
 	 
 	 (let ((*screen (dot (xcb_setup_roots_iterator (xcb_get_setup conn))
 			     data))
-               (*win (xcb_generate_id conn)))
+               (win (xcb_generate_id conn)))
+	   ;(declare (type xcb_window_t win))
            (xcb_create_window conn XCB_COPY_FROM_PARENT win screen->root 0 0
 			      600 400 0 XCB_WINDOW_CLASS_INPUT_OUTPUT
 			      screen->root_visual 0 nullptr)
-           (xcb_map_window conn win)
-           (xcb_flush conn))
+	   (let ((gc (xcb_generate_id conn))
+		 )
+	     (declare (type xcb_gcontext_t gc))
+	     (xcb_create_gc conn gc win XCB_GC_FOREGROUND &screen->black_pixel)
+             (xcb_map_window conn win)
+             (xcb_flush conn)))
 	 (while true
 	  (let ((*event (xcb_wait_for_event conn)))
             (unless event
@@ -43,18 +48,25 @@
 	      (XCB_EXPOSE
 	       (xcb_clear_area conn 0 win 0 0 0 0))
 	      (XCB_KEY_PRESS
-	       (let ((*key (reinterpret_cast<xcb_key_press_event_t*> event))
+	       (let (;(*key (reinterpret_cast<xcb_key_press_event_t*> event))
+		     (geom_c (xcb_get_geometry conn win))
 		     (*geom (xcb_get_geometry_reply conn
-						    (xcb_get_geometry conn win)
+						    geom_c
 						    nullptr)))
-		 (xcb_change_gc conn (xcb_generate_id conn)
-				win XCB_GC_FOREGROUND &screen->white_pixel)
-		 (xcb_image_text_8 conn
-				   (strlen (string "Hello World"))
-				   win
-				   geom->x geom->y (string "Hello World"))
-		 (xcb_flush conn))))
-	    
-	    (xcb_disconnect conn)
-	    (return 0))))))))
+		 (xcb_change_gc conn gc
+				 XCB_GC_FOREGROUND &screen->white_pixel)
+		 (let ((s (string "Hello World")))
+		   (declare (type "const char*" s))
+		   (xcb_image_text_8 conn
+				     (strlen s)
+				     win
+				     gc
+				     geom->x geom->y s))
+		 (xcb_flush conn)
+		 (free geom))))
+	    (free event)
+	    ))
+	 (do0
+	     (xcb_disconnect conn)
+	     (return 0)))))))
 
--- example/103_xcb/source00/main.cpp
+++ example/103_xcb/source00/main.cpp
@@ -4,19 +4,22 @@
 
 int main() {
   auto *conn = xcb_connect(nullptr, nullptr);
-  if (xcb_connection_has_error(conn)) {
+  if (xcb_connection_has_error(conn) != 0) {
     return 1;
   }
   auto *screen = xcb_setup_roots_iterator(xcb_get_setup(conn)).data;
-  auto *win = xcb_generate_id(conn);
+  auto win = xcb_generate_id(conn);
   xcb_create_window(conn, XCB_COPY_FROM_PARENT, win, screen->root, 0, 0, 600,
                     400, 0, XCB_WINDOW_CLASS_INPUT_OUTPUT, screen->root_visual,
                     0, nullptr);
+  xcb_gcontext_t gc = xcb_generate_id(conn);
+  xcb_create_gc(conn, gc, win, XCB_GC_FOREGROUND, &screen->black_pixel);
   xcb_map_window(conn, win);
   xcb_flush(conn);
+
   while (true) {
     auto *event = xcb_wait_for_event(conn);
-    if (!(event)) {
+    if ((event) == nullptr) {
       break;
     }
     switch (((event->response_type) & (~(0x80)))) {
@@ -25,19 +28,19 @@ int main() {
       break;
     }
     case XCB_KEY_PRESS: {
-      auto *key = reinterpret_cast<xcb_key_press_event_t *>(event);
-      auto *geom =
-          xcb_get_geometry_reply(conn, xcb_get_geometry(conn, win), nullptr);
-      xcb_change_gc(conn, xcb_generate_id(conn), win, XCB_GC_FOREGROUND,
-                    &screen->white_pixel);
-      xcb_image_text_8(conn, strlen("Hello World"), win, geom->x, geom->y,
-                       "Hello World");
+      auto geom_c = xcb_get_geometry(conn, win);
+      auto *geom = xcb_get_geometry_reply(conn, geom_c, nullptr);
+      xcb_change_gc(conn, gc, XCB_GC_FOREGROUND, &screen->white_pixel);
+      const char *s = "Hello World";
+      xcb_image_text_8(conn, strlen(s), win, gc, geom->x, geom->y, s);
       xcb_flush(conn);
+      free(geom);
 
       break;
     }
     }
-    xcb_disconnect(conn);
-    return 0;
+    free(event);
   }
+  xcb_disconnect(conn);
+  return 0;
 }
