51e09df044cc40d786e61a2198570aeed3573aa0
Martin Kielhorn <kielhorn.martin@gmail.com>
2023-01-24 02:00:56 +0100

    i can't get it compiled
    martin@mx:~/stage/cl-cpp-generator2/example/105_ceres_ad/source00
    $ ./compile.sh
    main.cpp: In function ‘double objective(const VectorXd&)’:
    main.cpp:7:18: error: no match for ‘operator+’ (operand types are ‘const double’ and ‘Eigen::internal::enable_if<true, const Eigen::CwiseBinaryOp<Eigen::internal::scalar_product_op<double, double>, const Eigen::CwiseNullaryOp<Eigen::internal::scalar_constant_op<double>, const Eigen::Matrix<double, -1, 1> >, const Eigen::Matrix<double, -1, 1> > >::type’ {aka ‘const Eigen::CwiseBinaryOp<Eigen::internal::scalar_product_op<double, double>, const Eigen::CwiseNullaryOp<Eigen::internal::scalar_constant_op<double>, const Eigen::Matrix<double, -1, 1> >, const Eigen::Matrix<double, -1, 1> >’})
        7 |   auto f = (x(0) + (x(1) * y));
          |             ~~~~ ^ ~~~~~~~~~~
          |              |           |
          |              |           Eigen::internal::enable_if<true, const Eigen::CwiseBinaryOp<Eigen::internal::scalar_product_op<double, double>, const Eigen::CwiseNullaryOp<Eigen::internal::scalar_constant_op<double>, const Eigen::Matrix<double, -1, 1> >, const Eigen::Matrix<double, -1, 1> > >::type {aka const Eigen::CwiseBinaryOp<Eigen::internal::scalar_product_op<double, double>, const Eigen::CwiseNullaryOp<Eigen::internal::scalar_constant_op<double>, const Eigen::Matrix<double, -1, 1> >, const Eigen::Matrix<double, -1, 1> >}
          |              const double

--- example/105_ceres_ad/source00/main.cpp
+++ example/105_ceres_ad/source00/main.cpp
@@ -1,66 +1,13 @@
 #include <Eigen/Dense>
 using namespace Eigen;
-do0;
 
 double objective(const VectorXd &x) {
   auto y = VectorXd(3);
-  auto f = VectorXd(3);
   y << 1, 2, 3;
-  f = (x(0) + (x(1) * y));
-
+  auto f = (x(0) + (x(1) * y));
   auto err = ((y) - (f));
 
   return err.squaredNorm();
 }
 
-VectorXd gradient(const VectorXd &x) {
-  auto y = VectorXd(3);
-  auto f = VectorXd(3);
-  y << 1, 2, 3;
-  f = (x(0) + (x(1) * y));
-
-  auto err = ((y) - (f));
-
-  auto result = VectorXd(2);
-  result(0) = (-2 * err.sum());
-  result(1) = (-2 * (err.transpose() * y).sum());
-
-  return result;
-}
-
-MatrixXd hessian(const VectorXd &x) {
-  auto y = VectorXd(3);
-  auto f = VectorXd(3);
-  y << 1, 2, 3;
-  f = (x(0) + (x(1) * y));
-
-  auto err = ((y) - (f));
-
-  auto result = MatrixXd(2, 2);
-  result(0, 0) = (2 * y.size());
-  result(0, 1) = (2 * y.sum());
-  result(1, 0) = (2 * y.sum());
-  result(1, 1) = (2 * (y.array() * y.array()).sum());
-
-  return result;
-}
-
-int main(int argc, char **argv) {
-  auto lam = (0.100000000000000000000000000000);
-  auto x = VectorXd(2);
-  auto tolerance = (1.00e-4f);
-  auto maxIterations = 100;
-  x << 1, 1;
-  for (auto i = 0; i < maxIterations; i += 1) {
-    auto grad = gradient(x);
-    auto hess = hessian(x);
-    hess += (lam * MatrixXd::Identity(x.size(), x.size()));
-    auto step = (-hess.ldlt().solve(grad))();
-    x += step;
-    auto change = step.norm();
-    if (change < tolerance) {
-      break;
-    }
-    lam *= ((0.90f));
-  }
-}
+int main(int argc, char **argv) {}
