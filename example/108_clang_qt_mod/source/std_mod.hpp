3c2998688b7982b025b3cff80cec0db6e060d5cb
Martin Kielhorn <kielhorn.martin@gmail.com>
2023-01-28 15:14:55 +0100

    26M

--- example/108_clang_qt_mod/source/std_mod.hpp
+++ example/108_clang_qt_mod/source/std_mod.hpp
@@ -9,65 +9,65 @@
 #include <QPushButton>
 #include <QVBoxLayout>
 
-// #include <QAction>
+#include <QAction>
 
-// #include <QButtonGroup>
-// #include <QCheckBox>
-// #include <QColor>
+#include <QButtonGroup>
+#include <QCheckBox>
+#include <QColor>
 
-// #include <QDir>
-// #include <QDrag>
-// #include <QFileDialog>
-// #include <QFrame>
+#include <QDir>
+#include <QDrag>
+#include <QFileDialog>
+#include <QFrame>
 
-// #include <QHeaderView>
-// #include <QLabel>
+#include <QHeaderView>
+#include <QLabel>
 
-// #include <QMap>
-// #include <QMimeData>
-// #include <QMouseEvent>
-// #include <QPointer>
+#include <QMap>
+#include <QMimeData>
+#include <QMouseEvent>
+#include <QPointer>
 
-// #include <QRadioButton>
-// #include <QSpacerItem>
-// #include <QStackedWidget>
-// #include <QStringList>
-// #include <QStyle>
-// #include <QTabWidget>
-// #include <QTimer>
-// #include <QToolButton>
-// #include <QToolTip>
+#include <QRadioButton>
+#include <QSpacerItem>
+#include <QStackedWidget>
+#include <QStringList>
+#include <QStyle>
+#include <QTabWidget>
+#include <QTimer>
+#include <QToolButton>
+#include <QToolTip>
 
-// #include <QValidator>
-// #include <QVariant>
-// #include <QVector>
-// #include <algorithm>
-// #include <array>
-// #include <bitset>
-// #include <cassert>
-// #include <cfloat>
-// #include <cmath>
-// #include <complex>
-// #include <condition_variable>
-// #include <cstddef>
-// #include <cstdint>
-// #include <cstdlib>
-// #include <functional>
-// #include <initializer_list>
-// #include <iterator>
-// #include <map>
-// #include <memory>
-// #include <mutex>
-// #include <numeric>
-// #include <set>
+#include <QValidator>
+#include <QVariant>
+#include <QVector>
+#include <algorithm>
+#include <array>
+#include <bitset>
+#include <cassert>
+#include <cfloat>
+#include <cmath>
+#include <complex>
+#include <condition_variable>
+#include <cstddef>
+#include <cstdint>
+#include <cstdlib>
+#include <functional>
+#include <initializer_list>
+#include <iterator>
+#include <map>
+#include <memory>
+#include <mutex>
+#include <numeric>
+#include <set>
 
-// #include <string>
-// #include <thread>
-// #include <type_traits>
-// #include <unordered_map>
-// #include <vector>
+#include <string>
+#include <thread>
+#include <type_traits>
+#include <unordered_map>
+#include <vector>
 
 //#include <spdlog/spdlog.h> // slows module down
 
-// 22M  std_mod.pcm
+// 26M  std_mod.pcm
 
