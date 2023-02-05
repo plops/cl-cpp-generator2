38d73969024c4bc012c5772d8f6af15f9517892b
Martin Kielhorn <kielhorn.martin@gmail.com>
2023-02-05 23:45:17 +0100

    not sure if this microbenchmark makes sense

--- example/115_microbench/source00/main.cpp
+++ example/115_microbench/source00/main.cpp
@@ -1,16 +1,17 @@
 #include <array>
 #include <chrono>
+#include <cmath>
 #include <iostream>
-constexpr int N = 1048576;
+constexpr int64_t N = 1048576;
 
-constexpr int ITER = 1000;
+constexpr int64_t ITER = 10000;
 
 int main(int argc, char **argv) {
   auto array = std::array<int, N>();
   for (auto i = 0; i < N; i += 1) {
     array[i] = i;
   }
-  auto sum = int(0);
+  auto sum = int32_t(0);
   auto start = std::chrono::high_resolution_clock::now();
   for (auto j = 0; j < ITER; j += 1) {
     for (auto i = 0; i < N; i += 1) {
@@ -19,8 +20,10 @@ int main(int argc, char **argv) {
   }
   auto end = std::chrono::high_resolution_clock::now();
   auto elapsed = ((end) - (start));
-  std::cout << "elapsed: " << elapsed.count() << " ns" << std::endl;
-  std::cout << "sum: " << sum << std::endl;
+  std::cout << "time per iteration and element: "
+            << ((elapsed.count()) / (static_cast<double>((N * ITER)))) << " ns"
+            << std::endl;
+  std::cout << "sum: " << sum << " N: " << N << std::endl;
 
   return 0;
 }
