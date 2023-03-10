e769d925631ae7ee713a4543bf96369afbb83a0b
Martin Kielhorn <martin.kielhorn@ideapad>
2023-03-10 07:17:51 +0100

    more changes

--- example/118_opencascade/gen00.lisp
+++ example/118_opencascade/gen00.lisp
@@ -21,7 +21,8 @@
 		     *source-dir*))
    `(do0
      (include<> vector
-		GL/glut.h)
+		GL/glut.h
+		memory)
      (include<> ,@(loop for e in `(Standard_Version
 				   TopoDS
 				   TopoDS_Edge
@@ -36,12 +37,32 @@
 				   TopTools_ListOfShape)
 			collect
 			(format nil "opencascade/~a.hxx" e)))
+
+     (defun display ()
+       (let ((knots (std--vector<double> (curly "0.0"
+						"0.0"
+						"0.0"
+						"1.0"
+						"2.0"
+						"3.0"
+						"3.0"
+						"3.0")))
+	     (control_points (std--vector<gp_Pnt> (curly
+						   (gp_Pnt "0.0" "0.0" "0.0")
+						   (gp_Pnt "1.0" "2.0" "0.0")
+						   (gp_Pnt "2.0" "-1.0" "0.0")
+						   (gp_Pnt "3.0" "0.0" "0.0"))))
+	     (points (std--make_shared<TColgp_HArray1OfPnt>
+		      1 (control_points.size))))
+	 (dotimes (i (control_points.size))
+	   (points->SetValue (+ 1 i)
+			     (aref control_points i)))))
      
      (defun main (argc argv)
        (declare (type int argc)
 		(type char** argv)
 		(values int))
-       
+       (display)
        (return 0))))
   )
 
--- example/118_opencascade/source00/main.cpp
+++ example/118_opencascade/source00/main.cpp
@@ -1,4 +1,5 @@
 #include <GL/glut.h>
+#include <memory>
 #include <opencascade/Geom_BSplineCurve.hxx>
 #include <opencascade/Geom_CartesianPoint.hxx>
 #include <opencascade/IGESControl_Reader.hxx>
@@ -13,4 +14,18 @@
 #include <opencascade/TopoDS_Edge.hxx>
 #include <vector>
 
-int main(int argc, char **argv) { return 0; }
+void display() {
+  auto knots = std::vector<double>({0.0, 0.0, 0.0, 1.0, 2.0, 3.0, 3.0, 3.0});
+  auto control_points =
+      std::vector<gp_Pnt>({gp_Pnt(0.0, 0.0, 0.0), gp_Pnt(1.0, 2.0, 0.0),
+                           gp_Pnt(2.0, -1.0, 0.0), gp_Pnt(3.0, 0.0, 0.0)});
+  auto points = std::make_shared<TColgp_HArray1OfPnt>(1, control_points.size());
+  for (auto i = 0; i < control_points.size(); i += 1) {
+    points->SetValue((1 + i), control_points[i]);
+  }
+}
+
+int main(int argc, char **argv) {
+  display();
+  return 0;
+}
