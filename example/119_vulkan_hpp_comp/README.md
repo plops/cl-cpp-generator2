99ac450e553c59e991682333eaa9cf5141bfe771
Martin Kielhorn <martin.kielhorn@ideapad>
2023-03-11 09:01:30 +0100

    start with vulkan compute example

---
+++ example/119_vulkan_hpp_comp/README.md
@@ -0,0 +1,5 @@
+vulkan compute
+https://www.youtube.com/watch?v=0Fv0jDZUyE4
+libvc kompute vuda
+vkfft
+5:40 minimal example using vulkan.hpp
---
+++ example/119_vulkan_hpp_comp/gen00.lisp
@@ -0,0 +1,38 @@
+(eval-when (:compile-toplevel :execute :load-toplevel)
+	   (ql:quickload "cl-cpp-generator2")
+	   (ql:quickload "cl-ppcre")
+	   (ql:quickload "cl-change-case"))
+
+(in-package :cl-cpp-generator2)
+
+(progn
+  (defparameter *source-dir* #P"example/119_vulkan_hpp_comp/source00/")
+  (defparameter *full-source-dir* (asdf:system-relative-pathname
+				   'cl-cpp-generator2
+				   *source-dir*))
+  (ensure-directories-exist *full-source-dir*)
+  
+  ;(load "util.lisp")
+  
+  (write-source
+   (asdf:system-relative-pathname
+    'cl-cpp-generator2
+    (merge-pathnames #P"main.cpp"
+		     *source-dir*))
+   `(do0
+     (include<> vector
+		GL/glut.h
+		memory)
+     
+
+     
+     (defun main (argc argv)
+       (declare (type int argc)
+		(type char** argv)
+		(values int))
+
+       (return 0))))
+  )
+
+
+  
