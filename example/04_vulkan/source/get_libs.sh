d054bf3b49f1116a1aae601c8bb124ad055923fb
Martin Kielhorn <martin.kielhorn@ideapad>
2023-03-11 09:46:53 +0100

    download model and texture from somewhere else
    the old vulkan example doesn't work anymore
    
    1678524375298659228 vkCmdPipelineBarrier12
    endSingleTimeCommands 0x555cfe699850
    1678524375305839420 call createTextureImageView
    1678524375305885027 run_01_base.cpp:1307 createImageView create image-view imageView=0xa7c5450000000023
    1678524375305892011 call createTextureSampler
    1678524375305925395 run_01_base.cpp:782 createTextureSampler create sampler _textureSampler=0xb991fa0000000024
    1678524375305932519 call loadModel
    1678524375306147282 call createVertexBuffer
    VUID-VkBufferCreateInfo-size-00912(ERROR / SPEC): msgNum: 2085897310 - Validation Error: [ VUID-VkBufferCreateInfo-size-00912 ] Object 0: handle = 0x555cfe486670, type = VK_OBJECT_TYPE_DEVICE; | MessageID = 0x7c54445e | vkCreateBuffer: parameter pCreateInfo->size (= 0) is not greater than 0 The Vulkan spec states: size must be greater than 0 (https://www.khronos.org/registry/vulkan/specs/1.3-extensions/html/vkspec.html#VUID-VkBufferCreateInfo-size-00912)
        Objects: 1

--- example/04_vulkan/source/get_libs.sh
+++ example/04_vulkan/source/get_libs.sh
@@ -1,6 +1,8 @@
 curl -O https://raw.githubusercontent.com/nothings/stb/master/stb_image.h
 curl -O https://vulkan-tutorial.com/images/texture.jpg
 curl -O https://raw.githubusercontent.com/syoyo/tinyobjloader/master/tiny_obj_loader.h
-curl -O https://vulkan-tutorial.com/resources/chalet.obj.zip
-unzip chalet.obj.zip
-curl -O https://vulkan-tutorial.com/resources/chalet.jpg
+#curl -O https://vulkan-tutorial.com/resources/chalet.obj.zip
+#unzip chalet.obj.zip
+#curl -O https://vulkan-tutorial.com/resources/chalet.jpg
+curl -O https://raw.githubusercontent.com/heitaoflower/vulkan-tutorial/master/Tutorial29/textures/chalet.jpg
+curl -O https://github.com/heitaoflower/vulkan-tutorial/raw/master/Tutorial29/models/chalet.obj
