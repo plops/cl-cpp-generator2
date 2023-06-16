da166e4c51372f7e402653ec084d585d1effe0d1
Martin Kielhorn <martin.kielhorn@ideapad>
2023-06-16 19:37:08 +0200

    define some more tests of paren*
    00 (3*(1+2)) should be 3*(1+2)
    01 ((3+4)*3*(1+2)) should be (3+4)*3*(1+2)
    02 ((3+4)*((3)/(4))*(((1+2))/(5))) should be (3+4)*3/4*(1+2)/5
    03 (3*5)%4 works
    04 74%(3*5) works

--- t/01_paren/gen00.lisp
+++ t/01_paren/gen00.lisp
@@ -17,31 +17,38 @@
       "Sunday"))
   (ensure-directories-exist *full-source-dir*)
 
-  (loop for e in `((:name basic1 :code (* 3 (+ 1 2)) :reference "3*(1+2)")
-		   (:name basic2 :code (* (+ 3 4) 3 (+ 1 2)) :reference "(3+4)*3*(1+2)")
-		   (:name basic3 :code (* (+ 3 4) (/ 3 4) (/ (+ 1 2) 5)) :reference "(3+4)*3/4*(1+2)/5"))
-	and e-i from 0
-	do
-	   (destructuring-bind (&key code name (lisp-code code) reference) e
-	     (let ((emit-str (emit-c :code code)))
-	       (unless (string= emit-str reference)
-		 (format t "~a should be ~a"
-			 emit-str reference))
-	       (write-source
-		(asdf:system-relative-pathname
-		 'cl-cpp-generator2
-		 (merge-pathnames (format nil "c~2,'0d_~a.cpp" e-i name)
-				  *source-dir*))
-		`(do0
-		  (include<> cassert)
-		  (defun main (argc argv)
-		    (declare (values int)
-			     (type int argc)
-			     (type char** argv))
-		    (comments ,reference)
-		    (assert (== ,code
-				,(eval lisp-code)))
-		    (return 0))))))))
+  (with-open-file (s "/home/martin/stage/cl-cpp-generator2/t/01_paren/source00/strings.txt"
+			    :if-exists :supersede
+			    :if-does-not-exist :create
+			     :direction :output)
+   (loop for e in `((:name basic1 :code (* 3 (+ 1 2)) :reference "3*(1+2)")
+		    (:name basic2 :code (* (+ 3 4) 3 (+ 1 2)) :reference "(3+4)*3*(1+2)")
+		    (:name basic3 :code (* (+ 3 4) (/ 3 4) (/ (+ 1 2) 5)) :reference "(3+4)*3/4*(1+2)/5")
+		    (:name mod1 :code (% (* 3 5) 4) :lisp-code (mod (* 3 5) 4) :reference "(3*5)%4")
+		    (:name mod1 :code (% 74 (* 3 5)) :lisp-code (mod 74 (* 3 5)) :reference "74%(3*5)"))
+	 and e-i from 0
+	 do
+	    (destructuring-bind (&key code name (lisp-code code) reference) e
+	      (let ((emit-str (emit-c :code code)))
+		(if (string= emit-str reference)
+		    (format s "~2,'0d ~a works~%" e-i emit-str)
+		    (format s "~2,'0d ~a should be ~a~%" e-i
+			    emit-str reference))
+		(write-source
+		 (asdf:system-relative-pathname
+		  'cl-cpp-generator2
+		  (merge-pathnames (format nil "c~2,'0d_~a.cpp" e-i name)
+				   *source-dir*))
+		 `(do0
+		   (include<> cassert)
+		   (defun main (argc argv)
+		     (declare (values int)
+			      (type int argc)
+			      (type char** argv))
+		     (comments ,reference)
+		     (assert (== ,code
+				 ,(eval lisp-code)))
+		     (return 0)))))))))
 
 
 
