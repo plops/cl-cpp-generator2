cc3302286a59d3894447f497a274997946df2291
Martin Kielhorn <martin.kielhorn@ideapad>
2023-06-17 23:59:55 +0200

    more changes

--- example/127_opencv_amd/gen00.lisp
+++ example/127_opencv_amd/gen00.lisp
@@ -17,6 +17,112 @@
       "Sunday"))
   (ensure-directories-exist *full-source-dir*)
   (load "util.lisp")
+
+  (let ((interface-name "CheckerboardDisplayInterface")
+	(name `ArucoCheckerboardDisplay)
+	(members `((squares-x :type int :param t)
+		   (squares-y :type int :param t)
+		   (square-length :type int :param t)
+		   (dictionary :type "cv::Ptr<cv::aruco::Dictionary>" :param t)
+		   (board-size :type "cv::Size" :initform (paren squaresX squaresY))
+		   (board-image :type "cv::Mat")
+		   )))
+
+    (write-source 
+     (asdf:system-relative-pathname
+      'cl-cpp-generator2
+      (merge-pathnames (format nil "~a.hpp"
+			       interface-name)
+		       *source-dir*))
+     `(do0
+       "#pragma once"
+       (include<> 
+	opencv2/aruco.hpp)
+       (defclass+ ,interface-name ()
+	 "public:"
+	 ,(format nil "virtual ~~~a() = default;" interface-name)
+	 (space virtual void
+		(displayCheckerboard
+		 "int squaresX"
+		 "int squaresY"
+		 "int squareLength"
+		 "cv::Ptr<cv::aruco::Dictionary> dictionary")
+		"= 0;")))
+     )
+    (write-class
+     :dir (asdf:system-relative-pathname
+	   'cl-cpp-generator2
+	   *source-dir*)
+     :name name
+     :headers `()
+     :header-preamble `(do0
+			(include ,(format nil "~a.hpp" interface-name)))
+     :implementation-preamble
+     `(do0
+       )
+     :code `(do0
+	     (defclass ,name ()	 
+	       "public:"
+	       #+nil (defmethod ,name (,@(remove-if #'null
+				    (loop for e in members
+					  collect
+					  (destructuring-bind (name &key type param (initform 0)) e
+					    (let ((nname (intern
+							  (string-upcase
+							   (cl-change-case:snake-case (format nil "~a" name))))))
+					      (when param
+						nname))))))
+		 (declare
+		  ,@(remove-if #'null
+			       (loop for e in members
+				     collect
+				     (destructuring-bind (name &key type param (initform 0)) e
+				       (let ((nname (intern
+						     (string-upcase
+						      (cl-change-case:snake-case (format nil "~a" name))))))
+					 (when param
+					   
+					   `(type ,type ,nname))))))
+		  (construct
+		   ,@(remove-if #'null
+				(loop for e in members
+				      collect
+				      (destructuring-bind (name &key type param (initform 0)) e
+					(let ((nname (cl-change-case:snake-case (format nil "~a" name)))
+					      (nname_ (format nil "~a_"
+							      (cl-change-case:snake-case (format nil "~a" name)))))
+					  (cond
+					    (param
+					     `(,nname_ ,nname))
+					    (initform
+					     `(,nname_ ,initform)))))))
+		   )
+		  (explicit)	    
+		  (values :constructor))
+		 
+		 )
+		 (defmethod displayCheckerboard (squaresX squaresY squareLength dictionary)
+		   (declare (type int squaresX squaresY squareLength)
+			    (type "cv::Ptr<cv::aruco::Dictionary>" dictionary))
+		   (cv--aruco--drawCharucoBoard boardSize
+						squareLength
+					       (/ squareLength 2)
+					       dictionary
+					       boardImage)
+		   (cv--imshow (string "checkerboard")
+			       boardImage)
+		   (cv--waitKey 0))
+	       "private:"
+	       ,@(remove-if #'null
+			    (loop for e in members
+				  collect
+				  (destructuring-bind (name &key type param (initform 0)) e
+				    (let ((nname (cl-change-case:snake-case (format nil "~a" name)))
+					  (nname_ (format nil "~a_" (cl-change-case:snake-case (format nil "~a" name)))))
+				      `(space ,type ,nname_)))))
+
+	       ))))
+  
   (write-source 
    (asdf:system-relative-pathname
     'cl-cpp-generator2
@@ -24,8 +130,8 @@
 		     *source-dir*))
    `(do0
      (include<> 
-		iostream
-		opencv2/core/ocl.hpp)
+      iostream
+      opencv2/core/ocl.hpp)
      
      (defun main (argc argv)
        (declare (values int)
