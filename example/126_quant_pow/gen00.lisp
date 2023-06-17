4b516084ff40b223c05303caadb59d780cfe6f15
Martin Kielhorn <martin.kielhorn@ideapad>
2023-06-17 14:58:55 +0200

    first bug
    The variable op is unbound.
       [Condition of type unbound-variable]
    
    I think this is in (H i i)

--- example/126_quant_pow/gen00.lisp
+++ example/126_quant_pow/gen00.lisp
@@ -45,7 +45,20 @@
        (let ((N 1000)
 	     (L 1d0)
 	     (dx (/ L (+ N 1)))
-	     (H (arma--sp_mat N N))))
+	     (H (arma--sp_mat N N)))
+	 (dotimes (i N)
+	   (when (< 0 i)
+	     (setf (H i (- i 1))
+		   (/ -1d0
+		      (* dx dx))))
+	   (setf (H i i)
+		 (/ 2d0
+		    (* dx dx)))
+	   (when (< i (- N 1))
+	     (setf (H i (+ i 1))
+		   (/ -1d0
+		      (* dx dx)))))
+	 (let ((psi ("arma::randu<arma::vec>" N)))))
        
        (return 0)))
    :format t
