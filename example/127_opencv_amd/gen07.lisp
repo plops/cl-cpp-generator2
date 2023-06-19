d65bc5c21ddd9f13959c7f79be995ebd414e9a90
Martin Kielhorn <martin.kielhorn@ideapad>
2023-06-19 23:21:46 +0200

    more code

---
+++ example/127_opencv_amd/gen07.lisp
@@ -0,0 +1,126 @@
+(eval-when (:compile-toplevel :execute :load-toplevel)
+  (ql:quickload "cl-cpp-generator2")
+  (ql:quickload "cl-ppcre")
+  (ql:quickload "cl-change-case"))
+
+(in-package :cl-cpp-generator2)
+
+(progn
+  (progn
+    (defparameter *source-dir* #P"example/127_opencv_amd/source07/")
+    (defparameter *full-source-dir* (asdf:system-relative-pathname
+				     'cl-cpp-generator2
+				     *source-dir*)))
+  (defparameter *day-names*
+    '("Monday" "Tuesday" "Wednesday"
+      "Thursday" "Friday" "Saturday"
+      "Sunday"))
+  (ensure-directories-exist *full-source-dir*)
+  (load "util.lisp")
+    
+  (write-source 
+   (asdf:system-relative-pathname
+    'cl-cpp-generator2
+    (merge-pathnames "main.cpp"
+		     *source-dir*))
+   `(do0
+     (include<>
+      iostream
+      opencv2/opencv.hpp
+      opencv2/aruco/charuco.hpp
+      opencv2/aruco.hpp
+      )
+
+     "using namespace cv;"
+     
+     
+     (defun main (argc argv)
+       (declare (values int)
+		(type int argc)
+		(type char** argv))
+       "(void) argc;"
+       "(void) argv;"
+       (let ((camera (VideoCapture 0))
+	     (title (string "Webcam"))
+	     )
+
+	 (let ((x 8)
+	     (y 3)
+	     (square_len 4s-2)
+	     (dict (aruco--getPredefinedDictionary
+		    aruco--DICT_6X6_250))
+	     (board
+	       (new
+		(aruco--CharucoBoard
+		 (Size x y) square_len
+		 (* .5 square_len)
+		 dict)))
+	     (img (Mat))
+	     )
+	 ;(declare (type "Ptr<aruco::CharucoBoard>" board))
+	 (board->generateImage (cv--Size 800 600)
+		      img
+		      10 ;; marginsize
+		      1 ;; bordebits
+		      )
+	   )
+
+	 
+	 
+	 (unless (camera.isOpened)
+	   ,(lprint :msg "Error: Could not open camera.")
+	   (return 1))
+	 (namedWindow title
+		      WINDOW_AUTOSIZE)
+
+	 (let ((waitTime 10))
+	   (comments "waitTime in milliseconds")
+	   
+	   (let ((dict (makePtr<aruco--Dictionary>
+			(aruco--getPredefinedDictionary
+			 aruco--DICT_6X6_250)))
+		 (frame (Mat))
+		 (ids (std--vector<int>))
+		 (corners (std--vector<std--vector<Point2f>>))
+		 (allCorners (std--vector<std--vector<std--vector<Point2f>>>))
+		 (allIds (std--vector<std--vector<int>>))
+		 )
+	     (while true
+		    (do0
+		     (comments "capture image")
+		     (>> camera frame)
+		     (when (frame.empty)
+		       break))
+		    (do0
+		     (comments "detect markers")
+		     (aruco--detectMarkers frame dict corners ids)
+		     #+nil (when (< 0 (ids.size))
+		       (aruco--drawDetectedMarkers frame corners ids)))
+
+		    (do0 (comments "interpolate charuco corners")
+			 (let ((charucoCorners (Mat))
+			       (charucoIds (Mat)))
+			   (aruco--interpolateCornersCharuco
+			    corners
+			    ids
+			    frame
+			    board
+			    charucoCorners
+			    charucoIds)
+			   (when (< 0 (charucoCorners.total))
+			     (comments "If at leas one charuco corner detected, draw the corners")
+			     (aruco--drawDetectedCornersCharuco
+			      frame
+			      charucoCorners
+			      charucoIds)
+			     (commments "Collect data for calibration")
+			     (allCorners.push_back corners)
+			     (allIds.push_back ids))))
+		    (imshow title
+			    frame)
+		    (when (<= 0 (waitKey 1))
+		      break)))))
+       
+       (return 0)))))
+
+
---
+++ example/127_opencv_amd/source07/main.cpp
@@ -0,0 +1,70 @@
+#include <iostream>
+#include <opencv2/aruco.hpp>
+#include <opencv2/aruco/charuco.hpp>
+#include <opencv2/opencv.hpp>
+using namespace cv;
+
+int main(int argc, char **argv) {
+  (void)argc;
+  (void)argv;
+  auto camera = VideoCapture(0);
+  auto title = "Webcam";
+  auto x = 8;
+  auto y = 3;
+  auto square_len = 4.00e-2f;
+  auto dict = aruco::getPredefinedDictionary(aruco::DICT_6X6_250);
+  auto board =
+      new aruco::CharucoBoard(Size(x, y), square_len, 0.50f * square_len, dict);
+  auto img = Mat();
+  board->generateImage(cv::Size(800, 600), img, 10, 1);
+
+  if (!camera.isOpened()) {
+    std::cout << "Error: Could not open camera." << std::endl;
+    return 1;
+  }
+  namedWindow(title, WINDOW_AUTOSIZE);
+  auto waitTime = 10;
+  // waitTime in milliseconds
+
+  auto dict = makePtr<aruco::Dictionary>(
+      aruco::getPredefinedDictionary(aruco::DICT_6X6_250));
+  auto frame = Mat();
+  auto ids = std::vector<int>();
+  auto corners = std::vector<std::vector<Point2f>>();
+  auto allCorners = std::vector<std::vector<std::vector<Point2f>>>();
+  auto allIds = std::vector<std::vector<int>>();
+  while (true) {
+    // capture image
+
+    camera >> frame;
+    if (frame.empty()) {
+      break;
+    }
+
+    // detect markers
+
+    aruco::detectMarkers(frame, dict, corners, ids);
+
+    // interpolate charuco corners
+
+    auto charucoCorners = Mat();
+    auto charucoIds = Mat();
+    aruco::interpolateCornersCharuco(corners, ids, frame, board, charucoCorners,
+                                     charucoIds);
+    if (0 < charucoCorners.total()) {
+      // If at leas one charuco corner detected, draw the corners
+
+      aruco::drawDetectedCornersCharuco(frame, charucoCorners, charucoIds);
+            commments(Collect data for calibration);
+            allCorners.push_back(corners);
+            allIds.push_back(ids);
+    }
+
+    imshow(title, frame);
+    if (0 <= waitKey(1)) {
+            break;
+    }
+  }
+
+  return 0;
+}
