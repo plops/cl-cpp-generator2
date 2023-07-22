20431bc6ed8469772bc58b66d50a5729d2fff3c7
Martin Kielhorn <wolpumba@gmail.com>
2023-07-22 11:03:51 +0200

    more debug

--- example/131_sdr/source01/setup01_build.sh
+++ example/131_sdr/source01/setup01_build.sh
@@ -1,1 +1,14 @@
- g++ main.cpp -lSoapySDR
+# https://news.ycombinator.com/item?id=35758898
+# https://nullprogram.com/blog/2023/04/29/
+
+g++ \
+    main.cpp \
+    -lSoapySDR \
+    -g -g3 -ggdb -gdwarf-4 \
+    -Wall -Wextra -Wconversion -Wdouble-promotion \
+    -Wno-unused-parameter -Wno-unused-function -Wno-sign-conversion \
+    -fsanitize=undefined  -fanalyzer \
+    -Wvla -Wframe-larger-than=5000 -Wstack-usage=10000 \
+    -Wshadow -Werror \
+    -fvisibility=hidden \
+    -fno-strict-overflow -Wno-strict-overflow
