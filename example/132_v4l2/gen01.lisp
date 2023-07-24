d44a2a2c11ba4f4ffa5bd4dc9d15c26a60971980
Martin Kielhorn <wolpumba@gmail.com>
2023-07-24 20:27:48 +0200

    now images are stored but
    i think  the data that comes back is not raw

--- example/132_v4l2/gen01.lisp
+++ example/132_v4l2/gen01.lisp
@@ -26,6 +26,7 @@
 
   (let* ((name `V4L2Capture)
 	 (members `((device :type "const std::string&" :param t)
+		    (buffer-count :type int :param t)
 		    (buffers :type "std::vector<buffer>" :param nil)
 		    (fd :type "int" :param nil)
 		    (width :type int)
@@ -124,40 +125,49 @@
 		   (setf (dot f fmt pix pixelformat ) pixelFormat
 			 (dot f fmt pix width) width
 			 (dot f fmt pix height) height
+			 (dot f fmt pix field) V4L2_FIELD_ANY
 			 )
 		   ,(xioctl `(:request s-fmt :var &f))
-		   #+nil (xioctl VIDIOC_S_FMT &f)
+		   (unless (== f.fmt.pix.pixelformat pixelFormat)
+		     ,(lprint :msg "warning: we don't get the requested pixel format"
+			      :vars `(f.fmt.pix.pixelformat
+				      pixelFormat)))
 		   (setf width_ (dot f fmt pix width)
 			 height_ (dot f fmt pix height))
-		   (let ((r (v4l2_requestbuffers (designated-initializer :count 1
+		   (let ((r (v4l2_requestbuffers (designated-initializer :count buffer_count_
 									 :type  V4L2_BUF_TYPE_VIDEO_CAPTURE
 									 :memory V4L2_MEMORY_MMAP))))
-		     ,(xioctl `(:request reqbufs :var &r)) ;(xioctl VIDIOC_REQBUFS &r)
+		     ,(lprint :msg "prepare several buffers"
+			      :vars `(buffer_count_))
+		     ,(xioctl `(:request reqbufs :var &r)) 
 		     (buffers_.resize r.count)
 		     (dotimes (i r.count)
 		       (let ((buf (v4l2_buffer
 				   (designated-initializer
 				    :index i
 				    :type  V4L2_BUF_TYPE_VIDEO_CAPTURE
-				    :memory V4L2_MEMORY_MMAP
-				    ))))
-					;(xioctl VIDIOC_QUERYBUF &buf)
+				    :memory V4L2_MEMORY_MMAP))))
+			 
 			 ,(xioctl `(:request querybuf :var &buf))
 			 (setf (dot (aref buffers_ i)
-				    length) buf.length
-				    (dot (aref buffers_ i)
-					 start) (mmap nullptr buf.length (or PROT_READ
-									     PROT_WRITE)
-					 MAP_SHARED fd_ buf.m.offset) 
+				    length)
+			       buf.length
+			       (dot (aref buffers_ i)
+				    start) (mmap nullptr buf.length (or PROT_READ
+									PROT_WRITE)
+						 MAP_SHARED fd_ buf.m.offset) 
 				    )
+			 ,(lprint :msg "mmap memory for buffer"
+				  :vars `(i buf.length (dot (aref buffers_ i) start)))
 			 (when (== MAP_FAILED (dot (aref buffers_ i) start))
-			   (throw (std--runtime_error (string "mmap failed")))))))))
+			   (throw (std--runtime_error (string "mmap failed"))))
+			 ,(xioctl `(:request qbuf :var &buf)))))))
 
 	       (defmethod getFrame (filename)
 		 (declare (type "std::string" filename))
 		 (let ((buf (v4l2_buffer (designated-initializer :type V4L2_BUF_TYPE_VIDEO_CAPTURE
 								 :memory V4L2_MEMORY_MMAP))))
-		   ;(xioctl VIDIOC_DQBUF &buf)
+					
 		   ,(xioctl `(:request dqbuf :var &buf))
 		   )
 		 (let ((outFile (std--ofstream filename std--ios--binary)))
@@ -172,9 +182,7 @@
 							   start))
 				  buf.bytesused)
 		   (outFile.close)
-		   ,(xioctl `(:request qbuf :var &buf))
-					;(xioctl VIDIOC_QBUF &buf)
-		   ))
+		   ,(xioctl `(:request qbuf :var &buf))))
 	       	       
 	       "private:"
 	       (defstruct0 buffer
@@ -244,9 +252,11 @@
 		(type int argc)
 		(type char** argv))
        (handler-case
-	   (let ((cap (V4L2Capture  (string "/dev/video0"))))
-	     (cap.setupFormat 640 480 V4L2_PIX_FMT_BGR24)
+	   (let ((cap (V4L2Capture  (string "/dev/video0")
+				    3)))
+	     (cap.setupFormat 320 240 V4L2_PIX_FMT_RGB24)
 	     (cap.startCapturing)
+	     (usleep 64000)
 	     (dotimes (i 9)
 	       (cap.getFrame (+ (string "/dev/shm/frame_")
 				(std--to_string i)
