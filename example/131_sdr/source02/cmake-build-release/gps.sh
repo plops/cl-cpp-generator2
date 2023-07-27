147d98e710cd8f5acd35da49bca6623992a1c380
Martin Kielhorn <wolpumba@gmail.com>
2023-07-27 19:49:38 +0200

    think about the bandwidth

--- example/131_sdr/source02/cmake-build-release/gps.sh
+++ example/131_sdr/source02/cmake-build-release/gps.sh
@@ -1,1 +1,8 @@
-./imgui_soapysdr  -f 1575.42e6 -b 64512 -F 1000000
+./imgui_soapysdr  -f 1575.42e6 -b 64512 -F 1000000 -r 10000000
+
+# The 1575.42 MHz ± 12 MHz frequency is used to transmit the Global
+# Positioning System (GPS) radionavigation-satellite service L1 signal
+# for military, aviation, space, and commercial applications.
+
+# My sdr only has a maximum sampling rate of 10MHz and 8MHz bandwidth
+# is that enough?
