b8d7572f8054be796fc2b48b23d10d032677d8d0
Martin Kielhorn <wolpumba@gmail.com>
2023-07-27 19:52:17 +0200

    more

--- example/131_sdr/source02/cmake-build-release/gps.sh
+++ example/131_sdr/source02/cmake-build-release/gps.sh
@@ -1,8 +1,17 @@
 ./imgui_soapysdr  -f 1575.42e6 -b 64512 -F 1000000 -r 10000000
 
-# The 1575.42 MHz ± 12 MHz frequency is used to transmit the Global
-# Positioning System (GPS) radionavigation-satellite service L1 signal
-# for military, aviation, space, and commercial applications.
+# The 1575.42 MHz ± 12 MHz (10.23 MHz × 154) frequency is used to
+# transmit the Global Positioning System (GPS)
+# radionavigation-satellite service L1 signal for military, aviation,
+# space, and commercial applications.
 
 # My sdr only has a maximum sampling rate of 10MHz and 8MHz bandwidth
 # is that enough?
+
+
+# The C/A code is transmitted on the L1 frequency as a 1.023 MHz
+# signal using a bi-phase shift keying (BPSK) modulation
+# technique. The P(Y)-code is transmitted on both the L1 and L2
+# frequencies as a 10.23 MHz signal using the same BPSK modulation,
+# however the P(Y)-code carrier is in quadrature with the C/A carrier
+# (meaning it is 90° out of phase).
