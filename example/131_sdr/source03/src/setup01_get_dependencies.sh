41814f2c4b0efd43e632b161addeb9397dd66b4f
Martin Kielhorn <wolpumba@gmail.com>
2023-07-29 18:00:54 +0200

    more files

--- example/131_sdr/source03/src/setup01_get_dependencies.sh
+++ example/131_sdr/source03/src/setup01_get_dependencies.sh
@@ -7,6 +7,8 @@ for i in \
   imgui.h imgui.cpp imgui_draw.cpp imgui_internal.h imgui_tables.cpp \
   imgui_widgets.cpp imstb_rectpack.h imstb_textedit.h imstb_truetype.h \
   backends/imgui_impl_glfw.h backends/imgui_impl_opengl3.h \
-  backends/imgui_impl_opengl3.cpp backends/imgui_impl_opengl3_loader.h  ;do
+  backends/imgui_impl_opengl3.cpp backends/imgui_impl_opengl3_loader.h \
+  misc/cpp/imgui_stdlib.cpp misc/cpp/imgui_stdlib.h ;do
 wget  https://raw.githubusercontent.com/ocornut/imgui/master/$i
 done
+
