b3fc014c0ef361912d1350448fd35f6006b995da
Martin Kielhorn <wolpumba@gmail.com>
2023-09-02 09:20:51 +0200

    try to create a script that configures multiple builds

--- example/134_grpc_glfw_imgui/source01/configure_builds.sh
+++ example/134_grpc_glfw_imgui/source01/configure_builds.sh
@@ -1,5 +1,14 @@
 #!/bin/bash
 
+
+# Release build without sanitizers
+mkdir -p build_amd64_release
+cd build_amd64_release
+cmake .. -G Ninja -DCMAKE_BUILD_TYPE=Release -DFETCHCONTENT_BASE_DIR=../fetchcontent
+ninja
+cd ..
+
+
 # Base build directory name
 base_dir="build_amd64_debug"
 
@@ -12,12 +21,9 @@ for sanitizer in "${sanitizers[@]}"; do
   build_dir="${base_dir}_${sanitizer}"
   mkdir -p $build_dir
   cd $build_dir
-
-  # Get dependencies that I already fetched
-  cp -ar ../b/_deps .
     
   # Configure with CMake
-  cmake_args="-G Ninja -DCMAKE_BUILD_TYPE=Debug -DENABLE_${sanitizer^^}=ON"
+  cmake_args="-G Ninja -DCMAKE_BUILD_TYPE=Debug -DFETCHCONTENT_BASE_DIR=../fetchcontent  -DENABLE_${sanitizer^^}=ON"
   cmake .. $cmake_args
   ninja 
   # Go back to the original directory
