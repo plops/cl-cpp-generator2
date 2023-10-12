d089fa6c12f35d590e3a7639b52fb9c37a93a964
Martin Kielhorn <wolpumba@gmail.com>
2023-10-12 21:48:57 +0200

    start writing protofile

---
+++ example/137_sdl_grpc_viewer/source01/imgui/setup01_get_imgui.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+#Create a bash script that downloads the following files relative to this github url:
+BASE=https://raw.githubusercontent.com/ocornut/imgui/master/
+
+# List of files to download
+FILES=("imgui.h" "imconfig.h" "imgui.cpp" "imgui_draw.cpp" "imgui_internal.h" "imgui_tables.cpp" 
+       "imgui_widgets.cpp" "imstb_rectpack.h" "imstb_truetype.h" "imstb_textedit.h" "imgui_demo.cpp"
+       "backends/imgui_impl_sdl2.h" "backends/imgui_impl_sdl2.cpp" 
+       "backends/imgui_impl_opengl3.h" "backends/imgui_impl_opengl3_loader.h" "backends/imgui_impl_opengl3.cpp")
+
+# Download each file
+for file in "${FILES[@]}"; do
+    # Create the directory if it doesn't exist
+    mkdir -p $(dirname $file)
+    
+    # Download the file
+    curl -o $file "$BASE$file"
+done
+
+# The script will download the files into the current directory, creating any necessary subdirectories like backends/.
+
+
+
