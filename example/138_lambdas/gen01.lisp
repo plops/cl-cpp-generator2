ecad1a21925e70d9aea0e0b5b0a963325eabb8b2
Martin Kielhorn <wolpumba@gmail.com>
2023-11-17 01:26:32 +0100

    more

--- example/138_lambdas/gen01.lisp
+++ example/138_lambdas/gen01.lisp
@@ -25,9 +25,9 @@
    `(do0
      (include<>
       iostream
-      memory
+      ;memory
 					;string
-      vector
+      ;vector
 					;algorithm
       					;chrono
 					;thread
@@ -36,11 +36,16 @@
 					;cstdlib
       cmath
       complex
-      unordered_map
+      ;unordered_map
       format
-      thread
+      ;thread
       
       )
+     (space template "<typename... Ts>"
+	    struct "overload : Ts..."
+	    (progn
+	      (comments "operator of of all of these types so which means if you write using TS operator param paren dot dot it means that whatever types we give it.   you know if like the call operators of those types are going to be callable  directly through this overload object so it's kind of inheriting the call Operator so to say overload is an aggregate, no user defined constructor, no private members. elements of that aggregate are the base classes. initialize the overload object with aggregate initializaton using curly braces and give it a bunch of lambdas as base classes for the overload set. it will inherit the call operator from them. this behaves similar to typecase in common lisp")
+	      "using Ts::operator()...;"))
      (defun main (argc argv)
        (declare (values int)
 		(type int argc)
@@ -48,8 +53,29 @@
 
        ,(lprint :msg "main entry point" :vars `(argc (aref argv 0)))
 
-       (comments "45:00 overload sets")
-     ))
+       (do0
+	(comments "45:00 overload sets")
+	(let ((f (curly ,@(loop for e in `((int16_t int16 )
+					   (int32_t int32
+					    )
+					   (int64_t int64 )
+					   (float)
+					   (double)
+					   ("std::complex<float>" cfloat)
+					   ("std::complex<double>" cdouble))
+				collect
+				(destructuring-bind (type &optional (short type) ) e
+				  `(lambda (v)
+				     (declare (type ,type v)
+					      (capture ""))
+				     ,(lprint :msg (format nil "~a thingy" type))))))))
+	  (declare (type overload f))))
+
+       (f 2)
+       (f 2.0)
+       (f 2d0)
+       
+       ))
    :omit-parens t
    :format t
    :tidy nil))
--- example/138_lambdas/source01/src/main.cpp
+++ example/138_lambdas/source01/src/main.cpp
@@ -2,13 +2,38 @@
 #include <complex>
 #include <format>
 #include <iostream>
-#include <memory>
-#include <thread>
-#include <unordered_map>
-#include <vector>
+template <typename... Ts> struct overload : Ts... {
+  // operator of of all of these types so which means if you write using TS
+  // operator param paren dot dot it means that whatever types we give it.   you
+  // know if like the call operators of those types are going to be callable
+  // directly through this overload object so it's kind of inheriting the call
+  // Operator so to say overload is an aggregate, no user defined constructor,
+  // no private members. elements of that aggregate are the base classes.
+  // initialize the overload object with aggregate initializaton using curly
+  // braces and give it a bunch of lambdas as base classes for the overload set.
+  // it will inherit the call operator from them. this behaves similar to
+  // typecase in common lisp
+
+  using Ts::operator()...;
+};
 
 int main(int argc, char **argv) {
   std::cout << std::format("main entry point argc='{}' argv[0]='{}'\n", argc,
                            argv[0]);
   // 45:00 overload sets
+
+  overload f = {[](int16_t v) { std::cout << std::format("int16_t thingy\n"); },
+                [](int32_t v) { std::cout << std::format("int32_t thingy\n"); },
+                [](int64_t v) { std::cout << std::format("int64_t thingy\n"); },
+                [](float v) { std::cout << std::format("float thingy\n"); },
+                [](double v) { std::cout << std::format("double thingy\n"); },
+                [](std::complex<float> v) {
+                  std::cout << std::format("std::complex<float> thingy\n");
+                },
+                [](std::complex<double> v) {
+                  std::cout << std::format("std::complex<double> thingy\n");
+                }};
+  f(2);
+  f(2.0F);
+  f(2.0);
 }
