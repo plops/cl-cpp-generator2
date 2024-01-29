08367d5a7333a3daa2a822360e763ff299c2e5a4
Martin Kielhorn <wolpumba@gmail.com>
2024-01-29 21:43:56 +0100

    gui works

--- example/143_ryzen_monitor/gen01.lisp
+++ example/143_ryzen_monitor/gen01.lisp
@@ -36,6 +36,65 @@
        (declare (type int argc)
 		(type char** argv)
 		(values int))
+
+       (do0
+	(glfwSetErrorCallback glfw_error_callback)
+	(unless (glfwInit)
+	  ,(lprint :msg "glfwInit failed")
+	  (return 1))
+	(let ((glsl_version (string "#version 130")))
+	  (glfwWindowHint GLFW_CONTEXT_VERSION_MAJOR 4)
+	  (glfwWindowHint GLFW_CONTEXT_VERSION_MINOR 6))
+	(let ((window (glfwCreateWindow 1280 720
+					(string "ryzen_mon_glgui")
+					nullptr
+					nullptr)))
+	  (when (== nullptr window)
+	    ,(lprint :msg "Can't open glfw window")
+	    (return 1))
+	  (glfwMakeContextCurrent window)
+	  (glfwSwapInterval 1)
+	  (IMGUI_CHECKVERSION)
+	  (ImGui--CreateContext)
+	  (let ((io (ImGui--GetIO)))
+	    (setf io.ConfigFlags
+		  (or io.ConfigFlags
+		      ImGuiConfigFlags_NavEnableKeyboard)))
+	  (ImGui--StyleColorsDark)
+	  (ImGui_ImplGlfw_InitForOpenGL window true)
+	  (ImGui_ImplOpenGL3_Init glsl_version))
+	)
+
+       (do0
+	(let ((show_demo_window true)
+	      (clear_color (ImVec4 .4s0 .5s0 .6s0 1s0)))
+	  (while (!glfwWindowShouldClose window)
+		 (glfwPollEvents)
+		 (ImGui_ImplOpenGL3_NewFrame)
+		 (ImGui_ImplGlfw_NewFrame)
+		 (ImGui--NewFrame)
+		 (when show_demo_window
+		   (ImGui--ShowDemoWindow &show_demo_window))
+		 (ImGui--Render)
+		 (let ((w 0)
+		       (h 0))
+		   (glfwGetFramebufferSize window &w &h)
+		   (glViewport 0 0 w h)
+		   (glClearColor (* clear_color.x clear_color.w)
+				 (* clear_color.y clear_color.w)
+				 (* clear_color.z clear_color.w)
+				 clear_color.w)
+		   (glClear GL_COLOR_BUFFER_BIT)
+		   (ImGui_ImplOpenGL3_RenderDrawData
+		    (ImGui--GetDrawData))
+		   (glfwSwapBuffers window)))))
+       (do0
+	(ImGui_ImplOpenGL3_Shutdown)
+	(ImGui_ImplGlfw_Shutdown)
+	(ImGui--DestroyContext)
+	(glfwDestroyWindow window)
+	(glfwTerminate)
+	(return 0))
        ))
    :omit-parens t
    :format t
--- example/143_ryzen_monitor/source01/src/main.cpp
+++ example/143_ryzen_monitor/source01/src/main.cpp
@@ -9,4 +9,54 @@ void glfw_error_callback(int err, const char *description) {
   std::cout << std::format(" err='{}' description='{}'\n", err, description);
 }
 
-int main(int argc, char **argv) {}
+int main(int argc, char **argv) {
+  glfwSetErrorCallback(glfw_error_callback);
+  if (!glfwInit()) {
+    std::cout << std::format("glfwInit failed\n");
+    return 1;
+  }
+  auto glsl_version{"#version 130"};
+  glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 4);
+  glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 6);
+  auto window{glfwCreateWindow(1280, 720, "ryzen_mon_glgui", nullptr, nullptr)};
+  if (nullptr == window) {
+    std::cout << std::format("Can't open glfw window\n");
+    return 1;
+  }
+  glfwMakeContextCurrent(window);
+  glfwSwapInterval(1);
+  IMGUI_CHECKVERSION();
+  ImGui::CreateContext();
+  auto io{ImGui::GetIO()};
+  io.ConfigFlags = io.ConfigFlags | ImGuiConfigFlags_NavEnableKeyboard;
+  ImGui::StyleColorsDark();
+  ImGui_ImplGlfw_InitForOpenGL(window, true);
+  ImGui_ImplOpenGL3_Init(glsl_version);
+  auto show_demo_window{true};
+  auto clear_color{ImVec4(0.40F, 0.50F, 0.60F, 1.0F)};
+  while (!glfwWindowShouldClose(window)) {
+    glfwPollEvents();
+    ImGui_ImplOpenGL3_NewFrame();
+    ImGui_ImplGlfw_NewFrame();
+    ImGui::NewFrame();
+    if (show_demo_window) {
+      ImGui::ShowDemoWindow(&show_demo_window);
+    }
+    ImGui::Render();
+    auto w{0};
+    auto h{0};
+    glfwGetFramebufferSize(window, &w, &h);
+    glViewport(0, 0, w, h);
+    glClearColor(clear_color.x * clear_color.w, clear_color.y * clear_color.w,
+                 clear_color.z * clear_color.w, clear_color.w);
+    glClear(GL_COLOR_BUFFER_BIT);
+    ImGui_ImplOpenGL3_RenderDrawData(ImGui::GetDrawData());
+    glfwSwapBuffers(window);
+  }
+  ImGui_ImplOpenGL3_Shutdown();
+  ImGui_ImplGlfw_Shutdown();
+  ImGui::DestroyContext();
+  glfwDestroyWindow(window);
+  glfwTerminate();
+  return 0;
+}
