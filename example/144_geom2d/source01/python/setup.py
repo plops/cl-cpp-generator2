bfa0d348f265a70f5e230eddb600ca2d9c85f054
Martin Kielhorn <wolpumba@gmail.com>
2024-02-10 10:57:11 +0100

    setup.py seems to compile the library:
    /144_geom2d/source01/python $ time python setup.py build_ext --inplace
    running build_ext
    INFO: Disabling color, you really want to install colorlog.
    Disabling color, you really want to install colorlog.
    building 'olcUTIL_Geometry2D_py' extension
    creating build
    creating build/temp.linux-x86_64-cpython-311
    x86_64-pc-linux-gnu-gcc -Wsign-compare -DNDEBUG -fPIC -I/usr/lib/python3.11/site-packages/pybind11/include -I/usr/include/python3.11 -c olcUTIL_Geometry2D_py.cpp -o build/temp.linux-x86_64-cpython-311/olcUTIL_Geometry2D_py.o -fvisibility=hidden -g0
    In file included from olcUTIL_Geometry2D_py.cpp:8:
    olcUTIL_Geometry2D.h: In function ‘constexpr bool olc::utils::geom2d::contains(const rect<T1>&, const olc::v_2d<TR>&)’:
    olcUTIL_Geometry2D.h:1043:28: warning: expected ‘template’ keyword before dependent template name [-Wmissing-template-keyword]
     1043 |                 return !(p.x < r.pos.x || p.y < r.pos.y ||
          |                            ^
          |                            template
    olcUTIL_Geometry2D.h:1043:45: warning: expected ‘template’ keyword before dependent template name [-Wmissing-template-keyword]
     1043 |                 return !(p.x < r.pos.x || p.y < r.pos.y ||
          |                                             ^
          |                                             template
    creating build/lib.linux-x86_64-cpython-311
    x86_64-pc-linux-gnu-gcc -shared build/temp.linux-x86_64-cpython-311/olcUTIL_Geometry2D_py.o -L/usr/lib64 -o build/lib.linux-x86_64-cpython-311/olcUTIL_Geometry2D_py.cpython-311-x86_64-linux-gnu.so
    copying build/lib.linux-x86_64-cpython-311/olcUTIL_Geometry2D_py.cpython-311-x86_64-linux-gnu.so ->
    
    real    0m4.715s
    user    0m4.492s
    sys     0m0.222s

--- example/144_geom2d/source01/python/setup.py
+++ example/144_geom2d/source01/python/setup.py
@@ -1,4 +1,5 @@
 #!/usr/bin/env python3
 from setuptools import setup, Extension
-ext_module=Extension("pygeometry", sources=["pygeom.cpp"], include_dirs=[pybind11.get_include()], language="c++20")
-setup(name="pygeometry", ext_modules=[ext_module])
+from pybind11.setup_helpers import Pybind11Extension
+ext_modules=[Pybind11Extension("olcUTIL_Geometry2D_py", sources=["olcUTIL_Geometry2D_py.cpp"], language="c++20")]
+setup(name="olcUTIL_Geometry2D_py", version="0.0.1", author="Developers of olcUTIL_Geometry2D", description="Python bindings for olcUTIL_Geometry2D library using Pybind11", ext_modules=ext_modules)
