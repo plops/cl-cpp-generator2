bf741facf766cff83527bae3b447e9b8d26ac784
Martin Kielhorn <wolpumba@gmail.com>
2024-02-16 23:11:00 +0100

    start wirting code

---
+++ example/146_mch_mcu/gen01.lisp
@@ -0,0 +1,45 @@
+(eval-when (:compile-toplevel :execute :load-toplevel)
+  (ql:quickload "cl-cpp-generator2")
+  (ql:quickload "alexandria"))
+
+(in-package :cl-cpp-generator2)
+
+(progn
+  (setf *features* (set-difference *features* (list :more
+						    )))
+  (setf *features* (set-exclusive-or *features* (list ;:more
+						      ))))
+
+
+
+(let ((module-name "main"))
+  (defparameter *source-dir* #P"example/146_mch_mcu/source01/src/")
+  (defparameter *full-source-dir* (asdf:system-relative-pathname
+				   'cl-cpp-generator2
+				   *source-dir*))
+  (ensure-directories-exist *full-source-dir*)
+ 
+
+  (write-source 
+   (asdf:system-relative-pathname
+    'cl-cpp-generator2 
+    (merge-pathnames (format nil "~a.cpp" module-name)
+		     *source-dir*))
+   
+   `(do0
+     
+     (include<> stdio.h
+					;format
+					;unistd.h
+					;vector deque chrono
+					;cmath
+     
+      )
+     (defun main ())
+     )
+   :omit-parens t
+   :format t
+   :tidy nil))
+
+
+
