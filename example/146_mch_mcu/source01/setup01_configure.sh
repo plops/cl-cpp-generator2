c901ed186837d998d0968055dbc3fbaf7012067f
Martin Kielhorn <wolpumba@gmail.com>
2024-02-18 23:08:41 +0100

    the xpack gcc is not working:
    /home/martin/stage/cl-cpp-generator2/example/146_mch_mcu/source01/../hal/StdPeriphDriver/inc/CH59x_sys.h:196: Error: unrecognized opcode `csrr a4,0x800', extension `zicsr' required

--- example/146_mch_mcu/source01/setup01_configure.sh
+++ example/146_mch_mcu/source01/setup01_configure.sh
@@ -13,3 +13,4 @@ time ninja
 # https://youtu.be/L9Wrv7nW-S8?t=626
 #~/risc/RISC-V_Embedded_GCC12/bin/riscv-none-elf-objcopy -O ihex risc_test risc_test.hex 
 #~/risc/xpack-riscv-none-elf-gcc-13.2.0-2/bin/riscv-none-elf-objcopy 
+ 
