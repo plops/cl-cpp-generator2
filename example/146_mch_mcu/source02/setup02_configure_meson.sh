0cfc6231bb9e70ded26d8802e7ea9236195588bc
Martin Kielhorn <wolpumba@gmail.com>
2024-02-21 23:36:39 +0100

    meson build is quite a lot larger than c++ build

--- example/146_mch_mcu/source02/setup02_configure_meson.sh
+++ example/146_mch_mcu/source02/setup02_configure_meson.sh
@@ -1,22 +1,26 @@
-#export CC=~/risc/RISC-V_Embedded_GCC12/bin/riscv-none-elf-gcc
-#export CXX=~/risc/RISC-V_Embedded_GCC12/bin/riscv-none-elf-g++
+#!/bin/bash
 
-export CC=~/risc/xpack-riscv-none-elf-gcc-13.2.0-2/bin/riscv-none-elf-gcc
-export CXX=~/risc/xpack-riscv-none-elf-gcc-13.2.0-2/bin/riscv-none-elf-g++
+# This script is used to configure the meson build system for the RISC-V project
+meson setup bmeson --cross-file riscv_cross.txt --unity=on --default-library=static --buildtype=release --unity-size=2048
+cd bmeson
+# perform the build
+meson compile
 
 
-mkdir b
-cd b
-cmake .. -G Ninja -DCMAKE_BUILD_TYPE=Release
-time ninja
-# turns out i can flash elf files. conversion to hex is not necessary
-# https://youtu.be/L9Wrv7nW-S8?t=626
-#~/risc/RISC-V_Embedded_GCC12/bin/riscv-none-elf-objcopy -O ihex risc_test risc_test.hex 
-#~/risc/xpack-riscv-none-elf-gcc-13.2.0-2/bin/riscv-none-elf-objcopy 
- 
-wget https://github.com/ch32-rs/wchisp/releases/download/nightly/wchisp-linux-x64.tar.gz
-tar xavf wchisp-linux-x64.tar.gz
+# download wchisp if it doesn't already exist
+if [ ! -f wchisp ]; then
+    wget https://github.com/ch32-rs/wchisp/releases/download/nightly/wchisp-linux-x64.tar.gz
+    tar xavf wchisp-linux-x64.tar.gz
+fi
 
+
+# to allow non-root users to access the USB device, create a udev rule
+# if this file doesn't exist write a message for the user
+if [ ! -f /etc/udev/rules.d/50-wchisp.rules ]; then
+    echo "To allow non-root users to access the USB device, create a udev rule"
+    echo "as root run:"
+    echo "echo 'SUBSYSTEM==\"usb\", ATTRS{idVendor}==\"4348\", ATTRS{idProduct}==\"55e0\", MODE=\"0666\"' > /etc/udev/rules.d/50-wchisp.rules"
+fi
 # as root run:
 # echo 'SUBSYSTEM=="usb", ATTRS{idVendor}=="4348", ATTRS{idProduct}=="55e0", MODE="0666"' > /etc/udev/rules.d/50-wchisp.rules
 
