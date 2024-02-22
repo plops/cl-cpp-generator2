96f0a90e9a65e2648d747dae6ad06e7d91242af4
Martin Kielhorn <wolpumba@gmail.com>
2024-02-22 23:12:04 +0100

    more  code with documentatino

--- example/146_mch_mcu/gen03.lisp
+++ example/146_mch_mcu/gen03.lisp
@@ -48,7 +48,7 @@
      
 		      )
 
-     (space const uint8_t DevEP0Size (hex #x40))
+     (space constexpr uint8_t (= DevEP0Size (hex #x40)))
       (comments "vendor id and product id:")
       ,(let ((l `(#x12 1 #x10 1 #xff #x80 #x55 DevEP0Size #x48 #x43 #x37 #x55
 		       0 1 1 2 0 1)))
@@ -136,20 +136,72 @@
 	       (errflag (uint8_t 0))))
 	 (let ((intflag (uint8_t R8_USB_INT_FG)))
 	   (when (& intflag RB_UIF_TRANSFER)
-	     (unless (== MSK_UIS_TOKEN
+	     (unless (== MASK_UIS_TOKEN
 			 (& R8_USB_INT_ST MASK_UIS_TOKEN))
 	       (case (& R8_USB_INT_ST (or MASK_UIS_TOKEN
 					  MASK_UIS_ENDP))
 		 (UIS_TOKEN_IN
 		  (case SetupReqCode
 		    (USB_GET_DESCRIPTOR
-		     (setf len (std--min DevEp0Size
+		     (comments "Handles the standard 'Get Descriptor' request. The device sends the appropriate descriptor data to the host.")
+		     (comments "Calculate length of data to send. Limit to device endpoint size if needed.")
+		     (setf len (std--min (static_cast<uint16_t> DevEP0Size)
 					 SetupReqLen)
 			   #+nil(? (<= DevEP0Size SetupReqLen)
-			      DevEp0Size
-			      SetupReqLen))
-		     (memcpy ))))
-		 ))))))
+				   DevEP0Size
+				   SetupReqLen))
+		     (comments "Copy the descriptor data to the endpoint buffer for transmission to the host.")
+		     (memcpy pEP0_DataBuf pDescr len )
+		     (decf SetupReqLen len)
+		     (incf pDescr len)
+		     (comments "Update state variables (length of the remaining request, pointer to the next chunk of descriptor data) and prepare for the next stage of the transfer.")
+		     (setf R8_UEP0_T_LEN len
+			   )
+		     (setf R8_UEP0_CTRL (^ R8_UEP0_CTRL RB_UEP_T_TOG)))
+		    (USB_SET_ADDRESS
+		     (comments "Handles the standard 'Set Address' request. The device records the new USB address.")
+		     (setf R8_USB_DEV_AD (or (& R8_USB_DEV_AD RB_UDA_GP_BIT)
+					     SetupReqLen))
+		     (setf R8_UEP0_CTRL (or UEP_R_RES_ACK UEP_T_RES_NAK)))
+		    (t
+		     (comments "Handles any other control requests. This usually results in a stall condition, as the device didn't recognize the request.")
+		     (setf R8_UEP0_T_LEN 0
+			   R8_UEP0_CTRL (or UEP_R_RES_ACK UEP_T_RES_NAK))))
+		  )
+		 (UIS_TOKEN_OUT
+		  (comments "Handles 'OUT' token transactions, meaning the host is sending data to the device.")
+		  (comments "Get length of received data.")
+		  (setf len R8_USB_RX_LEN))
+		 #+nil ((or UIS_TOKEN_OUT 1)
+		  (when (& R8_USB_INT_ST
+			   RB_UIS_TOG_OK)
+		    (comments "f a particular status flag is set (indicating data is ready to be processed)")
+		    (setf R8_UEP1_CTRL  (^  R8_UEP1_CTRL RB_UEP_R_TOG)
+			  len R8_USB_RX_LEN)
+		    (comments "Update state, get the data length, and call a function (DevEP1_OUT_Deal) to process the received data (on endpoint 1).")
+		    (DevEP1_OUT_Deal len)))
+		 (t )
+		 )
+	       (setf R8_USB_INT_FG RB_UIF_TRANSFER))
+	     (comments "This code handles the initial 'Setup' stage of USB control transfers. When the host sends a setup packet to the device, this code analyzes the request and prepares a response.")
+	     (when (& R8_USB_INT_ST
+		      RB_UIS_SETUP_ACT)
+	       (comments "A setup packet has been received.")
+	       (comments "Prepare the control endpoint for a response.")
+	       (setf R8_UEP0_CTRL (or RB_UEP_R_TOG
+				      RB_UEP_T_TOG
+				      UEP_R_RES_ACK
+				      UEP_T_RES_NAK))
+	       (comments "Extract the length, request code, and type from the setup packet.")
+	       (setf SetupReqLen (-> pSetupReqPak wLength))
+	       (setf SetupReqCode (-> pSetupReqPak bRequest))
+	       (setf chtype (-> pSetupReqPak bRequestType))
+	       (setf len 0
+		     errflag 0)
+	       (unless (== USB_REQ_TYP_STANDARD
+			   (& chtype USB_REQ_TYP_MASK))
+		 (comments "If the request type is NOT a standard request, set an error flag.")
+		 (setf errflag (hex #xff))))))))
      
     )
    :omit-parens t
