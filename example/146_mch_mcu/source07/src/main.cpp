06b063faf0b7499f8a7c69a154c1db3c415e9078
Martin Kielhorn <wolpumba@gmail.com>
2024-03-12 22:13:54 +0100

    that is an okay representation of registers

--- example/146_mch_mcu/source07/src/main.cpp
+++ example/146_mch_mcu/source07/src/main.cpp
@@ -1,45 +1,88 @@
 #include <cstdint>
 #include <iostream>
+#include <ostream>
 
-class A {
-    public:
-    union {
-        uint8_t reg;
-        struct {
-            uint8_t bit0 : 1;
-            uint8_t bit1 : 1;
-            uint8_t bit2 : 1;
-            uint8_t bit3 : 1;
-            uint8_t bit4 : 1;
-            uint8_t bit5 : 1;
-            uint8_t bit6 : 1;
-            uint8_t bit7 : 1;
+class A
+{
+public:
+    struct Ctl
+    {
+        union
+        {
+            uint8_t reg;
+            struct
+            {
+                uint8_t enable : 1;
+                uint8_t start_bit_align : 1;
+                uint8_t bit_align_finished : 1;
+                uint8_t bit3 : 1;
+                uint8_t bit4 : 1;
+                uint8_t bit5 : 1;
+                uint8_t bit6 : 1;
+                uint8_t bit7 : 1;
+            };
         };
+        Ctl &operator=(uint8_t val)
+        {
+            reg = val;
+            return *this;
+        }
+        std::ostream &print(std::ostream &os) const
+        {
+            os << "enable: " << static_cast<int>(enable) << std::endl;
+            os << "start_bit_align: " << static_cast<int>(start_bit_align) << std::endl;
+            os << "bit_align_finished: " << static_cast<int>(bit_align_finished) << std::endl;
+            return os;
+        }
     } ctl;
+
+    struct Status
+    {
+        union
+        {
+            uint8_t reg;
+            struct
+            {
+                uint8_t enabled : 1;
+                uint8_t bit_align_running : 1;
+                uint8_t bit_align_error : 1;
+                uint8_t reserved : 5;
+            };
+        };
+        Status &operator=(uint8_t val)
+        {
+            reg = val;
+            return *this;
+        }
+        std::ostream &print(std::ostream &os) const
+        {
+            os << "enabled: " << static_cast<int>(enabled) << std::endl;
+            os << "bit_align_running: " << static_cast<int>(bit_align_running) << std::endl;
+            os << "bit_align_error: " << static_cast<int>(bit_align_error) << std::endl;
+            return os;
+        }
+    } status;
 };
 
-int main(int argc, char**argv)
+int main(int argc, char **argv)
 {
     A a;
-    a.ctl.reg = 0xaf;
+    a.ctl = 0xaf;
+    a.status = 0x07;
     // print all bits
-    std::cout << "bit0: " << static_cast<int>(a.ctl.bit0) << std::endl;
-    std::cout << "bit1: " << static_cast<int>(a.ctl.bit1) << std::endl;
-    std::cout << "bit2: " << static_cast<int>(a.ctl.bit2) << std::endl;
-    std::cout << "bit3: " << static_cast<int>(a.ctl.bit3) << std::endl;
-    std::cout << "bit4: " << static_cast<int>(a.ctl.bit4) << std::endl;
-    std::cout << "bit5: " << static_cast<int>(a.ctl.bit5) << std::endl;
-    std::cout << "bit6: " << static_cast<int>(a.ctl.bit6) << std::endl;
-    std::cout << "bit7: " << static_cast<int>(a.ctl.bit7) << std::endl;
-    /* /source07/src $ ./a.out 
-bit0: 1
-bit1: 1
-bit2: 1
-bit3: 1
-bit4: 0
-bit5: 1
-bit6: 0
-bit7: 1
+
+    a.ctl.print(std::cout) << std::endl;
+    a.status.print(std::cout) << std::endl;
+
+    /*
+    source07/src $ ./a.out
+    enable: 1
+    start_bit_align: 1
+    bit_align_finished: 1
+
+    enabled: 1
+    bit_align_running: 1
+    bit_align_error: 1
     */
     return 0;
 }
