bb003384418498d6c8fcc70899317bbcd99203f3
Martin Kielhorn <wolpumba@gmail.com>
2024-04-18 00:11:24 +0200

    print all entries

--- example/147_arma/source01/src/main.cpp
+++ example/147_arma/source01/src/main.cpp
@@ -1,5 +1,14 @@
 #include <armadillo>
 #include <format>
 #include <iostream>
+using namespace arma;
 
-int main(int argc, char **argv) { return 0; }
+int main(int argc, char **argv) {
+  std::cout << std::format("start\n");
+  arma_rng::set_seed_random();
+  auto A{randn(5, 5)};
+  for (const auto &a : A) {
+    std::cout << std::format(" a='{}'\n", a);
+  }
+  return 0;
+}
