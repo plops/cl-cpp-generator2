2460fd748aa54faf9f96e188e9ea2bb1b56ced43
Martin Kielhorn <wolpumba@gmail.com>
2024-05-01 23:01:28 +0200

    more code

---
+++ example/148_opencv_capture/source01/src/main.cpp
@@ -0,0 +1,19 @@
+#include <X11/Xlib.h>
+#include <X11/Xutil.h>
+#include <X11/extensions/XShm.h>
+#include <sys/ipc.h>
+#include <sys/shm.h>
+#include <opencv2/opencv.hpp>
+#include <format>
+#include <iostream> 
+
+int main (int argc, char** argv)        {
+        std::cout<<std::format("start\n");
+            auto img {cv::Mat()}; 
+    auto count {0}; 
+    while ( true ) {
+                count++;
+}  
+        return 0;
+}
+ 
