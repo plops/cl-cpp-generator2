6e45008ef5051adb85b81546718f6fe240996b50
Martin Kielhorn <wolpumba@gmail.com>
2024-05-03 22:41:51 +0200

    start new prject

---
+++ example/149_shunting_yard/READM.md
@@ -0,0 +1,3 @@
+https://www.youtube.com/watch?v=unh6aK8WMwM
+
+DIY Programming Language #1: The Shunting Yard Algorithm
---
+++ example/149_shunting_yard/source01/src/main.cpp
@@ -0,0 +1,7 @@
+#include <format>
+#include <iostream>
+
+int main(int argc, char **argv) {
+  std::cout << std::format("start\n");
+  return 0;
+}
