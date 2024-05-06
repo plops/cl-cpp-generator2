699402a9d41400cdd88a5145721e3c4e369eb707
Martin Kielhorn <wolpumba@gmail.com>
2024-05-07 00:31:24 +0200

    script to download onnx

---
+++ example/148_opencv_capture/source02/src/setup01_get_onnx.sh
@@ -0,0 +1,2 @@
+wget https://github.com/opencv/opencv_zoo/raw/main/models/text_recognition_crnn/text_recognition_CRNN_EN_2022oct_int8.onnx
+wget https://github.com/opencv/opencv_zoo/raw/main/models/text_detection_ppocr/text_detection_en_ppocrv3_2023may_int8.onnx
