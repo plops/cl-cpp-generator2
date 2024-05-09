eb4503f4fc7c524fdc0bb03d5c68ede458cd6703
Martin Kielhorn <wolpumba@gmail.com>
2024-05-09 11:50:18 +0200

    code for 102

---
+++ example/102_simple_test/source00/main.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+
+int main(int argc, **char argv) {
+  for (char i = 0; (i) < (23); (i) += (1)) {
+    if (((i) + (3)) == ((i) + (((i) + (2)) * (a)))) {
+      (std::cout) << ((i) + ((a) * ((a) + (1))) + (b)) << (std::endl);
+    }
+  }
+  for (auto &th : threads) {
+    th.join();
+  }
+}
