e0a3ce92ac2296133015cd41f4c66e750c45f895
Martin Kielhorn <wolpumba@gmail.com>
2024-05-28 22:50:30 +0200

    it is receiving packets

--- example/151_raw_ethernet/gen01.lisp
+++ example/151_raw_ethernet/gen01.lisp
@@ -29,8 +29,8 @@
      (include<>
       arpa/inet.h
       linux/if_packet.h
-      ;linux/ip.h
-      ;linux/udp.h
+      linux/ip.h
+      linux/udp.h
       net/if.h
       netinet/ether.h
       sys/ioctl.h
@@ -38,9 +38,12 @@
       unistd.h
 
       iostream
+      array
       span
       string
-      system_error)
+      system_error
+
+      cstring)
      (defun check (result msg)
        (declare (type int result)
 		(type "const std::string&" msg))
@@ -54,10 +57,54 @@
 		(values int))
        (handler-case
 	   (do0
-	    (let ((ifName (string "eth0"))
+	    (comments "Open PF_PACKET socket")
+	    (let ((ifName (string "lo"))
 		  (sockfd (socket PF_PACKET SOCK_RAW (htons (hex #x800)))))
-	      (check sockfd (string "Failed  to create socket"))
-	      )
+	      (check sockfd (string "Failed to create socket")))
+	    (do0
+	     (comments "Set interface to promiscuous mode")
+	     (let ((ifopts "ifreq{}"))
+	       (strncpy ifopts.ifr_name
+			ifName
+			(- IFNAMSIZ 1))
+	       (check (ioctl sockfd SIOCGIFFLAGS &ifopts)
+		      (string "Failed to get interface flags"))
+	       (setf ifopts.ifr_flags (logior ifopts.ifr_flags
+					     IFF_PROMISC))
+	       (check (ioctl sockfd SIOCSIFFLAGS &ifopts)
+		      (string "Failed to set interface flags"))))
+	    (do0
+	     (comments "Allow the socket to be reused")
+	     (let ((sockopt SO_REUSEADDR))
+	       (check
+		(setsockopt sockfd SOL_SOCKET
+			    SO_REUSEADDR
+			    &sockopt (sizeof sockopt))
+		(string "Failed to set socket option SO_REUSEADDR"))))
+
+	    (do0
+	     (comments "Bind to device")
+	     (check (setsockopt sockfd SOL_SOCKET SO_BINDTODEVICE
+				ifName
+				(- IFNAMSIZ 1))
+		    (string "Failed to set socket option SO_BINDTODEVICE")))
+	    (do0
+	     (comments "Receive loop")
+	     (while true
+		    (let ((buf "std::array<uint8_t,1024>{}")
+			  (numbytes (recvfrom sockfd
+					      (buf.data)
+					      (buf.size)
+					      0
+					      nullptr
+					      nullptr)))
+		      (check numbytes (string "Failed to receive data"))
+		      (let ((eh (reinterpret_cast<ether_header*> (buf.data)))
+			    (receivedMac ("std::span<const uint8_t, 6>" eh->ether_dhost 6))))
+		      )
+	      ))
+	    (close sockfd)
+	    
 	    )
 	 ("const std::system_error&" (ex)
 	   (<< std--cerr
--- example/151_raw_ethernet/source01/src/main.cpp
+++ example/151_raw_ethernet/source01/src/main.cpp
@@ -1,8 +1,12 @@
 // based on https://gist.github.com/austinmarton/2862515
 
 #include <arpa/inet.h>
+#include <array>
+#include <cstring>
 #include <iostream>
 #include <linux/if_packet.h>
+#include <linux/ip.h>
+#include <linux/udp.h>
 #include <net/if.h>
 #include <netinet/ether.h>
 #include <span>
@@ -20,9 +24,41 @@ void check(int result, const std::string &msg) {
 
 int main(int argc, char **argv) {
   try {
-    const auto *ifName{"eth0"};
+    // Open PF_PACKET socket
+
+    auto ifName{"lo"};
     auto sockfd{socket(PF_PACKET, SOCK_RAW, htons(0x800))};
-    check(sockfd, "Failed  to create socket");
+    check(sockfd, "Failed to create socket");
+    // Set interface to promiscuous mode
+
+    auto ifopts{ifreq{}};
+    strncpy(ifopts.ifr_name, ifName, IFNAMSIZ - 1);
+    check(ioctl(sockfd, SIOCGIFFLAGS, &ifopts),
+          "Failed to get interface flags");
+    ifopts.ifr_flags = ifopts.ifr_flags || IFF_PROMISC;
+    check(ioctl(sockfd, SIOCSIFFLAGS, &ifopts),
+          "Failed to set interface flags");
+    // Allow the socket to be reused
+
+    auto sockopt{SO_REUSEADDR};
+    check(
+        setsockopt(sockfd, SOL_SOCKET, SO_REUSEADDR, &sockopt, sizeof(sockopt)),
+        "Failed to set socket option SO_REUSEADDR");
+    // Bind to device
+
+    check(setsockopt(sockfd, SOL_SOCKET, SO_BINDTODEVICE, ifName, IFNAMSIZ - 1),
+          "Failed to set socket option SO_BINDTODEVICE");
+    // Receive loop
+
+    while (true) {
+      auto buf{std::array<uint8_t, 1024>{}};
+      auto numbytes{
+          recvfrom(sockfd, buf.data(), buf.size(), 0, nullptr, nullptr)};
+      check(numbytes, "Failed to receive data");
+      auto eh{reinterpret_cast<ether_header *>(buf.data())};
+      auto receivedMac{std::span<const uint8_t, 6>(eh->ether_dhost, 6)};
+    }
+    close(sockfd);
   } catch (const std::system_error &ex) {
     std::cerr << "Error: " << ex.what() << " (" << ex.code() << ")\n";
     return 1;
