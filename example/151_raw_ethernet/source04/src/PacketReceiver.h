a8554064700bb31d46b336c8555d5cb12114451d
Martin Kielhorn <wolpumba@gmail.com>
2024-06-02 23:49:13 +0200

    more

--- example/151_raw_ethernet/source04/src/PacketReceiver.h
+++ example/151_raw_ethernet/source04/src/PacketReceiver.h
@@ -12,15 +12,21 @@ class PacketReceiver  {
         public:
          PacketReceiver (std::function<void(const uint8_t*, const size_t)> callback = nullptr, const std::string& if_name = "lo", const uint32_t& block_size = 4096, const uint32_t& block_nr = 1, const uint32_t& frame_size = 128)       ;   
          ~PacketReceiver ()       ;   
-            /** @brief Receives packets from a socket and stores them in a vector.
+            /** 
+@brief Continuously reads packets from a socket and processes them.
 
-This function receives packets from a socket and stores them in a vector. 
-The vector is passed by reference, allowing the function to modify its contents directly.
-At the start of the function, the vector is cleared, removing all elements but not deallocating its memory.
-This is done to minimize memory allocations if a similar number of packets is returned each time the function is called.
+This function runs an infinite loop that continuously polls a socket for incoming packets.
+When a packet is received, it is processed and the provided callback function is called with the packet data.
+The function also handles various status flags and errors, and provides detailed logging of packet information and status.
 
-@param packets A reference to a vector where the received packets will be stored. 
-       The vector is cleared at the start of the function, but its memory is not deallocated.
+The function uses a ring buffer mechanism for efficient packet reading. It keeps track of the current index in the ring buffer,
+and after processing each packet, it hands the buffer back to the kernel and moves to the next buffer in the ring.
+
+If the function encounters an error during polling, it throws a runtime_error exception.
+
+If no packets are available for reading, the function sleeps for 4 milliseconds before polling the socket again.
+
+@note This function runs indefinitely until an error occurs or the program is terminated.
  
 
 
