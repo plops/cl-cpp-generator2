cdf8bf1454791d4d3f6549c2ea5af47d5521b251
Martin Kielhorn <wolpumba@gmail.com>
2024-06-02 20:38:56 +0200

    get class to compile

--- example/151_raw_ethernet/source04/src/PacketReceiver.cpp
+++ example/151_raw_ethernet/source04/src/PacketReceiver.cpp
@@ -3,9 +3,23 @@
 // implementation
 
 #include "PacketReceiver.h"
+#include <arpa/inet.h>
+#include <cstdint>
+#include <iomanip>
+#include <iostream>
+#include <linux/if_packet.h>
+#include <net/if.h>
+#include <netinet/ether.h>
+#include <poll.h>
+#include <sys/mman.h>
+#include <sys/socket.h>
+#include <system_error>
+#include <thread>
+#include <unistd.h>
 PacketReceiver::PacketReceiver(const std::string &if_name,
-                               const uint32 &block_size, const uint32 &block_nr,
-                               const uint32 &frame_size)
+                               const uint32_t &block_size,
+                               const uint32_t &block_nr,
+                               const uint32_t &frame_size)
     : sockfd{-1}, mmap_base{nullptr}, mmap_size{0}, if_name{if_name},
       block_size{block_size}, block_nr{block_nr}, frame_size{frame_size},
       frame_nr{0}, rx_buffer_cnt{0} {
@@ -75,31 +89,41 @@ PacketReceiver::~PacketReceiver() {
 
   close(sockfd);
 }
-const int &PacketReceiver::GetSockfd() const { return sockfd; }
-void PacketReceiver::SetSockfd(int sockfd) { this->sockfd = sockfd; }
-void *PacketReceiver::GetMmapBase() { return mmap_base; }
-void PacketReceiver::SetMmapBase(void *mmap_base) {
-  this->mmap_base = mmap_base;
-}
-const size_t &PacketReceiver::GetMmapSize() const { return mmap_size; }
-void PacketReceiver::SetMmapSize(size_t mmap_size) {
-  this->mmap_size = mmap_size;
-}
-const std::string &PacketReceiver::GetIfName() const { return if_name; }
-void PacketReceiver::SetIfName(std::string if_name) { this->if_name = if_name; }
-const uint32 &PacketReceiver::GetBlockSize() const { return block_size; }
-void PacketReceiver::SetBlockSize(uint32 block_size) {
-  this->block_size = block_size;
-}
-const uint32 &PacketReceiver::GetBlockNr() const { return block_nr; }
-void PacketReceiver::SetBlockNr(uint32 block_nr) { this->block_nr = block_nr; }
-const uint32 &PacketReceiver::GetFrameSize() const { return frame_size; }
-void PacketReceiver::SetFrameSize(uint32 frame_size) {
-  this->frame_size = frame_size;
-}
-const uint32 &PacketReceiver::GetFrameNr() const { return frame_nr; }
-void PacketReceiver::SetFrameNr(uint32 frame_nr) { this->frame_nr = frame_nr; }
-const uint32 &PacketReceiver::GetRxBufferCnt() const { return rx_buffer_cnt; }
-void PacketReceiver::SetRxBufferCnt(uint32 rx_buffer_cnt) {
-  this->rx_buffer_cnt = rx_buffer_cnt;
+void PacketReceiver::receive(std::vector<std::vector<uint8_t>> &packets) {
+  auto idx{0U};
+  auto old_arrival_time64{uint64_t(0)};
+  while (true) {
+    auto pollfds{pollfd({.fd = sockfd, .events = POLLIN, .revents = 0})};
+    auto poll_res{ppoll(&pollfds, 1, nullptr, nullptr)};
+    if (poll_res < 0) {
+      throw std::runtime_error("ppoll error");
+    } else if (poll_res == 0) {
+      std::this_thread::sleep_for(std::chrono::milliseconds(4));
+    } else {
+      if (POLLIN & pollfds.revents) {
+        auto base{reinterpret_cast<uint8_t *>(mmap_base)};
+        auto header{reinterpret_cast<tpacket2_hdr *>(base + idx * frame_size)};
+        // Iterate through packets in the ring buffer
+
+        do {
+          if (header->tp_status & TP_STATUS_USER) {
+
+            std::cout << std::dec << std::endl;
+            // Hand this entry of the ring buffer (frame) back to kernel
+
+            header->tp_status = TP_STATUS_KERNEL;
+          } else {
+            // this packet is not tp_status_user, poll again
+
+            continue;
+          }
+          // Go to next frame in ring buffer
+
+          idx = ((idx + 1) % rx_buffer_cnt);
+          header = reinterpret_cast<tpacket2_hdr *>(base + idx * frame_size);
+        } while (header->tp_status & TP_STATUS_USER);
+      }
+      std::this_thread::sleep_for(std::chrono::milliseconds(4));
+    }
+  }
 }
--- example/151_raw_ethernet/source04/src/PacketReceiver.h
+++ example/151_raw_ethernet/source04/src/PacketReceiver.h
@@ -3,39 +3,38 @@
 
 // header
  
-#include <cstdint> 
+#include <cstdint>
+#include <string>
+#include <vector>
+#include <array> 
 class PacketReceiver  {
         public:
-         PacketReceiver (const std::string& if_name = "lo", const uint32& block_size = 4096, const uint32& block_nr = 1, const uint32& frame_size = 128)       ;   
+         PacketReceiver (const std::string& if_name = "lo", const uint32_t& block_size = 4096, const uint32_t& block_nr = 1, const uint32_t& frame_size = 128)       ;   
          ~PacketReceiver ()       ;   
-        const int& GetSockfd () const      ;   
-        void SetSockfd (int sockfd)       ;   
-        void* GetMmapBase ()       ;   
-        void SetMmapBase (void* mmap_base)       ;   
-        const size_t& GetMmapSize () const      ;   
-        void SetMmapSize (size_t mmap_size)       ;   
-        const std::string& GetIfName () const      ;   
-        void SetIfName (std::string if_name)       ;   
-        const uint32& GetBlockSize () const      ;   
-        void SetBlockSize (uint32 block_size)       ;   
-        const uint32& GetBlockNr () const      ;   
-        void SetBlockNr (uint32 block_nr)       ;   
-        const uint32& GetFrameSize () const      ;   
-        void SetFrameSize (uint32 frame_size)       ;   
-        const uint32& GetFrameNr () const      ;   
-        void SetFrameNr (uint32 frame_nr)       ;   
-        const uint32& GetRxBufferCnt () const      ;   
-        void SetRxBufferCnt (uint32 rx_buffer_cnt)       ;   
+            /** @brief Receives packets from a socket and stores them in a vector.
+
+This function receives packets from a socket and stores them in a vector. 
+The vector is passed by reference, allowing the function to modify its contents directly.
+At the start of the function, the vector is cleared, removing all elements but not deallocating its memory.
+This is done to minimize memory allocations if a similar number of packets is returned each time the function is called.
+
+@param packets A reference to a vector where the received packets will be stored. 
+       The vector is cleared at the start of the function, but its memory is not deallocated.
+ 
+
+
+    */ 
+        void receive (std::vector<std::vector<uint8_t>>& packets)       ;   
         private:
         int sockfd;
         void* mmap_base;
         size_t mmap_size;
         std::string if_name;
-        uint32 block_size;
-        uint32 block_nr;
-        uint32 frame_size;
-        uint32 frame_nr;
-        uint32 rx_buffer_cnt;
+        uint32_t block_size;
+        uint32_t block_nr;
+        uint32_t frame_size;
+        uint32_t frame_nr;
+        uint32_t rx_buffer_cnt;
 };
 
 #endif /* !PACKETRECEIVER_H */
