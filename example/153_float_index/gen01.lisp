37367820e48254a1474a1e6b1b405a0b6e6b76b1
Martin Kielhorn <wolpumba@gmail.com>
2024-06-09 22:50:37 +0200

    code still not working
     to_float(12)='-3.40282e+38'  float_to_index(to_float(12))='2130706421'

--- example/153_float_index/gen01.lisp
+++ example/153_float_index/gen01.lisp
@@ -12,7 +12,7 @@
 						      ))))
 
 (let ()
-  (defparameter *source-dir* #P"example/151_raw_ethernet/source01/src/")
+  (defparameter *source-dir* #P"example/153_float_index/source01/src/")
   (defparameter *full-source-dir* (asdf:system-relative-pathname
 				   'cl-cpp-generator2
 				   *source-dir*))
@@ -25,100 +25,45 @@
     (merge-pathnames "main.cpp"
 		     *source-dir*))
    `(do0
-     (comments "based on https://gist.github.com/austinmarton/2862515")
+     
      (include<>
-      arpa/inet.h
-      linux/if_packet.h
-      linux/ip.h
-      linux/udp.h
-      net/if.h
-      netinet/ether.h
-      sys/ioctl.h
-      sys/socket.h
-      unistd.h
-
       iostream
-      array
-      span
-      string
-      system_error
-      format
+      cstdint
+      cstring
+      )
 
-      cstring)
-     (defun check (result msg)
-       (declare (type int result)
-		(type "const std::string&" msg))
-       (when (== -1 result)
-	 (throw (std--system_error errno
-				   (std--generic_category)
-				   msg))))
+     (defun to_float (n)
+       (declare (values float)
+		(type uint32_t n))
+       (incf n (- (<< 1u 23) 1))
+       (if (and n (paren (<< 1u 31)))
+	   (setf n (^ n (paren (<< 1u 31))))
+	   (setf n ~n))
+       "float f;"
+       (memcpy &f &n 4)
+       (return f))
+
+     (defun float_to_index (f)
+       (declare (values uint32_t)
+		(type float f))
+       "uint32_t n;"
+       (memcpy &n &f (sizeof n))
+
+       (if (and n (paren (<< 1u 31)))
+	   (setf n (^ n (paren (<< 1 31))))
+	   (setf n ~n))
+       (return (- n (- (<< 1u 23) 1)))
+       )
+
+     
+     
      (defun main (argc argv)
        (declare (type int argc)
 		(type char** argv)
 		(values int))
-       (handler-case
-	   (do0
-	    (comments "Open PF_PACKET socket")
-	    (let ((ifName (string "lo"))
-		  (sockfd (socket PF_PACKET SOCK_RAW (htons (hex #x800)))))
-	      (check sockfd (string "Failed to create socket")))
-	    (do0
-	     (comments "Set interface to promiscuous mode")
-	     (let ((ifopts "ifreq{}"))
-	       (strncpy ifopts.ifr_name
-			ifName
-			(- IFNAMSIZ 1))
-	       (check (ioctl sockfd SIOCGIFFLAGS &ifopts)
-		      (string "Failed to get interface flags"))
-	       (setf ifopts.ifr_flags (logior ifopts.ifr_flags
-					     IFF_PROMISC))
-	       (check (ioctl sockfd SIOCSIFFLAGS &ifopts)
-		      (string "Failed to set interface flags"))))
-	    (do0
-	     (comments "Allow the socket to be reused")
-	     (let ((sockopt SO_REUSEADDR))
-	       (check
-		(setsockopt sockfd SOL_SOCKET
-			    SO_REUSEADDR
-			    &sockopt (sizeof sockopt))
-		(string "Failed to set socket option SO_REUSEADDR"))))
-
-	    (do0
-	     (comments "Bind to device")
-	     (check (setsockopt sockfd SOL_SOCKET SO_BINDTODEVICE
-				ifName
-				(- IFNAMSIZ 1))
-		    (string "Failed to set socket option SO_BINDTODEVICE")))
-	    (do0
-	     (comments "Receive loop")
-	     (while true
-		    (let ((buf "std::array<uint8_t,1024>{}")
-			  (numbytes (recvfrom sockfd
-					      (buf.data)
-					      (buf.size)
-					      0
-					      nullptr
-					      nullptr)))
-		      (check numbytes (string "Failed to receive data"))
-		      (let ((eh (reinterpret_cast<ether_header*> (buf.data)))
-			    (receivedMac ("std::span<const uint8_t, 6>" eh->ether_dhost 6)))
-			;(lprint :vars `(receivedMac))
-			
-			)
-		      )
-	      ))
-	    (close sockfd)
-	    
-	    )
-	 ("const std::system_error&" (ex)
-	   (<< std--cerr
-	       (string "Error: ")
-	       (ex.what)
-	       (string " (")
-	       (ex.code)
-	       (string ")\\n"))
-	   (return 1)))
        
+       ,(lprint :vars `((to_float 12)
+			(float_to_index (to_float 12))))
        (return 0)))
    :omit-parens t
    :format t
--- example/153_float_index/source01/src/main.cpp
+++ example/153_float_index/source01/src/main.cpp
@@ -1,3 +1,34 @@
+#include <cstdint>
+#include <cstring>
 #include <iostream>
 
-int main(int argc, char **argv) { return 0; }
+float to_float(uint32_t n) {
+  n += ((1U << 23) - 1);
+  if ((n & (1U << 31)) != 0u) {
+    n = n ^ (1U << 31);
+  } else {
+    n = ~n;
+  }
+  float f;
+  memcpy(&f, &n, 4);
+  return f;
+}
+
+uint32_t float_to_index(float f) {
+  uint32_t n;
+  memcpy(&n, &f, sizeof(n));
+  if ((n & (1U << 31)) != 0u) {
+    n = n ^ (1 << 31);
+  } else {
+    n = ~n;
+  }
+  return n - ((1U << 23) - 1);
+}
+
+int main(int argc, char **argv) {
+  std::cout << ""
+            << " to_float(12)='" << to_float(12) << "' "
+            << " float_to_index(to_float(12))='" << float_to_index(to_float(12))
+            << "' " << std::endl;
+  return 0;
+}
