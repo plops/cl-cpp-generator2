56456a5ae9436f3e3f367487618e07b21a28df92
Martin Kielhorn <wolpumba@gmail.com>
2024-09-20 00:09:05 +0200

    compile vulkan example

--- example/04_vulkan/source/run_00_test.cpp
+++ example/04_vulkan/source/run_00_test.cpp
@@ -8,8 +8,8 @@
 #define GLM_FORCE_RADIANS
 #define GLM_FORCE_DEPTH_ZERO_TO_ONE
 
-#include <glm/mat4x4.hpp>
-#include <glm/vec4.hpp>
+//#include <glm/mat4x4.hpp>
+//#include <glm/vec4.hpp>
 ;
 
 #include <iostream>
@@ -19,9 +19,10 @@ int main() {
   auto window = glfwCreateWindow(800, 600, "vulkan window", nullptr, nullptr);
   uint32_t extensionCount = 0;
   vkEnumerateInstanceExtensionProperties(nullptr, &extensionCount, nullptr);
-  glm::mat4 matrix;
+  /*glm::mat4 matrix;
   glm::vec4 vec;
   auto test = ((matrix) * (vec));
+  */
   while (!(glfwWindowShouldClose(window))) {
     glfwPollEvents();
   }
