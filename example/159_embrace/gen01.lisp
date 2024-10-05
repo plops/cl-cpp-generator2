93901cfd5a109cacfd9c3e5df3e5e7d3cd555313
Martin Kielhorn <wolpumba@gmail.com>
2024-10-05 14:59:36 +0200

    more

--- example/159_embrace/gen01.lisp
+++ example/159_embrace/gen01.lisp
@@ -213,6 +213,8 @@
        (comments "we can use noexcept to verify that an expression will never throw. i think that could be useful in embedded code. place it into unit tests"
 		 "static_assert(noexcept(sizeof(throw,1)))"
 		 "by default new can throw but there is an overload that takes std::nothrow_t argument and will return null instead")
+
+       (comments "move operations can lead to `memory diffusion`, causing cache misses and page faults. reducing runtime performance")
        
        (return 0)))
    :omit-parens t
