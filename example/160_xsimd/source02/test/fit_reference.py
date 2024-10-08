2129bd3a379c880a923592d7cde98dd3ca95b971
Martin Kielhorn <wolpumba@gmail.com>
2024-10-08 22:02:23 +0200

    forgot sqrt

--- example/160_xsimd/source02/test/fit_reference.py
+++ example/160_xsimd/source02/test/fit_reference.py
@@ -23,10 +23,10 @@ a = (Sy - Sx * b) / S # 1.83
 
 chi2 = sum(((y-a-b*x)/sig)**2) # .0017
 fac = sqrt(chi2/(N-2)) # .04
-siga_ = (1+Sx**2/(S*Stt))/S # 2.333
-sigb_ = 1/Stt # .5
-siga = siga_ * fac # .09
-sigb = sigb_ * fac #.02
+siga_ = sqrt((1+Sx**2/(S*Stt))/S) # 1.53
+sigb_ = sqrt(1/Stt) # .707
+siga = siga_ * fac # .062
+sigb = sigb_ * fac #.029
 # uncertainties of a and b estimated by assuming equal errors
 # and that line is a good model
 
