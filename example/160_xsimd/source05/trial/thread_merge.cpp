42cdbadc4abc1df126775958922b3c7ce73bfc45
Martin Kielhorn <wolpumba@gmail.com>
2024-10-12 12:38:33 +0200

    collection works
    ./thread_merge
    0 1 2 3 4 6 7 8 9 10 12 13 14 15 16 18 19 20 21 22 24 25 26 27 28 30 31 32 33 34
    martin@archlinux ~/stage/cl-cpp-generator2/example/160_xsimd/source05/trial/b $ ./thread_merge
    0 1 2 3 4 6 7 8 9 10 12 13 14 15 16 18 19 20 21 22 24 25 26 27 28 30 31 32 33 34
    martin@archlinux ~/stage/cl-cpp-generator2/example/160_xsimd/source05/trial/b $ ./thread_merge
    0 1 2 3 4 6 7 8 9 10 12 13 14 15 16 18 19 20 21 22 24 25 26 27 28 30 31 32 33 34
    martin@archlinux ~/stage/cl-cpp-generator2/example/160_xsimd/source05/trial/b $ ./thread_merge
    0 1 2 3 4 6 7 8 9 10 12 13 14 15 16 18 19 20 21 22 24 25 26 27 28 30 31 32 33 34
    martin@archlinux ~/stage/cl-cpp-generator2/example/160_xsimd/source05/trial/b $ ./thread_merge
    0 1 2 3 4 6 7 8 9 10 12 13 14 15 16 18 19 20 21 22 24 25 26 27 28 30 31 32 33 34

--- example/160_xsimd/source05/trial/thread_merge.cpp
+++ example/160_xsimd/source05/trial/thread_merge.cpp
@@ -1,27 +1,30 @@
 #include <iostream>
 #include <vector>
-#include <numeric> // For std::iota
-#include <algorithm> // For std::merge
-#include <chrono> // For std::chrono_literals
+#include <numeric> // For iota
+#include <algorithm> // For merge
+#include <chrono> // For chrono_literals
+#include <thread>
+
+using namespace std;
 
 // g++ -std=gnu++20 -march=native -O2 -ggdb3 thread_merge.cpp -o thread_merge
 
 // Function to compute a vector of 5 elements
-std::vector<int> computeVector() {
-    std::vector<int> result(5);
-    std::iota(result.begin(), result.end(), 0); // Fill with sequential values starting from 0
+vector<int> computeVector(int i) {
+    vector<int> result(5);
+    iota(result.begin(), result.end(), i*6); // Fill with sequential values starting from 0
     return result;
 }
 
 int main() {
     const int num_threads = 6;
-    std::vector<std::jthread> threads(num_threads);
-    std::vector<std::vector<int>> results(num_threads);
+    vector<jthread> threads(num_threads);
+    vector<vector<int>> results(num_threads);
 
     // Launch the computation in each thread
     for (int i = 0; i < num_threads; ++i) {
-        threads[i] = std::jthread([i, &results]() mutable {
-            results[i] = computeVector();
+        threads[i] = jthread([i, &results]() mutable {
+            results[i] = computeVector(i);
         });
     }
 
@@ -33,18 +36,18 @@ int main() {
     }
 
     // Merge the results into one vector with 30 elements
-    std::vector<int> final_result(30);
+    vector<int> final_result(30);
     int pos = 0;
     for (const auto& vec : results) {
-        std::copy(vec.begin(), vec.end(), final_result.begin() + pos);
+        copy(vec.begin(), vec.end(), final_result.begin() + pos);
         pos += vec.size();
     }
 
     // Print the merged result
     for (int val : final_result) {
-        std::cout << val << " ";
+        cout << val << " ";
     }
-    std::cout << std::endl;
+    cout << endl;
 
     return 0;
 }
