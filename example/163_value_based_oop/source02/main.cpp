b1cb8d51fb2350115faa4248af3331ea63843aac
Martin Kielhorn <wolpumba@gmail.com>
2025-01-18 12:26:52 +0100

    indent

--- example/163_value_based_oop/source02/main.cpp
+++ example/163_value_based_oop/source02/main.cpp
@@ -1,26 +1,28 @@
 #include <memory>
 
-struct Interface {
+struct Interface
+{
     virtual ~Interface() = default;
     virtual void getTreat() = 0;
     virtual void getPetted() = 0;
 };
 
 
-class StatelessTE {
+class StatelessTE
+{
 private:
     std::unique_ptr<Interface> pimpl;
-    public:
-        template<typename Object, typename Strategy>
-        StatelessTE(Object&& object, Strategy&& strategy)
-            : pimpl(std::make_unique<Implementation<std::remove_cvref_t<Object>,
-                std::remove_cvref_t<Strategy>>>(
-                std::forward<Object>(object),
-                                                  std::forward<Strategy>(strategy)))
-        {}
-    void getTreat() {pimpl->getTreat();}
-    void getPetted() {pimpl->getPetted();}
-        };
+
+public:
+    template <typename Object, typename Strategy>
+    StatelessTE(Object&& object, Strategy&& strategy) :
+        pimpl(std::make_unique<Implementation<std::remove_cvref_t<Object>, std::remove_cvref_t<Strategy>>>(
+            std::forward<Object>(object), std::forward<Strategy>(strategy)))
+    {
+    }
+    void getTreat() { pimpl->getTreat(); }
+    void getPetted() { pimpl->getPetted(); }
+};
 
 int main()
 {
