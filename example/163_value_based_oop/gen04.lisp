60565a7013cc470c69d6af811a30897c19bbed95
Martin Kielhorn <wolpumba@gmail.com>
2025-01-19 01:40:52 +0100

    more

--- example/163_value_based_oop/gen04.lisp
+++ example/163_value_based_oop/gen04.lisp
@@ -47,128 +47,8 @@
 		   (string "' ")))   
 	 std--endl))
 
-  
-  (defun create-type-erasure (&key name
-				functions
-				typenames
-				)
-    "functions .. ((:name getTreat :return void :params ()) ... )
-where params .. ((:pname alpha :type int) ...)"
-    (let* (
-	  (type-objects (loop for e in typenames
-			      collect
-			      (string-downcase (format nil "~a_" e))))
-	  (type-object-accessors (loop for e in typenames
-				       collect
-				       (string-downcase (format nil "~a" e))))
-	  (typenames2 (loop for e in typenames
-			    collect
-			    (gensym (format nil "~a" e))))
-	  (type-objects2 (loop for e in typenames2
-			      collect
-			      (intern (string-upcase (format nil "~a" e)))))
-	  )
-     `(defclass+ ,name ()
-	"private:"
-	(space std--unique_ptr (angle Interface) pimpl)
-	(defclass+ Interface ()
-	  "public:"
-	  (space virtual (~Interface) = default)
-	  ,@(loop for e in functions
-		  collect
-		  (destructuring-bind (&key name return params code) e
-		    `(space virtual ,return
-			    (,name
-			     ,@(loop for p in params
-				     collect
-				     (destructuring-bind (&key pname type)
-					 p
-				       `(space ,type ,pname)))) = 0))))
-	(space template (angle "typename Type")
-	       struct is_shared_ptr ":" std--false_type (progn))
-	(space template (angle "typename Type")
-	       struct is_shared_ptr (angle (space std--shared_ptr
-						  (angle Type)))
-	       (progn))
-	(space template
-	       (angle ,@(loop for ty in typenames
-			      collect
-			      (format nil "typename ~a" ty)))
-	       (defclass+ Implementation ("public Interface")
-		 "private:"
-		 ,@(loop for type in typenames
-			 and object in type-objects
-			 collect
-			 `(space ,type ,object))
-		 "public:"
-		 ,@(loop for type in typenames
-			 and access in type-object-accessors
-			 and object in type-objects
-			 collect
-			 `(defmethod ,access ()
-			    (if-constexpr (space is_shared_ptr
-					(angle (space
-						std--remove_cvref_t
-						(angle ,type)))
-					--value)
-				 (return (deref ,object))
-				 (return ,object))))))
-	(space template
-	       (angle ,@(loop for e in typenames2
-			      collect
-			      (format nil "typename ~a" e)))
-	       (defmethod Implementation (,@type-objects2)
-		 (declare ,@(loop for e in typenames2
-				  and f in type-objects2
-				  collect `(type ,(format nil "~a&&" e) ,f))
-			  
-			  (construct
-			   ,@(loop for e in typenames2
-				  and f in type-objects2
-				   and g in type-objects
-				   collect
-				   `(,g (space std--forward
-					       ((angle ,e) ,f)))))))
-	       )
-       
-	"public:"
-	,@(loop for e in functions
-		
-		collect
-		  (destructuring-bind (&key name return params code) e
-		    `(defmethod ,name ()
-		       #+nil ,(loop for p in params
-				     collect
-				     (destructuring-bind (&key pname type)
-					 p
-				       `(space ,type ,pname)))
-		       (declare (override)
-				(virtual)
-				(values ,return))
-		       ,code)))))
-    )
-  (write-source 
-   (asdf:system-relative-pathname
-    'cl-cpp-generator2 
-    (merge-pathnames "main.cpp"
-		     *source-dir*))
-   `(do0
-     (include<>
-      iostream
-      format
-      vector
-      memory
-      )
-
-     ,(create-type-erasure
-       :name `UniversalTE
-       :functions `((:name getTreat :return void :params () :code (dot (strategy)
-								       (getTreat (object))))
-		    (:name getPetted :return void :params () :code (dot (strategy)
-								       (getPetted (object)))))
-       :typenames `(Object Strategy))
-     #+nil
-     (defclass+ UniversalTE ()
+  #+nil
+  (defclass+ UniversalTE ()
        "private:"
        (space std--unique_ptr (angle Interface) pimpl)
        (defclass+ Interface ()
@@ -226,6 +106,128 @@ where params .. ((:pname alpha :type int) ...)"
 	 (declare (override))
 	 (dot (strategy)
 	      (getPetted (object)))))
+  
+  (defun create-type-erasure (&key name
+				functions
+				typenames
+				)
+    "functions .. ((:name getTreat :return void :params ()) ... )
+where params .. ((:pname alpha :type int) ...)"
+    (let* (
+	   (type-objects (loop for e in typenames
+			       collect
+			       (string-downcase (format nil "~a_" e))))
+	   (type-object-accessors (loop for e in typenames
+					collect
+					(string-downcase (format nil "~a" e))))
+	   (typenames2 (loop for e in typenames
+			     collect
+			     (gensym (format nil "~a" e))))
+	   (type-objects2 (loop for e in typenames2
+				collect
+				(intern (string-upcase (format nil "~a" e)))))
+	   )
+      `(defclass+ ,name ()
+	 "private:"
+	 (space std--unique_ptr (angle Interface) pimpl)
+	 (defclass+ Interface ()
+	   "public:"
+	   (space virtual (~Interface) = default)
+	   ,@(loop for e in functions
+		   collect
+		   (destructuring-bind (&key name return params code) e
+		     `(space virtual ,return
+			     (,name
+			      ,@(loop for p in params
+				      collect
+				      (destructuring-bind (&key pname type)
+					  p
+					`(space ,type ,pname)))) = 0))))
+	 (space template (angle "typename Type")
+		struct is_shared_ptr ":" std--false_type (progn))
+	 (space template (angle "typename Type")
+		struct is_shared_ptr (angle (space std--shared_ptr
+						   (angle Type)))
+		(progn))
+	 (space template
+		(angle ,@(loop for ty in typenames
+			       collect
+			       (format nil "typename ~a" ty)))
+		(defclass+ Implementation ("public Interface")
+		  "private:"
+		  ,@(loop for type in typenames
+			  and object in type-objects
+			  collect
+			  `(space ,type ,object))
+		  "public:"
+		  ,@(loop for type in typenames
+			  and access in type-object-accessors
+			  and object in type-objects
+			  collect
+			  `(defmethod ,access ()
+			     (if-constexpr (space is_shared_ptr
+						  (angle (space
+							  std--remove_cvref_t
+							  (angle ,type)))
+						  --value)
+					   (return (deref ,object))
+					   (return ,object))))))
+	 (space template
+		(angle ,@(loop for e in typenames2
+			       collect
+			       (format nil "typename ~a" e)))
+		(defmethod Implementation (,@type-objects2)
+		  (declare ,@(loop for e in typenames2
+				   and f in type-objects2
+				   collect `(type ,(format nil "~a&&" e) ,f))
+			  
+			   (construct
+			    ,@(loop for e in typenames2
+				    and f in type-objects2
+				    and g in type-objects
+				    collect
+				    `(,g (space std--forward
+						((angle ,e) ,f)))))))
+		)
+       
+	 "public:"
+	 ,@(loop for e in functions
+		
+		 collect
+		 (destructuring-bind (&key name return params code) e
+		   `(defmethod ,name ()
+		      #+nil ,(loop for p in params
+				   collect
+				   (destructuring-bind (&key pname type)
+				       p
+				     `(space ,type ,pname)))
+		      (declare (override)
+			       (virtual)
+			       (values ,return))
+		      ,code)))))
+    )
+  (write-source 
+   (asdf:system-relative-pathname
+    'cl-cpp-generator2 
+    (merge-pathnames "main.cpp"
+		     *source-dir*))
+   `(do0
+     (include<>
+      iostream
+      format
+      vector
+      memory
+      )
+
+     ,(create-type-erasure
+       :name `UniversalTE
+       :functions `((:name getTreat :return void :params () :code (dot (strategy)
+								       (getTreat (object))))
+		    (:name getPetted :return void :params () :code (dot (strategy)
+								       (getPetted (object)))))
+       :typenames `(Object Strategy))
+     4
+     
 
 
      )
