c9c36700593bfef54f63fc82fbd572d5af87566b
Martin Kielhorn <wolpumba@gmail.com>
2025-02-07 23:15:58 +0100

    start debugging

--- example/164_daxa/src/main.cpp
+++ example/164_daxa/src/main.cpp
@@ -4,13 +4,14 @@
 
 #include "window.h"
 #include <daxa/daxa.hpp>
+using namespace daxa;
 
 int main(int argc, char const* argv[]) {
     // Create a window
     auto window = AppWindow("Learn Daxa", 860, 640);
 
-    auto instance{daxa::create_instance({})};
-
+    auto instance{create_instance({})};
+    auto device{instance.create_device_2(instance.choose_device({},{}))};
     // Main loop
     while (!window.should_close()) {
         window.update();
