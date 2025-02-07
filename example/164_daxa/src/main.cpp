daf8dbb3d4704ef7a20e626eca5027597ad08bf1
Martin Kielhorn <wolpumba@gmail.com>
2025-02-07 23:25:35 +0100

    more code

--- example/164_daxa/src/main.cpp
+++ example/164_daxa/src/main.cpp
@@ -2,23 +2,35 @@
 // Created by martin on 2/7/25.
 //
 
-#include "window.h"
 #include <daxa/daxa.hpp>
+#include "window.h"
 using namespace daxa;
 
-int main(int argc, char const* argv[]) {
+int main(int argc, char const* argv[])
+{
     // Create a window
     auto window = AppWindow("Learn Daxa", 860, 640);
 
     auto instance{create_instance({})};
-    auto device{instance.create_device_2(instance.choose_device({},{}))};
-    auto swapchain{
-    device.create_swapchain(
-        {.native_window{window.get_native_handle()},
-        .native_window_platform{window.get_native_platform()},})};
+    auto device{instance.create_device_2(instance.choose_device({}, {}))};
+    auto swapchain{device.create_swapchain({.native_window{window.get_native_handle()},
+                                            .native_window_platform{window.get_native_platform()},
+                                            .surface_format_selector{[](Format format)
+                                                                     {
+                                                                         switch (format)
+                                                                         {
+                                                                         case Format::R8G8B8A8_UINT:
+                                                                             return 100;
+                                                                         default:
+                                                                             return default_format_score(format);
+                                                                         }
+                                                                     }},
+                                            .present_mode{PresentMode::MAILBOX},
+                                            .image_usage{ImageUsageFlagBits::TRANSFER_DST},
+                                            .name{"my swapchain"}})};
     // Main loop
-    while (!window.should_close()) {
-        window.update();
+    while (!window.should_close())
+    {
     }
 
     return 0;
