6a42a5af81ec7526838128b2b553af8ff0f7273a
Martin Kielhorn <wolpumba@gmail.com>
2025-02-08 07:08:07 +0100

    compile errors
    ~~~~~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
    /home/martin/src/Daxa/src/utils/impl_task_graph.hpp: At global scope:
    /home/martin/src/Daxa/src/utils/impl_task_graph.hpp:203:28: error: ‘atomic_uint32_t’ in namespace ‘std’ does not name a type
      203 |         static inline std::atomic_uint32_t exec_unique_next_index = 1;

--- example/164_daxa/daxa_cfg/setup01_compile_daxa.sh
+++ example/164_daxa/daxa_cfg/setup01_compile_daxa.sh
@@ -7,14 +7,14 @@ cmake .. \
       -DCMAKE_PREFIX_PATH="/home/martin/vulkan;/home/martin/src/imgui" \
       -DDAXA_USE_VCPKG=false \
       -DDAXA_ENABLE_UTILS_FSR2=false \
-      -DDAXA_ENABLE_UTILS_IMGUI=true \
+      -DDAXA_ENABLE_UTILS_IMGUI=false \
       -DDAXA_ENABLE_UTILS_MEM=false \
       -DDAXA_ENABLE_UTILS_PIPELINE_MANAGER_GLSLANG=true \
       -DDAXA_ENABLE_UTILS_PIPELINE_MANAGER_SLANG=false \
       -DDAXA_ENABLE_UTILS_PIPELINE_MANAGER_SPIRV_VALIDATION=false \
       -DDAXA_ENABLE_UTILS_TASK_GRAPH=true \
-      -DDAXA_ENABLE_TESTS=true \
-      -DDAXA_ENABLE_TOOLS=true \
+      -DDAXA_ENABLE_TESTS=false \
+      -DDAXA_ENABLE_TOOLS=false \
       -DDAXA_ENABLE_STATIC_ANALYSIS=false \
       -DCMAKE_CXX_STANDARD=20 \
       -DBUILD_SHARED_LIBS=false \
