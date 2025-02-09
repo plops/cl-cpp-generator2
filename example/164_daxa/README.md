034b0678cd0a7673a3b080d8cc450bba71c76c6e
Martin Kielhorn <wolpumba@gmail.com>
2025-02-09 12:12:14 +0100

    add readme

---
+++ example/164_daxa/README.md
@@ -0,0 +1,9 @@
+# install dependencies
+
+- install vulkan, glfw, spirv, glslang
+
+```bash
+cd ~/src
+git clone --recursive https://github.com/Ipotrick/Daxa
+git clone --recursive https://github.com/GPUOpen-LibrariesAndSDKs/VulkanMemoryAllocator
+```
---
+++ example/164_daxa/setup01_compile_daxa.sh
@@ -0,0 +1,21 @@
+#!/usr/bin/bash
+
+cmake .. \
+      -GNinja \
+      -DCMAKE_BUILD_TYPE=Release \
+      -DDAXA_INSTALL=ON -DCMAKE_INSTALL_PREFIX=/home/martin/vulkan \
+      -DCMAKE_PREFIX_PATH="/home/martin/vulkan;/home/martin/src/imgui" \
+      -DDAXA_USE_VCPKG=false \
+      -DDAXA_ENABLE_UTILS_FSR2=false \
+      -DDAXA_ENABLE_UTILS_IMGUI=true \
+      -DDAXA_ENABLE_UTILS_MEM=false \
+      -DDAXA_ENABLE_UTILS_PIPELINE_MANAGER_GLSLANG=true \
+      -DDAXA_ENABLE_UTILS_PIPELINE_MANAGER_SLANG=false \
+      -DDAXA_ENABLE_UTILS_PIPELINE_MANAGER_SPIRV_VALIDATION=false \
+      -DDAXA_ENABLE_UTILS_TASK_GRAPH=true \
+      -DDAXA_ENABLE_TESTS=true \
+      -DDAXA_ENABLE_TOOLS=true \
+      -DDAXA_ENABLE_STATIC_ANALYSIS=false \
+      -DCMAKE_CXX_STANDARD=20 \
+      -DBUILD_SHARED_LIBS=false \
+      -DCMAKE_INCLUDE_DIR=/home/martin/src/imgui
