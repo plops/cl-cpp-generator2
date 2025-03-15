37e3898175eaef5050c6d4574a2856bc78f7dfdd
Martin <wolpumba@gmail.com>
2025-03-15 11:08:00 +0100

    use regex to find videos

--- example/169_netview/source01/src/main.cpp
+++ example/169_netview/source01/src/main.cpp
@@ -1,5 +1,8 @@
+// Stroustrup A Tour of C++ (2022) p. 151
+
 #include <filesystem>
 #include <iostream>
+#include <regex>
 using namespace std;
 using namespace std::filesystem;
 int main(int argc, char* argv[])
@@ -9,6 +12,33 @@ int main(int argc, char* argv[])
         cerr << "Usage: " << argv[0] << " <path>" << endl;
         return 1;
     }
-   path f{argv[1]};
-
+    path p{argv[1]};
+    auto collect_videos = [](const path& p)
+    {
+        try
+        {
+            if (is_directory(p))
+            {
+                for (const auto& entry : recursive_directory_iterator(p))
+                {
+                    if (entry.is_regular_file())
+                    {
+                        auto fn{entry.path().filename().string()};
+                        regex pat{R"(.*\.(webm|mp4|mkv)(\.part)?$)"};
+                        auto match = regex_match(fn, pat);
+                        if (match)
+                        {
+                            cout << fn << endl;
+                        }
+                    }
+                }
+            }
+        }
+        catch (const filesystem_error& e)
+        {
+            cerr << e.what() << endl;
+        }
+    };
+    collect_videos(p);
+    return 0;
 }
