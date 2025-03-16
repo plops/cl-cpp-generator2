103f8fe0c6a2f08dd5b16b8c657113d7b9f935b1
Martin <wolpumba@gmail.com>
2025-03-16 16:15:32 +0100

    compute bin coordinates for histogram

--- example/169_netview/source01/src/VideoDecoder.cpp
+++ example/169_netview/source01/src/VideoDecoder.cpp
@@ -31,7 +31,7 @@ bool VideoDecoder::initialize(const string& uri, bool debug) {
     if (ec) { cerr << "Error finding stream information " << ec.message() << endl; }
     ssize_t videoStream{-1};
     auto    streamsCount = ctx->streamsCount();
-    for (auto i = 0; i < streamsCount; i++) {
+    for (long unsigned int i = 0; i < streamsCount; i++) {
         auto stream = ctx->stream(i);
         auto type   = stream.mediaType();
 
@@ -59,39 +59,41 @@ bool VideoDecoder::initialize(const string& uri, bool debug) {
     vdec.open({{"threads", "12"}}, av::Codec(), ec);
     if (ec) { cerr << "Error opening video decoder codec id=" << id << " " << ec.message() << endl; }
 
-    auto videoPacketCount = 0;
-    av::Timestamp timestamp;
-    av::Timestamp timestamp_prev;
-    bool histIsInitialized = false;
-    const int n=128;
-    double durationMin = .0158;
-    double durationMax = .0175;
-    double durationRealMin = 1000000000.;
-    double durationRealMax = -100000000.;
-    array<uint64_t,n> histogram;
+    auto               videoPacketCount = 0;
+    av::Timestamp      timestamp;
+    av::Timestamp      timestamp_prev;
+    bool               histIsInitialized = false;
+    const int          n                 = 128;
+    double             durationMin       = .0158;
+    double             durationMax       = .0175;
+    double             durationRealMin   = 1000000000.;
+    double             durationRealMax   = -100000000.;
+    array<uint64_t, n> histogram;
+    array<double, n>   histogramX;
+    auto               count = 0;
+    for (auto&& x : histogramX) {
+        x = durationMin + count * (durationMax - durationMin) / (n - 1);
+        count++;
+    }
     histogram.fill(0);
     auto accum = [&](av::Timestamp ts) {
         if (!histIsInitialized) {
-            timestamp_prev = ts;
+            timestamp_prev    = ts;
             histIsInitialized = true;
             return;
         }
-        auto duration = ts.seconds() - timestamp_prev.seconds();
-        timestamp_prev = ts;
-        durationRealMin = min(durationRealMin, duration);
-        durationRealMax = max(durationRealMax, duration);
-        duration = clamp(duration, durationMin, durationMax);
-        const auto tau = (duration-durationMin)/(durationMax-durationMin);
-        const auto durationIdx = static_cast<uint64_t>(tau*(n-1));
+        auto duration          = ts.seconds() - timestamp_prev.seconds();
+        timestamp_prev         = ts;
+        durationRealMin        = min(durationRealMin, duration);
+        durationRealMax        = max(durationRealMax, duration);
+        duration               = clamp(duration, durationMin, durationMax);
+        const auto tau         = (duration - durationMin) / (durationMax - durationMin);
+        const auto durationIdx = static_cast<uint64_t>(tau * (n - 1));
         histogram[durationIdx]++;
     };
-    while (pkt = ctx->readPacket(ec)) {
-        if (ec) {
-            cerr << "Packet reading error: " << ec.message() << endl;
-        }
-        if (pkt.streamIndex() != videoStream) {
-            continue;
-        }
+    while ((pkt = ctx->readPacket(ec))) {
+        if (ec) { cerr << "Packet reading error: " << ec.message() << endl; }
+        if (pkt.streamIndex() != videoStream) { continue; }
         timestamp = pkt.ts();
         if (debug)
             accum(timestamp);
@@ -99,12 +101,11 @@ bool VideoDecoder::initialize(const string& uri, bool debug) {
     }
     if (debug) {
         cout << "Packet #=" << videoPacketCount << " timestamp=" << timestamp.seconds() << "s" <<
-            endl;
+                endl;
         cout << "durationRealMin=" << durationRealMin << endl;
         cout << "durationRealMax=" << durationRealMax << endl;
-        for (const auto& h : histogram) {
-            cout << "hist " << h << endl;
-        }
+
+        for (int i = 0; i < n; i++) { cout << "hist " << histogramX[i] << " " << histogram[i] << endl; }
     }
     isInitialized = true;
     return true;
