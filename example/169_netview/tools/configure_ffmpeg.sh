625028c6154514e10add2007ecdd48cb3e25baee
Martin <wolpumba@gmail.com>
2025-03-19 06:04:18 +0100

    include ffmpeg sources directly
    /home/martin/src/ffmpeg-6.1.2/libavutil/channel_layout.c: At top level:
    /home/martin/src/ffmpeg-6.1.2/libavutil/channel_layout.c:216:32: error: expected ‘;’ before ‘static’
      216 | FF_DISABLE_DEPRECATION_WARNINGS
          |                                ^

--- example/169_netview/tools/configure_ffmpeg.sh
+++ example/169_netview/tools/configure_ffmpeg.sh
@@ -1,47 +1,16 @@
 #!/bin/bash
-# --enable-libaom av1 decode 
-#   --enable-libdav1d        enable AV1 decoding via libdav1d [no]
-#   --enable-libvpx vp9 decode
-  # --disable-safe-bitstream-reader 
-  #                          disable buffer boundary checking in bitreaders
-  #                          (faster, but may crash)
-# --disable-asm
-#--disable-avformat
-#--disable-avcodec 
-
-# ./configure \
-# --enable-gpl \
-# --enable-version3 \
-# --enable-nonfree \
-# --enable-static \
-# --disable-runtime-cpudetect \
-# --disable-autodetect \
-# --disable-programs \
-# 	    --disable-doc \
-# 	    --disable-iconv \
-#   --disable-avdevice \
-#   --disable-swresample \
-#   --disable-swscale \
-#   --disable-postproc \
-#   --disable-avfilter \
-#   --disable-muxers \
-#   --disable-bsfs \
-#   --disable-protocols \
-#   --disable-everything  \
-#   --enable-decoder=vp9,h264,av1,av1_decoder
-
-  ./configure \
---disable-everything \
-      --enable-gpl \
---enable-version3 \
---enable-nonfree \
-      --enable-static \
-      --disable-shared \
---disable-runtime-cpudetect \
---disable-autodetect \
---disable-programs \
-	    --disable-doc \
-	    --disable-iconv \
+./configure \
+  --disable-everything \
+  --enable-gpl \
+  --enable-version3 \
+  --enable-nonfree \
+  --enable-static \
+  --disable-shared \
+  --disable-runtime-cpudetect \
+  --disable-autodetect \
+  --disable-programs \
+	--disable-doc \
+	--disable-iconv \
   --disable-avdevice \
   --disable-swresample \
   --disable-swscale \
@@ -63,14 +32,14 @@
   --enable-parser=h264,vp9,av1 \
   --enable-bsf=av1_frame_merge,av1_frame_split,av1_metadata,h264_metadata,h264_mp4toannexb,h264_redundant_pps,vp9_metadata,vp9_raw_reorder,vp9_superframe,vp9_superframe_split \
   --disable-protocols \
-    --disable-indevs \
+  --disable-indevs \
   --disable-outdevs  \
   --disable-devices  \
   --disable-filters  \
-     --disable-xlib  \
+  --disable-xlib  \
   --enable-vaapi   \
   --enable-vulkan  \
   --enable-lto \
   --disable-debug  \
-   --enable-extra-warnings 
+  --disable-asm
  
