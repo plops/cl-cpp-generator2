8626a5a71fe8882fa8a78c713d2640f97c6b61d5
Martin <wolpumba@gmail.com>
2025-03-21 19:22:42 +0100

    nolibs build links with less libraries
    -rwxr-xr-x 1 martin martin 4.2M Mar 21 19:21 cxxnet_client
    martin@toboggan ~/stage/cl-cpp-generator2/example/169_netview/source01/cmake-build-release-nolibs $ ldd cxxnet_client
            linux-vdso.so.1 (0x00007f13c7236000)
            libx264.so.164 => /usr/lib64/libx264.so.164 (0x00007f13c6bca000)
            libpulse.so.0 => /usr/lib64/libpulse.so.0 (0x00007f13c6b71000)
            libpulsecommon-17.0.so => /usr/lib64/libpulsecommon-17.0.so (0x00007f13c6ae3000)
            libsndfile.so.1 => /usr/lib64/libsndfile.so.1 (0x00007f13c6a58000)
            libsystemd.so.0 => /usr/lib64/libsystemd.so.0 (0x00007f13c6951000)
            libasyncns.so.0 => /usr/lib64/libasyncns.so.0 (0x00007f13c6948000)
            libFLAC.so.12 => /usr/lib64/libFLAC.so.12 (0x00007f13c68e8000)
            libvorbis.so.0 => /usr/lib64/libvorbis.so.0 (0x00007f13c68ba000)
            libvorbisenc.so.2 => /usr/lib64/libvorbisenc.so.2 (0x00007f13c6822000)
            libogg.so.0 => /usr/lib64/libogg.so.0 (0x00007f13c6818000)
            libmpg123.so.0 => /usr/lib64/libmpg123.so.0 (0x00007f13c67cf000)
            libmp3lame.so.0 => /usr/lib64/libmp3lame.so.0 (0x00007f13c6754000)
            libcap.so.2 => /usr/lib64/libcap.so.2 (0x00007f13c6747000)
            libmvec.so.1 => /usr/lib64/libmvec.so.1 (0x00007f13c664e000)
            libstdc++.so.6 => /usr/lib/gcc/x86_64-pc-linux-gnu/14/libstdc++.so.6 (0x00007f13c63d2000)
            libm.so.6 => /usr/lib64/libm.so.6 (0x00007f13c62ef000)
            libgcc_s.so.1 => /usr/lib/gcc/x86_64-pc-linux-gnu/14/libgcc_s.so.1 (0x00007f13c62c1000)
            libc.so.6 => /usr/lib64/libc.so.6 (0x00007f13c60cd000)
            libdbus-1.so.3 => /usr/lib64/libdbus-1.so.3 (0x00007f13c607c000)
            libxcb.so.1 => /usr/lib64/libxcb.so.1 (0x00007f13c6050000)
            libopus.so.0 => /usr/lib64/libopus.so.0 (0x00007f13c5fee000)
            /lib64/ld-linux-x86-64.so.2 (0x00007f13c7238000)
            libXau.so.6 => /usr/lib64/libXau.so.6 (0x00007f13c5fe6000)
            libXdmcp.so.6 => /usr/lib64/libXdmcp.so.6 (0x00007f13c5fde000)

--- example/169_netview/tools/configure_ffmpeg.sh
+++ example/169_netview/tools/configure_ffmpeg.sh
@@ -43,3 +43,44 @@
   --disable-debug  \
   --disable-asm
  
+./configure \
+  --disable-everything \
+  --enable-gpl \
+  --enable-version3 \
+  --enable-nonfree \
+  --enable-static \
+  --disable-shared \
+  --disable-runtime-cpudetect \
+  --disable-autodetect \
+  --disable-programs \
+	--disable-doc \
+	--disable-iconv \
+  --disable-postproc \
+  --disable-w32threads \
+  --disable-os2threads \
+  --disable-network    \
+  --disable-dwt \
+  --disable-error-resilience \
+  --disable-lsp      \
+  --disable-faan     \
+  --disable-pixelutils \
+  --disable-encoders   \
+  --enable-decoder=h264 \
+  --disable-hwaccels \
+  --disable-muxers \
+  --enable-demuxer=h264 \
+  --enable-parser=h264 \
+  --enable-bsf=h264_metadata,h264_mp4toannexb,h264_redundant_pps \
+  --disable-protocols \
+  --disable-indevs \
+  --disable-outdevs  \
+  --disable-devices  \
+  --disable-filters  \
+  --disable-xlib  \
+  --disable-vaapi   \
+  --disable-vulkan  \
+  --enable-lto \
+  --disable-debug  \
+  --disable-asm \
+  --prefix=/home/martin/ffmpeg
+
