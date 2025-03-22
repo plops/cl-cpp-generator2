81126c89b543e47027e7f18570db970ed5c276cc
Martin <wolpumba@gmail.com>
2025-03-22 19:04:50 +0100

    another unit test

--- example/169_netview/source01/tests/test_VideoDecoder.cpp
+++ example/169_netview/source01/tests/test_VideoDecoder.cpp
@@ -7,23 +7,32 @@
 #include <gtest/gtest.h>
 #include <stdexcept>
 #include <unistd.h>
-
+#include <string>
 class VideoDecoderBaseTest : public ::testing::Test {
 public:
     VideoDecoderBaseTest() {}
 
 protected:
     void SetUp() final {
-        auto r=dec.initialize("/home/martin/stage/cl-cpp-generator2/example/143_ryzen_monitor/source01/tests/ring.webm");
-        ASSERT_EQ(r, 1);
+
     }
     void TearDown() final {
         // not needed
     }
+    string videoDir{"/home/martin/stage/cl-cpp-generator2/example/169_netview/source01/tests/"};
     VideoDecoder dec{};
 };
 
 TEST_F(VideoDecoderBaseTest, ShortVideo_CollectKeyFrames_CountCorrect) {
+    auto r=dec.initialize(videoDir+"ring.webm");
+    ASSERT_EQ(r, 1);
     auto kf = dec.collectKeyFrames();
     ASSERT_EQ(kf.size(), 14);
+};
+
+TEST_F(VideoDecoderBaseTest, OtherShortVideo_CollectKeyFrames_CountCorrect) {
+    auto r=dec.initialize(videoDir+"sonic.webm");
+    ASSERT_EQ(r, 1);
+    auto kf = dec.collectKeyFrames();
+    ASSERT_EQ(kf.size(), 21);
 };
