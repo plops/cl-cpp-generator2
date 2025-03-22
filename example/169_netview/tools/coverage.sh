775911b5ea59822f86d96cdfcd2d087aa5e07886
Martin <wolpumba@gmail.com>
2025-03-22 19:38:04 +0100

    add script to create coverage files

---
+++ example/169_netview/tools/coverage.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+BUILD_DIR=../source01/cmake-build-debug
+mkdir -p $BUILD_DIR
+cd $BUILD_DIR
+cmake .. -G Ninja \
+-DCMAKE_BUILD_TYPE=Debug \
+-DENABLE_ASAN:BOOL=ON -DENABLE_UBSAN=ON -DENABLE_LSAN=ON \
+-DENABLE_VID_TESTS:BOOL=ON
+ninja &&
+./unit_tests &&
+mkdir -p gcov &&
+cd gcov &&
+find ..|grep gcda|xargs gcov
