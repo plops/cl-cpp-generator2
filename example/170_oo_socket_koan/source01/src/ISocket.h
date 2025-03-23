86261f14da3da63db26258d992959497155db37c
Martin <wolpumba@gmail.com>
2025-03-23 23:20:13 +0100

    add interface

---
+++ example/170_oo_socket_koan/source01/src/ISocket.h
@@ -0,0 +1,18 @@
+//
+// Created by martin on 3/23/25.
+//
+
+#ifndef ISOCKET_H
+#define ISOCKET_H
+#include <string>
+
+class ISocket {
+public:
+virtual ~ISocket() = default;
+virtual bool open(int port) = 0;
+virtual void close() = 0;
+virtual bool send(const std::string& data) = 0;
+virtual std::string receive() = 0;
+}
+
+#endif //ISOCKET_H
