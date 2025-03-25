e07c9fbbc26581a0ca3ebc29b1bc372a9e0f0c78
Martin <wolpumba@gmail.com>
2025-03-25 22:28:00 +0100

    i don't know how to create an array of GrayscaleImage in a unique ptr

--- example/171_oo_pool_koan/src/FixedSizeImagePool.cpp
+++ example/171_oo_pool_koan/src/FixedSizeImagePool.cpp
@@ -3,8 +3,11 @@
 //
 
 #include "FixedSizeImagePool.h"
+#include <iostream>
 FixedSizeImagePool::FixedSizeImagePool(size_t capacity, int width, int height) :
-    width_{width}, height_{height}, capacity_{capacity}, images_{make_unique<GrayscaleImage[]>(capacity)},
+    width_{width}, height_{height}, capacity_{capacity},
+//images_{make_unique<GrayscaleImage[]>(capacity)},
+images_{new GrayscaleImage[]{{1,2},{3,4}}},
     available_(capacity_, true) {}
 FixedSizeImagePool::~FixedSizeImagePool() {}
 IImage* FixedSizeImagePool::acquireImage() {
@@ -20,4 +23,15 @@ IImage* FixedSizeImagePool::acquireImage() {
         cv_.wait(lock); // wait for an image to become available
     }
 }
-void    FixedSizeImagePool::releaseImage(IImage* image) {}
+void    FixedSizeImagePool::releaseImage(IImage* image) {
+    lock_guard lock{mutex_};
+    for (size_t i = 0; i < capacity_; ++i) {
+        if (&images_[i] == image) {
+            available_[i] = true;
+            cv_.notify_one(); // signal a waiting thread
+            return;
+        }
+    }
+    cerr << "Error: Image not found in pool." << endl;
+
+}
