bbd1d4e6fe61309876e1af434ce5026edeb33ad2
Martin <wolpumba@gmail.com>
2025-03-25 22:57:08 +0100

    try the pool

--- example/171_oo_pool_koan/src/main.cpp
+++ example/171_oo_pool_koan/src/main.cpp
@@ -2,8 +2,19 @@
 // Created by martin on 3/25/25.
 //
 
+#include "FixedSizeImagePool.h"
 #include "GrayscaleImage.h"
 #include "IImage.h"
 #include "IImagePool.h"
+#include <iostream>
+int main() {
+    FixedSizeImagePool pool(3,128,64);
+    for (int i = 0; i < 100; i++) {
+        IImage* image = pool.acquireImage();
+        cout<<"Acqured image: "<<i<<endl;
+        pool.releaseImage(image);
+        cout <<"Released image: "<<i<<endl;
+    }
 
-int main() { return 0; }
+    return 0;
+}
