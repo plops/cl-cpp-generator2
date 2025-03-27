2cb0f184126121372f577aedac209f97d6612d08
Martin <wolpumba@gmail.com>
2025-03-27 22:09:33 +0100

    some code was accidentally deleted

--- example/169_netview/source01/src/VideoDecoder.cpp
+++ example/169_netview/source01/src/VideoDecoder.cpp
@@ -36,7 +36,41 @@ bool VideoDecoder::initialize(const string& uri, bool debug) {
 
     av::init();
     if (debug) { av::setFFmpegLoggingLevel(AV_LOG_DEBUG); }
-   C
+        ctx = make_unique<av::FormatContext>();
+        ctx->openInput(uri, ec);
+        if (ec) {
+                cerr << "Error opening input file " << uri << " " << ec.message() << endl;
+                return false;
+            }
+        ctx->findStreamInfo(ec);
+        if (ec) { cerr << "Error finding stream information " << ec.message() << endl; }
+        auto streamsCount = ctx->streamsCount();
+        for (long unsigned int i = 0; i < streamsCount; i++) {
+                auto stream = ctx->stream(i);
+                auto type   = stream.mediaType();
+
+                if (debug) { cout << "Stream #=" << i << " type=" << type << endl; }
+                if (type == AVMEDIA_TYPE_VIDEO) {
+                        if (debug) { cout << "Found video stream" << endl; }
+                        vst         = stream;
+                        videoStream = i;
+                        break;
+                    }
+            }
+        if ((videoStream == -1) || vst.isNull()) {
+                cerr << "Video stream not found streamsCount=" << streamsCount << endl;
+                return false;
+            }
+        if (!vst.isValid()) {
+                cerr << "Video stream is not valid." << endl;
+                return false;
+            }
+        vdec    = av::VideoDecoderContext(vst);
+        auto id = vdec.raw()->codec_id;
+        codec   = av::findDecodingCodec(id);
+        vdec.setCodec(codec);
+        vdec.setRefCountedFrames(true);
+        vdec.open({{"threads", "1"}}, av::Codec(), ec);
     if (ec) { cerr << "Error opening video decoder codec id=" << id << " " << ec.message() << endl; }
 
     isInitialized = true;
