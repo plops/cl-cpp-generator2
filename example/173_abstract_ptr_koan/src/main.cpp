793fefbc91d04403a7ed713bcc1d99b839577428
Martin <wolpumba@gmail.com>
2025-04-01 21:31:20 +0200

    more

--- example/173_abstract_ptr_koan/src/main.cpp
+++ example/173_abstract_ptr_koan/src/main.cpp
@@ -1,3 +1,19 @@
 //
 // Created by martin on 4/1/25.
 //
+
+#include <iostream>
+#include <memory>
+#include <string>
+#include <cassert>
+
+using namespace std;
+
+class Widget {
+public:
+    int    id;
+    string name;
+
+    Widget(int i, string n) :
+        id{i}, name{move(n)} { cout << "Widget " << id << " (" << name << ") created." << endl; }
+};
