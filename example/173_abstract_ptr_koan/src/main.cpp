5b0a4531ef87313ac1aa2f8b8f12b19e2ac66130
Martin <wolpumba@gmail.com>
2025-04-01 21:38:19 +0200

    more

--- example/173_abstract_ptr_koan/src/main.cpp
+++ example/173_abstract_ptr_koan/src/main.cpp
@@ -11,9 +11,31 @@ using namespace std;
 
 class Widget {
 public:
-    int    id;
-    string name;
-
     Widget(int i, string n) :
         id{i}, name{move(n)} { cout << "Widget " << id << " (" << name << ") created." << endl; }
+
+    virtual      ~Widget() { cout << "Widget " << id << " (" << name << ") destroyed." << endl; }
+    virtual void display() const { cout << "Widget ID: " << id << ", Name: " << name << endl; }
+
+private:
+    int    id;
+    string name;
+};
+
+template <typename T>
+class AbstractPtr {
+public:
+    virtual      ~AbstractPtr() = default;
+    virtual bool isValid() const =0;
+    virtual T*   get() const = 0;
+    virtual T*   operator->() const = 0;
+    virtual T&   operator*() const =0;
+    AbstractPtr(const AbstractPtr&)            = delete;
+    AbstractPtr& operator=(const AbstractPtr&) = delete;
+    AbstractPtr(AbstractPtr&&)                 = delete;
+    AbstractPtr& operator=(AbstractPtr&&)      = delete;
+
+protected:
+    // Constructor for base class use only
+    AbstractPtr() = default;
 };
