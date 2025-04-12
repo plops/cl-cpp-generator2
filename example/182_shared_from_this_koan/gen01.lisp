a6b2cdf73e187e7bbc8527301fe6dcc00d262f94
Martin <wolpumba@gmail.com>
2025-04-12 08:05:28 +0200

    start a new koan

---
+++ example/182_shared_from_this_koan/gen01.lisp
@@ -0,0 +1,216 @@
+(eval-when (:compile-toplevel :execute :load-toplevel)
+  (ql:quickload "cl-cpp-generator2")
+  (ql:quickload "cl-ppcre")
+  (ql:quickload "cl-change-case"))
+
+(in-package :cl-cpp-generator2)
+
+(progn
+  (setf *features* (set-difference *features* (list :more)))
+  (setf *features* (set-exclusive-or *features* (list :more))))
+
+(progn
+  (progn
+    (defparameter *source-dir* #P"example/181_reference_koan/src/")
+    (defparameter *full-source-dir* (asdf:system-relative-pathname
+				     'cl-cpp-generator2
+				     *source-dir*)))
+  (defparameter *day-names*
+    '("Monday" "Tuesday" "Wednesday"
+      "Thursday" "Friday" "Saturday"
+      "Sunday"))
+  (ensure-directories-exist *full-source-dir*)
+  (load "../163_value_based_oop/util.lisp")
+  (write-source 
+   (asdf:system-relative-pathname
+    'cl-cpp-generator2
+    (merge-pathnames "main.cpp"
+		     *source-dir*))
+   `(do0
+     (include<>
+      iostream
+      array
+      deque
+      memory
+      atomic
+      ;condition_variable
+      ;mutex
+      ;thread
+      cassert
+      )
+     "using namespace std;"
+
+      "constexpr int N{3};"
+
+     "template<typename T, int N> class Arena;"
+     
+     (space "template<typename T>"
+	    (defclass+ Ref ()
+	      "public:"
+	      ;; ctor
+	      (defmethod Ref (r idx arena)
+		(declare (type T& r)
+			 (type "Arena<T,N>&" arena)
+			 (type int idx)
+			 (construct (ref r)
+				    (sp (createPriv idx arena)))
+			 (explicit)
+			 (values :constructor))
+		,(lprint :msg "Ref::ctor"
+			 :vars `(idx (dot sp (load) (get)) &ref &arena )))
+	      ;; dtor
+	      (defmethod ~Ref ()
+		(declare (values :constructor))
+		,(lprint :msg "~Ref" :vars `((use_count)))
+		(when (== 3 (use_count))
+		  
+		  #-nil ,(lprint :msg "#### # tell arena" :vars `((idx) (use_count)))
+		  ("sp.load()->arena.setUnused" (idx))
+		  )
+		)
+	      ;; copy ctor
+	      (defmethod Ref (rhs)
+		(declare (type "const Ref&" rhs)
+			 (construct (ref rhs.ref)
+				    (sp (rhs.sp.load) #+nil  (createPriv (-> (dot rhs sp (load))
+									idx)
+								    (-> (dot rhs sp (load))
+									arena))))
+			 (values :constructor))
+		,(lprint :msg "Ref::copy-ctor"
+			 :vars `("sp.load()->idx")))
+	      (defmethod use_count ()
+		(declare (values "long int"))
+		(return (dot sp (load)
+			     (use_count))))
+	      (defmethod idx ()
+		(declare (values "long int"))
+		(return (-> (dot sp (load)
+				 )
+			    idx)))
+	      "private:"
+	      (defclass+ Priv ()
+		"public:"
+		"int idx;"
+		"Arena<T,N>& arena;")
+	      (defmethod createPriv (idx arena)
+		(declare (type int idx)
+			 (type "Arena<T,N>&" arena)
+			 (values shared_ptr<Priv>))
+		(return (shared_ptr<Priv> (new (Priv idx arena))
+					  (lambda (p)
+					    (declare (type Priv* p))
+					    ,(lprint :msg "~shared_ptr" :vars `(p p->idx ))
+					    ;("p->arena.setUnused" p->idx)
+					    (delete p)))))
+	      
+	      "T& ref;"
+	      "atomic<shared_ptr<Priv>> sp{nullptr};"
+	      ))
+
+     ,(let ((name "Arena"))
+	`(space "template<typename T, int N>"
+	       (defclass+ ,name ()
+		 "public:"
+		 (defmethod aquire ()
+		   (declare (values Ref<T>))
+		   (let ((it (find (used.begin)
+				   (used.end)
+				   false)))
+		     (if (== (used.end)
+			     it)
+			 (do0
+			  (throw (runtime_error (string "no free arena element"))))
+
+			 (do0
+			  (setf *it true)
+			  (let ((idx (- it (used.begin)))
+				(el (dot r (at idx)))))
+			  ,(lprint :msg "found unused element"
+				   :vars `(idx))
+			  (return el)))))
+
+		 (defmethod setUnused (idx)
+		   (declare (type int idx))
+		   ,(lprint :msg "Arena::setUnused"
+			    :vars `(idx))
+		   (setf (aref used idx) false))
+		 (defmethod use_count (idx)
+		   (declare (values "long int")
+			    (type int idx))
+		   (let ((count (dot (aref r idx) (use_count)))))
+		   ,(lprint :msg "Arena::use_count"
+			    :vars `(count))
+		   (return count))
+		 
+		 (defmethod ,name ()
+		   (declare (values :constructor))
+		   "int idx=0;"
+		   (for-range (e a)
+			      (r.emplace_back e idx *this)
+			      (incf idx)))
+
+		 ,@(loop for e in `(,(format nil "~a(const T&)" name)
+				    ,(format nil "~a(T&&)" name)
+				    "const T& operator=(const T&)"
+				    "T& operator=(T&&)")
+			 collect
+			 (format nil "~a = delete;" e))
+		 "private:"
+		 "array<T,N> a;"
+		 "array<bool,N> used{};"
+		 "deque<Ref<T>> r;")))
+     
+     (defun main (argc argv)
+       (declare (values int)
+		(type int argc)
+		(type char** argv))
+       (defclass+ Widget ()
+	 "public:"
+	 "private:"
+	 "int i{3};"
+	 "float f{4.5F};")
+             
+       (do0
+	(let ((a (space Arena (angle Widget N) (paren)))))
+
+	(let ((v (deque<Ref<Widget>> ))))
+	(dotimes (i N)
+	  (let ((e (a.aquire))))
+	  (assert (== i (e.idx)))
+	  (v.push_back e))
+	,(lprint :msg "#### CLEAR ####")
+	(v.clear)
+	,(lprint :msg "#### REUSE N ELEMENTS ####")
+	(dotimes (i N)
+	  (let ((e (a.aquire))))
+	  (assert (== i (e.idx)))
+	  (v.push_back e)))
+       ,(lprint :msg "#### TRY TO GET ONE ELEMENT TOO MANY ####")
+       (v.push_back (a.aquire))
+       
+       #+nil
+       (do0 (let ((as (space array (angle Widget N)
+			     (paren)))))
+	    (let ((ar (space deque (angle (space Ref (angle Widget)))
+			     (paren))))
+	      (for-range (e as)
+			 (ar.emplace_back e)))
+	    
+	    ,(lprint :vars `((sizeof as)))
+	    ,(lprint :vars `((sizeof ar)))
+	    (let ((e (dot (aref ar 0)
+			  (get)))))
+	    (let ((qq (aref ar 0))))
+	    ,(lprint :vars `((dot (aref ar 0) (use_count)))))
+            
+       (return 0)))
+   :omit-parens t
+   :format nil
+   :tidy nil)
+  (sb-ext:run-program "/usr/bin/clang-format"
+		      `("-i"
+			,@(loop for e in (directory (format nil "~a/*.*" *full-source-dir*))
+				collect (format nil "~a" e))
+			"-style=file")))
+
---
+++ example/182_shared_from_this_koan/src/main.cpp
@@ -0,0 +1,113 @@
+#include <array>
+#include <atomic>
+#include <cassert>
+#include <deque>
+#include <iostream>
+#include <memory>
+using namespace std;
+constexpr int N{3};
+template <typename T, int N>
+class Arena;
+template <typename T>
+class Ref {
+public:
+    explicit Ref(T& r, int idx, Arena<T, N>& arena) : ref{r}, sp{createPriv(idx, arena)} {
+        std::cout << "Ref::ctor" << " idx='" << idx << "' " << " sp.load().get()='" << sp.load().get() << "' "
+                  << " &ref='" << &ref << "' " << " &arena='" << &arena << "' " << std::endl;
+    }
+    ~Ref() {
+        std::cout << "~Ref" << " use_count()='" << use_count() << "' " << std::endl;
+        if (3 == use_count()) {
+            std::cout << "#### # tell arena" << " idx()='" << idx() << "' " << " use_count()='" << use_count() << "' "
+                      << std::endl;
+            sp.load()->arena.setUnused(idx());
+        }
+    }
+    Ref(const Ref& rhs) : ref{rhs.ref}, sp{rhs.sp.load()} {
+        std::cout << "Ref::copy-ctor" << " sp.load()->idx='" << sp.load()->idx << "' " << std::endl;
+    }
+    long int use_count() { return sp.load().use_count(); }
+    long int idx() { return sp.load()->idx; }
+
+private:
+    class Priv {
+    public:
+        int          idx;
+        Arena<T, N>& arena;
+    };
+    shared_ptr<Priv> createPriv(int idx, Arena<T, N>& arena) {
+        return shared_ptr<Priv>(new Priv(idx, arena), [&](Priv* p) {
+            std::cout << "~shared_ptr" << " p='" << p << "' " << " p->idx='" << p->idx << "' " << std::endl;
+            delete (p);
+        });
+    }
+    T&                       ref;
+    atomic<shared_ptr<Priv>> sp{nullptr};
+};
+template <typename T, int N>
+class Arena {
+public:
+    Ref<T> aquire() {
+        auto it{find(used.begin(), used.end(), false)};
+        if (used.end() == it) { throw runtime_error("no free arena element"); }
+        else {
+            *it = true;
+            auto idx{it - used.begin()};
+            auto el{r.at(idx)};
+            std::cout << "found unused element" << " idx='" << idx << "' " << std::endl;
+            return el;
+        }
+    }
+    void setUnused(int idx) {
+        std::cout << "Arena::setUnused" << " idx='" << idx << "' " << std::endl;
+        used[idx] = false;
+    }
+    long int use_count(int idx) {
+        auto count{r[idx].use_count()};
+        std::cout << "Arena::use_count" << " count='" << count << "' " << std::endl;
+        return count;
+    }
+    Arena() {
+        int idx = 0;
+        for (auto&& e : a) {
+            r.emplace_back(e, idx, *this);
+            idx++;
+        }
+    }
+    Arena(const T&)              = delete;
+    Arena(T&&)                   = delete;
+    const T& operator=(const T&) = delete;
+    T&       operator=(T&&)      = delete;
+
+private:
+    array<T, N>    a;
+    array<bool, N> used{};
+    deque<Ref<T>>  r;
+};
+
+int main(int argc, char** argv) {
+    class Widget {
+    public:
+    private:
+        int   i{3};
+        float f{4.5F};
+    };
+    auto a{Arena<Widget, N>()};
+    auto v{deque<Ref<Widget>>()};
+    for (decltype(0 + N + 1) i = 0; i < N; i += 1) {
+        auto e{a.aquire()};
+        assert(i == e.idx());
+        v.push_back(e);
+    }
+    std::cout << "#### CLEAR ####" << std::endl;
+    v.clear();
+    std::cout << "#### REUSE N ELEMENTS ####" << std::endl;
+    for (decltype(0 + N + 1) i = 0; i < N; i += 1) {
+        auto e{a.aquire()};
+        assert(i == e.idx());
+        v.push_back(e);
+    }
+    std::cout << "#### TRY TO GET ONE ELEMENT TOO MANY ####" << std::endl;
+    v.push_back(a.aquire());
+    return 0;
+}
