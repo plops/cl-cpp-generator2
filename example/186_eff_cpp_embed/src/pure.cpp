112247c19dc6cc36feede43299292f8876841083
Martin <wolpumba@gmail.com>
2025-05-27 23:02:45 +0200

    more

--- example/186_eff_cpp_embed/src/pure.cpp
+++ example/186_eff_cpp_embed/src/pure.cpp
@@ -2,9 +2,21 @@
 // Created by martin on 5/27/25.
 //
 
+// class B {
+// public:
+// B() { f3(10); }
+//     // warning: pure virtual ‘virtual void B::f3(int)’ called from constructor
+// virtual void f3(int x) = 0;
+// };
+
 class B {
-public:
-B() { f3(10); }
-    // warning: pure virtual ‘virtual void B::f3(int)’ called from constructor
-virtual void f3(int x) = 0;
+    public:
+    B(){ f1();}
+    virtual void f1() {f3(10);}
+    virtual void f3(int x) = 0;
 };
+
+int main() {
+    B b; // error: cannot declare variable ‘b’ to be of abstract type ‘B’
+    return 0;
+}
