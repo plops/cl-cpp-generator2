52ac660e8c26638ecaf64c4784640c0e8dd50609
wol <wolpumba@gmail.com>
2025-12-11 23:49:43 +0100

    feat(cmake): add CMake configuration for Lua library and main application
    Introduce CMakeLists.txt to manage the build process for the Lua library and the main application. This includes setting C++ and C standards, defining source directories, and configuring the build of a static library for Lua. Additionally, implement custom memory allocation for Lua within the main application.

--- example/190_lua/gen01.lisp
+++ example/190_lua/gen01.lisp
@@ -10,7 +10,7 @@
 
 (in-package #:my-cpp-project)
 
-
+;; wget https://www.lua.org/ftp/lua-5.4.8.tar.gz; tar xaf lua-5.4.8.tar.gz
 
 
 (let ()
@@ -22,11 +22,28 @@
    "src/main.cpp"
    `(do0
      (include<> cstdio vector array)
+     (comments "check luaconf.h, LUA_32BITS LUA_NUMBER=float LUA_INT_TYPE=long LUA_IDSIZE reduce debug info to save memory")
      (space extern "\"C\"" (progn
 			     (include lua.h lualib.h lauxlib.h)))
+
+     (do0
+      "constexpr size_t LUA_HEAP_SIZE = 16 * 1024;"
+      "static std::array<uint8_t, LUA_HEAP_SIZE> lua_memory_pool;"
+      "static size_t lua_mem_used = 0;")
+     (defun custom_alloc (ud ptr osize nsize)
+       (declare (type void* ud ptr)
+		(type size_t osize nsize)
+		(values "static void*"))
+       (when (== 0 nsize)
+	 (return nullptr))
+       (when (< LUA_HEAP_SIZE (+ lua_mem_used nsize))
+	 (return nullptr))
+       (setf "void* new_ptr"
+	     (aref &lua_memory_pool lua_mem_used))
+       (incf lua_mem_used nsize)
+       (return new_ptr))
      (defun main ()
        (declare (values int))
        (return 0)))
    :omit-parens t
-   :dir *full-source-dir*)
-  #+nil (write-class ))
+   :dir *full-source-dir*))
--- example/190_lua/src/main.cpp
+++ example/190_lua/src/main.cpp
@@ -1,13 +1,27 @@
+#include <array>
 #include <cstdio>
 #include <vector>
-#include <array> 
+// check luaconf.h, LUA_32BITS LUA_NUMBER=float LUA_INT_TYPE=long LUA_IDSIZE
+// reduce debug info to save memory
 extern "C" {
-        #include "lua.h"
+#include "lauxlib.h"
+#include "lua.h"
 #include "lualib.h"
-#include "lauxlib.h" 
 };
+constexpr size_t LUA_HEAP_SIZE = 16 * 1024;
+static std::array<uint8_t, LUA_HEAP_SIZE> lua_memory_pool;
+static size_t lua_mem_used = 0;
 
-int main ()        {
-        return 0;
+static void *custom_alloc(void *ud, void *ptr, size_t osize, size_t nsize) {
+  if (0 == nsize) {
+    return nullptr;
+  }
+  if (LUA_HEAP_SIZE < lua_mem_used + nsize) {
+    return nullptr;
+  }
+  void *new_ptr = &lua_memory_pool[lua_mem_used];
+  lua_mem_used += nsize;
+  return new_ptr;
 }
- 
+
+int main() { return 0; }
