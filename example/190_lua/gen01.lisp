cf61fadfd341f18264c4d97f18e7a65881ada3b4
wol <wolpumba@gmail.com>
2025-12-12 00:12:18 +0100

    feat(lua): add panic handler and native addition function
    this line   luaL_requiref(L, "_G", luaopen_base, 1);
    enters the panic handler

--- example/190_lua/gen01.lisp
+++ example/190_lua/gen01.lisp
@@ -14,6 +14,20 @@
 
 
 (let ()
+  (defun lprint (&key (msg "")
+		 (vars nil)
+		 )
+    `(<< std--cout
+       (string ,(format nil "~a"
+			msg
+			
+			))
+       ,@(loop for e in vars
+	       appending
+	       `((string ,(format nil " ~a='" (emit-c :code e :omit-redundant-parentheses t)))
+		 ,e
+		 (string "' ")))   
+       std--endl))
   (defparameter *source-dir* #P"example/190_lua/")
   (defparameter *full-source-dir* (asdf:system-relative-pathname
 				     'cl-cpp-generator2
@@ -21,7 +35,7 @@
    (write-source
    "src/main.cpp"
    `(do0
-     (include<> cstdio vector array cstdint)
+     (include<> cstdio vector array cstdint iostream)
      (comments "check luaconf.h, LUA_32BITS LUA_NUMBER=float LUA_INT_TYPE=long LUA_IDSIZE reduce debug info to save memory")
      (space extern "\"C\"" (progn
 			     (include lua.h lualib.h lauxlib.h)))
@@ -42,8 +56,56 @@
 	     (aref &lua_memory_pool lua_mem_used))
        (incf lua_mem_used nsize)
        (return new_ptr))
+
+     (defun panic_handler (L)
+       (declare (type lua_State* L)
+		(values "static int"))
+       (let ((*msg (lua_tostring L -1))))
+       ,(lprint :vars `(msg))
+       (while true)
+       (return 0))
+
+     (defun native_add (L)
+       (declare (type lua_State* L)
+		(values "static int"))
+       (let ((a (luaL_checknumber L 1))
+	     (b (luaL_checknumber L 2)))
+	 (lua_pushnumber L (+ a b))
+	 (return 1)))
+     
      (defun main ()
        (declare (values int))
+       (let ((*L (lua_newstate custom_alloc nullptr)))
+	 (when (== nullptr L)
+	   ,(lprint :msg "Failed to init Lua state (OOM)")
+	   (return -1)))
+       (lua_atpanic L panic_handler)
+       (comments "lade nur was noetig ist, kein luaL_openlibs")
+       (luaL_requiref L (string "_G")
+		      luaopen_base 1)
+       (lua_pop L 1)
+       (lua_pushcfunction L native_add)
+       (lua_setglobal L (string "cpp_add"))
+       (let ((*lua_script (string-r "local x = 10
+local y = 20
+local sum = cpp_add(x,y)
+print('Sum: ' .. sum)
+lua_status = 'OK'"))))
+       (let ((status (luaL_dostring L lua_script))))
+       (if (== LUA_OK status)
+	   (do0
+	    (lua_getglobal L (string lua_status))
+	    (when (lua_isstring L -1)
+	      (let ((stat (lua_tostring L -1)))
+		,(lprint :vars `(stat))))
+	    (lua_pop L 1))
+	   (do0
+	    (when (lua_isstring L -1)
+	      (let ((err (lua_tostring L -1)))
+		,(lprint :vars `(err))))
+	    (lua_pop L 1))
+	   )
+       (lua_close L)
        (return 0)))
    :omit-parens t
    :dir *full-source-dir*))
--- example/190_lua/src/main.cpp
+++ example/190_lua/src/main.cpp
@@ -1,6 +1,7 @@
 #include <array>
 #include <cstdint>
 #include <cstdio>
+#include <iostream>
 #include <vector>
 // check luaconf.h, LUA_32BITS LUA_NUMBER=float LUA_INT_TYPE=long LUA_IDSIZE
 // reduce debug info to save memory
@@ -25,4 +26,53 @@ static void *custom_alloc(void *ud, void *ptr, size_t osize, size_t nsize) {
   return new_ptr;
 }
 
-int main() { return 0; }
+static int panic_handler(lua_State *L) {
+  auto *msg{lua_tostring(L, -1)};
+  std::cout << "" << " msg='" << msg << "' " << std::endl;
+  while (true) {
+  }
+  return 0;
+}
+
+static int native_add(lua_State *L) {
+  auto a{luaL_checknumber(L, 1)};
+  auto b{luaL_checknumber(L, 2)};
+  lua_pushnumber(L, a + b);
+  return 1;
+}
+
+int main() {
+  auto *L{lua_newstate(custom_alloc, nullptr)};
+  if (nullptr == L) {
+    std::cout << "Failed to init Lua state (OOM)" << std::endl;
+    return -1;
+  }
+  lua_atpanic(L, panic_handler);
+  // lade nur was noetig ist, kein luaL_openlibs
+  luaL_requiref(L, "_G", luaopen_base, 1);
+  lua_pop(L, 1);
+  lua_pushcfunction(L, native_add);
+  lua_setglobal(L, "cpp_add");
+  auto *lua_script{R"(local x = 10
+local y = 20
+local sum = cpp_add(x,y)
+print('Sum: ' .. sum)
+lua_status = 'OK')"};
+  auto status{luaL_dostring(L, lua_script)};
+  if (LUA_OK == status) {
+    lua_getglobal(L, "lua_status");
+    if (lua_isstring(L, -1)) {
+      auto stat{lua_tostring(L, -1)};
+      std::cout << "" << " stat='" << stat << "' " << std::endl;
+    }
+    lua_pop(L, 1);
+  } else {
+    if (lua_isstring(L, -1)) {
+      auto err{lua_tostring(L, -1)};
+      std::cout << "" << " err='" << err << "' " << std::endl;
+    }
+    lua_pop(L, 1);
+  }
+  lua_close(L);
+  return 0;
+}
