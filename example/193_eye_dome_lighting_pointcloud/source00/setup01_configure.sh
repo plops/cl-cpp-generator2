29f8146368c5c73746e1e8beb8aef795612502b8
wol <wolpumba@gmail.com>
2026-02-22 16:18:17 +0000

    cmake configures

--- example/193_eye_dome_lighting_pointcloud/source00/setup01_configure.sh
+++ example/193_eye_dome_lighting_pointcloud/source00/setup01_configure.sh
@@ -1,8 +1,8 @@
-export OpenGL_GL_PREFERENCE="GLVND"
 mkdir b
 cd b
 cmake .. \
       -GNinja \
       -DCMAKE_BUILD_TYPE=Release \
       -DGLFW_BUILD_WAYLAND=OFF \
-      -DGLFW_INSTALL=OFF
+      -DGLFW_INSTALL=OFF \
+      -DOpenGL_GL_PREFERENCE=GLVND
