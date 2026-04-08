f57bf2cb5a94f29ba5a229a01dcbf7afe92b6af0
wol <wolpumba@gmail.com>
2026-04-08 19:30:53 +0000

    mroe

--- example/195_gccjit/gen01.lisp
+++ example/195_gccjit/gen01.lisp
@@ -9,18 +9,44 @@
   (setf *features* (set-difference *features* (list :more)))
   (setf *features* (set-exclusive-or *features* (list :more))))
 
+
+
+
 (progn
   (defparameter *source-dir* #P"example/195_gccjit/")
   (defparameter *full-source-dir* (asdf:system-relative-pathname 'cl-cpp-generator2 *source-dir*))
   (ensure-directories-exist *full-source-dir*)
-  
+
+  (defun lprint (&key (msg "")
+		   (vars nil)
+		   )
+    #+nil `(<< std--cout
+	       (std--format
+		(string ,(format nil "~a~{ ~a='{}'~}\\n"
+				 msg
+				 (loop for e in vars collect (emit-c :code e  :omit-redundant-parentheses t)) ))
+		,@vars))
+    #-more
+    ""
+    #+more
+    `(<< std--cout
+	 (string ,(format nil "~a"
+			  msg
+			  
+			  ))
+	 ,@(loop for e in vars
+		 appending
+		 `((string ,(format nil " ~a='" (emit-c :code e :omit-redundant-parentheses t)))
+		   ,e
+		   (string "' ")))   
+	 std--endl))
   
   (write-source
    (asdf:system-relative-pathname 'cl-cpp-generator2 (merge-pathnames "main.cpp" *source-dir*))
    `(do0
      (include<> iostream
 		libgccjit++.h)
-  
+     
 
      (defun main (argc argv)
        (declare (type int argc) (type char** argv) (values int))
@@ -28,16 +54,29 @@
 	     (int_type (ctx.get_type GCC_JIT_TYPE_INT))
 	     (param_i (ctx.new_param int_type
 				     (string "i")))
-	     (params (curly param_i)))
-	 (declare (type "std::vector<gccjit::param>"
-			params))
+	     )
+	 "std::vector<gccjit::param> params = { param_i };"
 	 (let ((func (ctx.new_function
 		      GCC_JIT_FUNCTION_EXPORTED
 		      int_type
 		      (string "square")
 		      params
-		      0)))))
+		      0))
+	       (block (func.new_block (string "entry")))
+	       (i_rval param_i))
+	   (block.end_with_return (* i_rval i_rval))
+	   (let ((*result (ctx.compile)))
+	     (unless result
+	       ,(lprint :msg "compilation failed")
+	       (return 1)))
+	   (let ((square ("reinterpret_cast<int (*)(int)>" (gcc_jit_result_get_code
+							    result (string "square"))))))
+	   (let ((val 5)
+		 (sq (square val)))
+	     ,(lprint :msg "result"
+		      :vars `(val sq)))
+	   (gcc_jit_result_release result)))
        (return 0)))
-  :omit-parens t
-  :format t
-  :tidy nil))
+   :omit-parens t
+   :format t
+   :tidy nil))
--- example/195_gccjit/main.cpp
+++ example/195_gccjit/main.cpp
@@ -5,8 +5,23 @@ int main(int argc, char **argv) {
   auto ctx{gccjit::context::acquire()};
   auto int_type{ctx.get_type(GCC_JIT_TYPE_INT)};
   auto param_i{ctx.new_param(int_type, "i")};
-  std::vector<gccjit::param> params{{param_i}};
+  std::vector<gccjit::param> params = {param_i};
   auto func{ctx.new_function(GCC_JIT_FUNCTION_EXPORTED, int_type, "square",
                              params, 0)};
+  auto block{func.new_block("entry")};
+  auto i_rval{param_i};
+  block.end_with_return(i_rval * i_rval);
+  auto *result{ctx.compile()};
+  if (!result) {
+    std::cout << "compilation failed" << std::endl;
+    return 1;
+  }
+  auto square{reinterpret_cast<int (*)(int)>(
+      gcc_jit_result_get_code(result, "square"))};
+  auto val{5};
+  auto sq{square(val)};
+  std::cout << "result" << " val='" << val << "' " << " sq='" << sq << "' "
+            << std::endl;
+  gcc_jit_result_release(result);
   return 0;
 }
