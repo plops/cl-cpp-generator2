1cd2847f8371cec8fb67c43e370fb788cb2ee223
wol <wolpumba@gmail.com>
2026-04-11 19:54:36 +0000

    more

--- example/196_forthjit/gen01.lisp
+++ example/196_forthjit/gen01.lisp
@@ -17,58 +17,6 @@
   
   (load "util.lisp")
   
-  
-  
-
-  
-
-  #+nil
-  (defclass+ ForthVM ()
-    "static constexpr auto MAX_STACK =  256;"
-    "static constexpr auto MAX_DICT =  64;"
-    "static constexpr auto FUEL_LIMIT =  10'000;"
-    "std::vector<int> stack;"
-    "std::unordered_map<std::string, int> variables;"
-    "std::unordered_map<std::string, void(*)()> dictionary;"
-    "int fuel = 0;"
-    "public:"
-    (defmethod push (val)
-      (declare (type int val)
-	       (values void))
-      (when (<= MAX_STACK (stack.size))
-	(throw Error--Stack_Error))
-      (stack.push_back val))
-
-    (defmethod pop ()
-      (declare (values int))
-      (when (stack.empty)
-	(throw Error--Stack_Error))
-      (let ((val (stack.back)))
-	(stack.pop_back)
-	(return val)))
-
-    (defmethod consume_fuel ()
-      (when (< FUEL_LIMIT
-	       "++fuel")
-	(throw Error--Stack_Error)))
-
-    (defmethod dot ()
-      (<< std--cout (pop) (string " ")))
-
-    (defmethod dup ()
-      (let ((v (pop)))
-	(push v)
-	(push v)))
-
-    (defmethod drop ()
-      (pop))
-
-    (defmethod swap ()
-      (let ((a (pop))
-	    (b (pop)))
-	(push a)
-	(push b))))
-  
   (let* ((l-prim0 `((:name Add :symbol +) (:name Sub :symbol -) (:name Mul :symbol *) (:name Dup)
 		    (:name Drop) (:name Swap) (:name Dot :symbol ".") (:name LessThan :symbol < :short lt) (:name GreaterThan :symbol > :short gt)
 		    (:name Equal :symbol = :short eq) (:name Fetch :symbol @) (:name Store :symbol !)))
@@ -84,4 +32,5 @@
     (load "operation.lisp")
     (load "jit.lisp")
     (load "helpers.lisp")
+    (load "vm.lisp")
     (load "main.lisp")))
